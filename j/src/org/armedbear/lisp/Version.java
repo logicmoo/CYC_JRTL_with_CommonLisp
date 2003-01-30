c1f153da5609959e837e9bd0588b1b718c352f5d
Peter Graves <gnooth@gmail.com>
2003-01-30 15:25:37 +0000

    0.0.0.5+

--- j/src/org/armedbear/lisp/Version.java
+++ j/src/org/armedbear/lisp/Version.java
@@ -2,7 +2,7 @@
  * Version.java
  *
  * Copyright (C) 2003 Peter Graves
- * $Id: Version.java,v 1.4 2003-01-29 17:41:35 piso Exp $
+ * $Id: Version.java,v 1.5 2003-01-30 15:25:37 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -27,6 +27,6 @@ public final class Version
 
     public static String getVersion()
     {
-        return "0.0.0.5";
+        return "0.0.0.5+";
     }
 }
