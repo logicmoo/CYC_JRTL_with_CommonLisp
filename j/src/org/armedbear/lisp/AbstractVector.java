eb155fc37d94ddc3d70c42feb5574ca299256434
Peter Graves <gnooth@gmail.com>
2003-02-09 18:00:12 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/AbstractVector.java
@@ -0,0 +1,38 @@
+/*
+ * AbstractVector.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public abstract class AbstractVector extends LispObject
+{
+    protected void badIndex(long index) throws LispException
+    {
+        StringBuffer sb = new StringBuffer("invalid array index ");
+        sb.append(index);
+        sb.append(" for ");
+        sb.append(toString());
+        if (length() > 0) {
+            sb.append(" (should be >= 0 and < ");
+            sb.append(length());
+            sb.append(')');
+        }
+        throw new LispException(sb.toString());
+    }
+}
