d0b4e6bd727e938a70d51b41a0a47e190d1763a8
Peter Graves <gnooth@gmail.com>
2003-02-12 02:37:51 +0000

    multiple-value-list: single-value case.

--- j/src/org/armedbear/lisp/Tests.java
+++ j/src/org/armedbear/lisp/Tests.java
@@ -2,7 +2,7 @@
  * Tests.java
  *
  * Copyright (C) 2002-2003 Peter Graves
- * $Id: Tests.java,v 1.4 2003-02-09 00:52:07 piso Exp $
+ * $Id: Tests.java,v 1.5 2003-02-12 02:37:51 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -572,6 +572,7 @@ public class Tests extends TestCase
             "(1 2 NIL)");
 
         // multiple-value-list
+        verify("(multiple-value-list (car '(a b c)))", "(A)");
         verify("(multiple-value-list (values 1 2 3))", "(1 2 3)");
 
         // macroexpand-1
