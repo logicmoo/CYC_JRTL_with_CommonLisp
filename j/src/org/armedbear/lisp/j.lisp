734fa20df95a6b20c144f743c1abc2a1b06dc083
Peter Graves <gnooth@gmail.com>
2003-02-14 20:55:21 +0000

    set-global-property: support multiple key/value pairs.

--- j/src/org/armedbear/lisp/j.lisp
+++ j/src/org/armedbear/lisp/j.lisp
@@ -21,8 +21,17 @@
 (defun unmap-key-for-mode (key mode)
   (jstatic "mapKeyForMode" "org.armedbear.j.API" key mode))
 
-(defun set-global-property (key value)
-  (jstatic "setGlobalProperty" "org.armedbear.j.API" key value))
+(defun %set-global-property (key value)
+  (jstatic "setGlobalProperty" "org.armedbear.j.Editor" key value))
+
+(defun set-global-property (&rest args)
+  (let ((nargs (length args)) key value)
+    (when (oddp nargs)
+      (error "odd number of arguments to SET-GLOBAL-PROPERTY"))
+    (loop
+      (when (null args) (return (values)))
+      (%set-global-property (first args) (second args))
+      (setf args (cddr args)))))
 
 (in-package "COMMON-LISP-USER")
 (use-package "J")
