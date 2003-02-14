3c7c1ac3669dff1c703b13019b32a85b57d4782e
Peter Graves <gnooth@gmail.com>
2003-02-14 16:46:23 +0000

    listAllPackages()

--- j/src/org/armedbear/lisp/Packages.java
+++ j/src/org/armedbear/lisp/Packages.java
@@ -2,7 +2,7 @@
  * Packages.java
  *
  * Copyright (C) 2002-2003 Peter Graves
- * $Id: Packages.java,v 1.1 2003-01-17 19:43:23 piso Exp $
+ * $Id: Packages.java,v 1.2 2003-02-14 16:46:23 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,62 +21,61 @@
 
 package org.armedbear.lisp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class Packages extends Lisp
+public final class Packages extends Lisp
 {
+    private static final ArrayList packages = new ArrayList();
     private static final HashMap map = new HashMap();
 
     // Creates package if it doesn't already exist.
-    public static final Package getPackage(String name)
+    public static final synchronized Package getPackage(String name)
     {
-        synchronized (map) {
-            Package pkg = (Package) map.get(name);
-            if (pkg == null)
-                map.put(name, pkg = new Package(name));
-            return pkg;
+        Package pkg = (Package) map.get(name);
+        if (pkg == null) {
+            pkg = new Package(name);
+            packages.add(pkg);
+            map.put(name, pkg);
         }
+        return pkg;
     }
 
     // Returns null if package doesn't exist.
-    public static final Package findPackage(String name)
+    public static final synchronized Package findPackage(String name)
     {
-        synchronized (map) {
-            return (Package) map.get(name);
-        }
+        return (Package) map.get(name);
     }
 
-    public static final Package makePackage(String name) throws LispException
-    {
-        synchronized (map) {
-            if (map.get(name) != null)
-                throw new LispException("a package named " + name +
-                    " already exists");
-            Package pkg = new Package(name);
-            map.put(name, pkg);
-            return pkg;
-        }
-    }
-
-    public static final void addNickname(Package pkg, String nickname)
+    public static final synchronized Package makePackage(String name)
         throws LispException
     {
-        synchronized (map) {
-            if (map.get(nickname) != null)
-                throw new LispException("a package named " + nickname +
-                    " already exists");
-            map.put(nickname, pkg);
-        }
+        if (map.get(name) != null)
+            throw new LispException("a package named " + name +
+                " already exists");
+        Package pkg = new Package(name);
+        packages.add(pkg);
+        map.put(name, pkg);
+        return pkg;
     }
 
-    public static final LispObject listAllPackages()
+    public static final synchronized void addNickname(Package pkg,
+        String nickname) throws LispException
     {
-        LispObject list = NIL;
-        for (Iterator it = map.values().iterator(); it.hasNext();) {
+        if (map.get(nickname) != null)
+            throw new LispException("a package named " + nickname +
+                " already exists");
+        map.put(nickname, pkg);
+    }
+
+    public static final synchronized LispObject listAllPackages()
+    {
+        LispObject result = NIL;
+        for (Iterator it = packages.iterator(); it.hasNext();) {
             Package pkg = (Package) it.next();
-            list = new Cons(pkg, list);
+            result = new Cons(pkg, result);
         }
-        return list;
+        return result;
     }
 }
