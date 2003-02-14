3f176f20467a67061c9de9c9f68f01823010a42f
Peter Graves <gnooth@gmail.com>
2003-02-14 19:17:52 +0000

    Refactoring.

--- j/src/org/armedbear/lisp/boot.lisp
+++ j/src/org/armedbear/lisp/boot.lisp
@@ -1,8 +1,9 @@
 ;;; boot.lisp
 
-(cl::%load "documentation.lisp")
-(cl::%load "backquote.lisp")
-(cl::%load "setf.lisp")
-(cl::%load "macros.lisp")
-(cl::%load "list.lisp")
-(cl::%load "symbol.lisp")
+(dolist (name '("documentation"
+                "backquote"
+                "setf"
+                "macros"
+                "list"
+                "symbol"))
+  (cl::%load (concatenate 'string name ".lisp")))
