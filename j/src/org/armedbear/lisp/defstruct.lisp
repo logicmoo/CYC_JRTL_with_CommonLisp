4973f4c782e4db6289501ab9c9f0a82a4bc33459
Peter Graves <gnooth@gmail.com>
2003-02-18 01:26:44 +0000

    Refactoring.

--- j/src/org/armedbear/lisp/defstruct.lisp
+++ j/src/org/armedbear/lisp/defstruct.lisp
@@ -4,6 +4,15 @@
 
 (export 'defstruct)
 
+(defun make-constructor (name slots)
+  (let ((nslots (length slots))
+        (constructor (intern (concatenate 'string "MAKE-" (symbol-name name)))))
+    (setf (symbol-function constructor)
+          #'(lambda ()
+             (let ((structure (make-array (1+ nslots))))
+               (setf (svref structure 0) name)
+               structure)))))
+
 (defun make-access-function (name slot index)
   (let ((accessor
          (intern (concatenate 'string (symbol-name name) "-" (symbol-name slot)))))
@@ -12,16 +21,13 @@
     (%put accessor *setf-expander*
           #'(lambda (instance new-value) (%svset instance `,index new-value)))))
 
-(defmacro defstruct (name &rest slots)
-  (let* ((dd (append (list name) slots))
-         (n-slots (length slots))
-         (constructor (intern (concatenate 'string "MAKE-" (symbol-name name))))
-         (index 1))
-    (eval `(defun ,constructor ()
-       (let ((structure (make-array (1+ ,n-slots))))
-         (setf (svref structure 0) ',name)
-         structure)))
+(defun make-access-functions (name slots)
+  (let ((index 1))
     (dolist (slot slots)
       (make-access-function name slot index)
-      (incf index)))
+      (incf index))))
+
+(defmacro defstruct (name &rest slots)
+  (make-constructor name slots)
+  (make-access-functions name slots)
   `',name)
