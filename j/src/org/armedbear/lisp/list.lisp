cde6d7ed7030760951a85fc68efbd4944eee04ab
Peter Graves <gnooth@gmail.com>
2003-02-18 19:45:39 +0000

    cdddr, list-length, copy-list, copy-tree, revappend

--- j/src/org/armedbear/lisp/list.lisp
+++ j/src/org/armedbear/lisp/list.lisp
@@ -2,7 +2,43 @@
 
 (in-package "COMMON-LISP")
 
-(export '(nconc nreconc))
+(export '(cdddr list-length copy-list copy-tree revappend nconc nreconc))
+
+(defun cdddr (list)
+  (cdr (cdr (cdr list))))
+
+(defun list-length (list)
+  "Returns the length of the given List, or Nil if the List is circular."
+  (do ((n 0 (+ n 2))
+       (y list (cddr y))
+       (z list (cdr z)))
+    (())
+    (declare (fixnum n) (list y z))
+    (when (endp y) (return n))
+    (when (endp (cdr y)) (return (+ n 1)))
+    (when (and (eq y z) (> n 0)) (return nil))))
+
+(defun copy-list (list)
+  (if (atom list)
+      list
+      (let ((result (list (car list))))
+	(do ((x (cdr list) (cdr x))
+	     (splice result
+		     (cdr (rplacd splice (cons (car x) '() ))) ))
+          ((atom x)
+           (unless (null x)
+             (rplacd splice x))))
+	result)))
+
+(defun copy-tree (object)
+  (if (consp object)
+      (cons (copy-tree (car object)) (copy-tree (cdr object)))
+      object))
+
+(defun revappend (x y)
+  (do ((top x (cdr top))
+       (result y (cons (car top) result)))
+    ((endp top) result)))
 
 (defun nconc (&optional list &rest rest)
   (if rest
@@ -13,9 +49,7 @@
             rest-conc))
       list))
 
-;; From CMUCL.
 (defun nreconc (x y)
-  "Returns (nconc (nreverse x) y)"
   (do ((1st (cdr x) (if (atom 1st) 1st (cdr 1st)))
        (2nd x 1st)		;2nd follows first down the list.
        (3rd y 2nd))		;3rd follows 2nd down the list.
