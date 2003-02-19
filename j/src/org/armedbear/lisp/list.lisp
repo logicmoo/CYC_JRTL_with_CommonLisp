76b13ae7627eca26f544db4c1416e70559711769
Peter Graves <gnooth@gmail.com>
2003-02-19 03:52:03 +0000

    complement

--- j/src/org/armedbear/lisp/list.lisp
+++ j/src/org/armedbear/lisp/list.lisp
@@ -3,7 +3,7 @@
 (in-package "COMMON-LISP")
 
 (export '(cdddr list-length make-list copy-list copy-tree revappend nconc
-          nreconc))
+          nreconc complement))
 
 (defun cdddr (list)
   (cdr (cdr (cdr list))))
@@ -60,3 +60,14 @@
        (3rd y 2nd))		;3rd follows 2nd down the list.
     ((atom 2nd) 3rd)
     (rplacd 2nd 3rd)))
+
+(defun complement (function)
+  "Builds a new function that returns T whenever FUNCTION returns NIL and
+  NIL whenever FUNCTION returns T."
+  #'(lambda (&optional (arg0 nil arg0-p) (arg1 nil arg1-p) (arg2 nil arg2-p)
+		       &rest more-args)
+     (not (cond (more-args (apply function arg0 arg1 arg2 more-args))
+                (arg2-p (funcall function arg0 arg1 arg2))
+                (arg1-p (funcall function arg0 arg1))
+                (arg0-p (funcall function arg0))
+                (t (funcall function))))))
