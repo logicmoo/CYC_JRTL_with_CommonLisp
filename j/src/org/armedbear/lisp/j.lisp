3f50cfd4c905026d6f071c0b80ff3cb5ee71e8f9
Peter Graves <gnooth@gmail.com>
2003-02-20 18:50:16 +0000

    run-hooks etc.

--- j/src/org/armedbear/lisp/j.lisp
+++ j/src/org/armedbear/lisp/j.lisp
@@ -7,7 +7,11 @@
 (use-package "JAVA")
 
 (export '(global-map-key global-unmap-key map-key-for-mode unmap-key-for-mode
-          set-global-property))
+          set-global-property
+          run-hooks
+          defcommand
+          save-all
+          key-pressed-hook))
 
 (defun global-map-key (key command)
   (jstatic "globalMapKey" "org.armedbear.j.API" key command))
@@ -32,6 +36,52 @@
       (when (null args) (return (values)))
       (%set-global-property (first args) (second args))
       (setf args (cddr args)))))
+
+(defun run-hooks (hook &rest args)
+  (when (symbolp hook)
+    (setf hook (symbol-value hook))
+  (cond ((consp hook)
+         (dolist (fun hook)
+           (when (functionp fun)
+             (apply fun args))))
+        ((functionp hook)
+         (apply hook args)))))
+
+(defconstant current-editor-method
+  (jmethod "org.armedbear.j.Editor" "currentEditor"))
+
+(defun current-editor ()
+  (jstatic current-editor-method nil))
+
+(defconstant update-display-method
+  (jmethod "org.armedbear.j.Editor" "updateDisplay"))
+
+(defun update-display (&optional ed)
+  (let ((ed (or ed (current-editor))))
+    (jcall update-display-method ed)))
+
+(defun status (string &optional ed)
+  (let ((ed (or ed (current-editor)))
+        (method (jmethod "org.armedbear.j.Editor" "status"
+                         "java.lang.String")))
+    (jcall method ed string)))
+
+(defconstant execute-command-method
+  (jmethod "org.armedbear.j.Editor" "executeCommand"
+           "java.lang.String"))
+
+(defun execute-command (command &optional ed)
+  (let ((ed (or ed (current-editor))))
+    (jcall execute-command-method ed command)
+    (update-display ed)))
+
+(defmacro defcommand (name command)
+  `(setf (symbol-function ',name)
+         (lambda () (execute-command ,command))))
+
+(defcommand save-all "saveAll")
+
+(defvar key-pressed-hook nil)
 
 (in-package "COMMON-LISP-USER")
 (use-package "J")
