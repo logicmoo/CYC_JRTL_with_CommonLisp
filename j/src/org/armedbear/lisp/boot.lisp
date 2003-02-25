8b832170268487d60cb07cc9f8aa6424dcf5d44d
Peter Graves <gnooth@gmail.com>
2003-02-25 16:47:45 +0000

    open

--- j/src/org/armedbear/lisp/boot.lisp
+++ j/src/org/armedbear/lisp/boot.lisp
@@ -10,3 +10,14 @@
                 "error"
                 "defstruct"))
   (cl::%load (concatenate 'string name ".lisp")))
+
+(defun open (filename
+	     &key
+	     (direction :input)
+	     (element-type 'base-char)
+	     (if-exists nil if-exists-given)
+	     (if-does-not-exist nil if-does-not-exist-given)
+	     (external-format :default))
+  (if (eq direction :output)
+      (%open-output-file filename)
+      (error "operation not supported")))
