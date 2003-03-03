3d680653267150548b2045915245e78281687346
Peter Graves <gnooth@gmail.com>
2003-03-03 18:15:21 +0000

    typep()

--- j/src/org/armedbear/lisp/Function.java
+++ j/src/org/armedbear/lisp/Function.java
@@ -2,7 +2,7 @@
  * Function.java
  *
  * Copyright (C) 2002-2003 Peter Graves
- * $Id: Function.java,v 1.4 2003-03-03 03:04:50 piso Exp $
+ * $Id: Function.java,v 1.5 2003-03-03 18:15:21 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -65,6 +65,13 @@ public abstract class Function extends LispObject
     public LispObject typeOf()
     {
         return Symbol.FUNCTION;
+    }
+
+    public LispObject typep(LispObject typeSpecifier) throws LispError
+    {
+        if (typeSpecifier == Symbol.FUNCTION)
+            return T;
+        return super.typep(typeSpecifier);
     }
 
     public final String getName()
