cd5936d939c58c14891a8dc8a2f63c86de12c4bd
Peter Graves <gnooth@gmail.com>
2003-03-04 15:29:48 +0000

    fdefinition %set-fdefinition

--- j/src/org/armedbear/lisp/setf.lisp
+++ j/src/org/armedbear/lisp/setf.lisp
@@ -1,8 +1,25 @@
 ;;; setf.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: setf.lisp,v 1.7 2003-03-04 15:29:48 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 (in-package "COMMON-LISP")
 
-(export '(setf incf decf defsetf))
+(export '(setf incf decf fdefinition defsetf))
 
 (defun %rplaca (x val) (rplaca x val) val)
 
@@ -50,6 +67,20 @@
 (defmacro decf (place &optional (delta 1))
   `(setf ,place (- ,place ,delta)))
 
+(defun fdefinition (name)
+  (cond ((symbolp name)
+         (symbol-function name))
+        ((and (consp name) (eq (car name) 'setf))
+         (or (get *setf-expander* (cadr name)) (error 'undefined-function)))
+        (t (error 'type-error))))
+
+(defun %set-fdefinition (name function)
+  (cond ((symbolp name)
+         (fset name function))
+        ((and (consp name) (eq (car name) 'setf))
+         (%put (cadr name) *setf-expander* function))
+        (t (error 'type-error))))
+
 (defconstant *setf-expander* (make-symbol "SETF-EXPANDER"))
 
 (%put 'car *setf-expander* '%rplaca)
@@ -59,6 +90,7 @@
 (%put 'aref *setf-expander* '%aset)
 (%put 'svref *setf-expander* '%svset)
 (%put 'fill-pointer *setf-expander* '%set-fill-pointer)
+(%put 'fdefinition *setf-expander* '%set-fdefinition)
 (%put 'symbol-function *setf-expander* 'fset)
 (%put 'symbol-plist *setf-expander* '%set-symbol-plist)
 (%put 'get *setf-expander* '%put)
