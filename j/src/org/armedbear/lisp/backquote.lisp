2758753368b50440126539b70e7b7bcdc707dbaf
Peter Graves <gnooth@gmail.com>
2003-03-06 02:22:59 +0000

    Added license header.

--- j/src/org/armedbear/lisp/backquote.lisp
+++ j/src/org/armedbear/lisp/backquote.lisp
@@ -1,4 +1,21 @@
 ;;; backquote.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: backquote.lisp,v 1.2 2003-03-06 02:22:59 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 (in-package "COMMON-LISP")
 
@@ -77,5 +94,3 @@
               (null (cdddr x)))
          (cons 'cons (maptree #'bq-remove-tokens (cdr x))))
         (t (maptree #'bq-remove-tokens x))))
-
-(provide "backquote")
