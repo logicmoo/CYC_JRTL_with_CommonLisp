dee57b2864d40565ce06d5304d22d6ae68920c28
Peter Graves <gnooth@gmail.com>
2003-05-11 13:54:41 +0000

    doStdin(): don't prepend prompt.

--- j/src/org/armedbear/j/jdb/Jdb.java
+++ j/src/org/armedbear/j/jdb/Jdb.java
@@ -2,7 +2,7 @@
  * Jdb.java
  *
  * Copyright (C) 2000-2003 Peter Graves
- * $Id: Jdb.java,v 1.4 2003-05-11 02:54:06 piso Exp $
+ * $Id: Jdb.java,v 1.5 2003-05-11 13:54:41 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -351,7 +351,6 @@ public final class Jdb extends Buffer
             logCommand("resume");
             doResume();
         } else if (command.equals("stdin")) {
-            logCommand("stdin", args);
             doStdin(args);
         } else if (command.equals("step")) {
             logCommand("step", args);
@@ -1079,8 +1078,10 @@ public final class Jdb extends Buffer
         if (process != null) {
             OutputStream out = process.getOutputStream();
             try {
-                if (s != null)
+                if (s != null) {
                     out.write(s.getBytes());
+                    log(s);
+                }
                 out.write('\n');
                 out.flush();
             }
