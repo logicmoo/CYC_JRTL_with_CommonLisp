a8eede5ca24ef6c9f5676ddb405b11c481172d3b
Peter Graves <gnooth@gmail.com>
2003-05-15 01:17:48 +0000

    saveBreakpoints(): don't save temporary breakpoints.

--- j/src/org/armedbear/j/jdb/JdbSession.java
+++ j/src/org/armedbear/j/jdb/JdbSession.java
@@ -1,8 +1,8 @@
 /*
  * JdbSession.java
  *
- * Copyright (C) 2000-2002 Peter Graves
- * $Id: JdbSession.java,v 1.1.1.1 2002-09-24 16:09:39 piso Exp $
+ * Copyright (C) 2000-2003 Peter Graves
+ * $Id: JdbSession.java,v 1.2 2003-05-15 01:17:48 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -220,10 +220,12 @@ public final class JdbSession extends Properties
                     Object obj = iter.next();
                     if (obj instanceof MethodBreakpoint) {
                         MethodBreakpoint bp = (MethodBreakpoint) obj;
-                        writer.write(bp.toXml());
+                        if (!bp.isTemporary())
+                            writer.write(bp.toXml());
                     } else if (obj instanceof LineNumberBreakpoint) {
                         LineNumberBreakpoint bp = (LineNumberBreakpoint) obj;
-                        writer.write(bp.toXml());
+                        if (!bp.isTemporary())
+                            writer.write(bp.toXml());
                     }
                 }
                 writer.write("  </breakpoints>");
