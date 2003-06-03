7c73fdc904ddc13881be1ceb5886fadbd29a9d84
Peter Graves <gnooth@gmail.com>
2003-06-03 16:16:03 +0000

    Java 1.4, SAX2.

--- j/src/org/armedbear/j/FileHistory.java
+++ j/src/org/armedbear/j/FileHistory.java
@@ -1,8 +1,8 @@
 /*
  * FileHistory.java
  *
- * Copyright (C) 1998-2002 Peter Graves
- * $Id: FileHistory.java,v 1.1.1.1 2002-09-24 16:08:42 piso Exp $
+ * Copyright (C) 1998-2003 Peter Graves
+ * $Id: FileHistory.java,v 1.2 2003-06-03 16:16:03 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -25,14 +25,15 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import org.xml.sax.AttributeList;
-import org.xml.sax.DocumentHandler;
-import org.xml.sax.HandlerBase;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.InputSource;
-import org.xml.sax.Parser;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
-public final class FileHistory extends HandlerBase implements DocumentHandler
+public final class FileHistory extends DefaultHandler implements ContentHandler
 {
     private static final int MAX_ENTRIES = 100;
 
@@ -92,22 +93,20 @@ public final class FileHistory extends HandlerBase implements DocumentHandler
 
     private void load()
     {
-        Parser parser = null;
+        XMLReader xmlReader = null;
         try {
-            // Jul 25 2000 8:41 PM
-            // The following line works with IBM 1.3 for Windows, but
-            // ParserFactory.makeParser does not. (Don't know why.)
-            parser =
-                (Parser) Class.forName("org.armedbear.j.aelfred.SAXDriver").newInstance();
+            String defaultReader = "org.apache.crimson.parser.XMLReaderImpl";
+            xmlReader = XMLReaderFactory.createXMLReader(defaultReader);
         }
         catch (Exception e) {
             Log.error(e);
         }
-        if (parser != null) {
-            parser.setDocumentHandler(this);
+        if (xmlReader != null) {
+            Log.debug("FileHistory.load xmlReader = " + xmlReader);
+            xmlReader.setContentHandler(this);
             try {
                 InputSource inputSource = new InputSource(file.getInputStream());
-                parser.parse(inputSource);
+                xmlReader.parse(inputSource);
             }
             catch (Exception e) {
                 Log.error(e);
@@ -117,38 +116,38 @@ public final class FileHistory extends HandlerBase implements DocumentHandler
 
     private FileHistoryEntry currentEntry = null;
 
-    public void startElement(String name, AttributeList attributes)
-        throws SAXException
+    public void startElement(String uri, String localName, String qName,
+        Attributes attributes) throws SAXException
     {
-        if (name.equals("files")) {
+        if (localName.equals("files")) {
             String version = attributes.getValue("version");
             if (!version.equals(getVersion()))
                 throw new SAXException("Unknown file history format");
-        } else if (name.equals("file")) {
+        } else if (localName.equals("file")) {
             // Start a new entry.
             currentEntry = new FileHistoryEntry();
-            currentEntry.setName(attributes.getValue("name"));
-            currentEntry.setEncoding(attributes.getValue("encoding"));
-            currentEntry.setMode(attributes.getValue("mode"));
+            currentEntry.setName(attributes.getValue("", "name"));
+            currentEntry.setEncoding(attributes.getValue("", "encoding"));
+            currentEntry.setMode(attributes.getValue("", "mode"));
             try {
                 currentEntry.setWhen(Long.parseLong(attributes.getValue("when")));
             }
             catch (NumberFormatException e) {
                 Log.error(e);
             }
-        } else if (name.equals("property")) {
+        } else if (localName.equals("property")) {
             Debug.assertTrue(currentEntry != null);
-            String key = attributes.getValue("name");
-            String value = attributes.getValue("value");
+            String key = attributes.getValue("", "name");
+            String value = attributes.getValue("", "value");
             Property property = Property.findProperty(key);
             if (property != null)
                 currentEntry.setPropertyFromString(property, value);
         }
     }
 
-    public void endElement(String name)
+    public void endElement(String uri, String localName, String qName)
     {
-        if (name.equals("file")) {
+        if (localName.equals("file")) {
             list.add(currentEntry);
             currentEntry = null;
         }
