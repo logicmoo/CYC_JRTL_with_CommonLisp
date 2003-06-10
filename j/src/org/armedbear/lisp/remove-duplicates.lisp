92c1599fde967e15fb354109158681c6b55cb479
Peter Graves <gnooth@gmail.com>
2003-06-10 15:26:57 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/remove-duplicates.lisp
@@ -0,0 +1,99 @@
+;;; remove-duplicates.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: remove-duplicates.lisp,v 1.1 2003-06-10 15:22:04 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(export 'remove-duplicates)
+
+;;; From CMUCL.
+
+(defun list-remove-duplicates* (list test test-not start end key from-end)
+  (let* ((result (list ()))
+	 (splice result)
+	 (current list))
+    (do ((index 0 (1+ index)))
+      ((= index start))
+      (setq splice (cdr (rplacd splice (list (car current)))))
+      (setq current (cdr current)))
+    (do ((index 0 (1+ index)))
+      ((or (and end (= index end))
+           (atom current)))
+      (if (or (and from-end
+		   (not (member (apply-key key (car current))
+				(nthcdr (1+ start) result)
+				:test test
+				:test-not test-not
+				:key key)))
+	      (and (not from-end)
+		   (not (do ((it (apply-key key (car current)))
+			     (l (cdr current) (cdr l))
+			     (i (1+ index) (1+ i)))
+                          ((or (atom l) (and end (= i end)))
+                           ())
+			  (if (if test-not
+				  (not (funcall test-not it (apply-key key (car l))))
+				  (funcall test it (apply-key key (car l))))
+			      (return t))))))
+	  (setq splice (cdr (rplacd splice (list (car current))))))
+      (setq current (cdr current)))
+    (do ()
+      ((atom current))
+      (setq splice (cdr (rplacd splice (list (car current)))))
+      (setq current (cdr current)))
+    (cdr result)))
+
+
+(defun vector-remove-duplicates* (vector test test-not start end key from-end
+					 &optional (length (length vector)))
+  (when (null end) (setf end (length vector)))
+  (let ((result (make-sequence-like vector length))
+	(index 0)
+	(jndex start))
+    (do ()
+      ((= index start))
+      (setf (aref result index) (aref vector index))
+      (setq index (1+ index)))
+    (do ((elt))
+      ((= index end))
+      (setq elt (aref vector index))
+      (unless (or (and from-end
+                       (position (apply-key key elt) result :start start
+                                 :end jndex :test test :test-not test-not :key key))
+		  (and (not from-end)
+                       (position (apply-key key elt) vector :start (1+ index)
+                                 :end end :test test :test-not test-not :key key)))
+	(setf (aref result jndex) elt)
+	(setq jndex (1+ jndex)))
+      (setq index (1+ index)))
+    (do ()
+      ((= index length))
+      (setf (aref result jndex) (aref vector index))
+      (setq index (1+ index))
+      (setq jndex (1+ jndex)))
+    (shrink-vector result jndex)))
+
+
+(defun remove-duplicates (sequence &key (test #'eql) test-not (start 0) from-end
+				   end key)
+  (if (listp sequence)
+      (if sequence
+          (list-remove-duplicates* sequence test test-not
+                                   start end key from-end))
+      (vector-remove-duplicates* sequence test test-not
+                                 start end key from-end)))
---
+++ j/src/org/armedbear/lisp/remove.lisp
@@ -0,0 +1,181 @@
+;;; remove.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: remove.lisp,v 1.1 2003-06-10 15:26:57 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(export '(remove remove-if remove-if-not))
+
+;;; From CMUCL.
+
+(defmacro real-count (count)
+  `(cond ((null ,count) most-positive-fixnum)
+         ((fixnump ,count) (if (minusp ,count) 0 ,count))
+         ((integerp ,count) (if (minusp ,count) 0 most-positive-fixnum))
+         (t ,count)))
+
+(defmacro mumble-remove-macro (bump left begin finish right pred)
+  `(do ((index ,begin (,bump index))
+        (result
+         (do ((index ,left (,bump index))
+              (result (make-sequence-like sequence length)))
+           ((= index ,begin) result)
+           (setf (aref result index) (aref sequence index))))
+        (new-index ,begin)
+        (number-zapped 0)
+        (this-element))
+     ((or (= index ,finish) (= number-zapped count))
+      (do ((index index (,bump index))
+           (new-index new-index (,bump new-index)))
+        ((= index ,right) (shrink-vector result new-index))
+        (setf (aref result new-index) (aref sequence index))))
+     (setq this-element (aref sequence index))
+     (cond (,pred (setq number-zapped (1+ number-zapped)))
+           (t (setf (aref result new-index) this-element)
+              (setq new-index (,bump new-index))))))
+
+(defmacro mumble-remove (pred)
+  `(mumble-remove-macro 1+ 0 start end length ,pred))
+
+(defmacro mumble-remove-from-end (pred)
+  `(let ((sequence (copy-seq sequence)))
+     (mumble-delete-from-end ,pred)))
+
+(defmacro normal-mumble-remove ()
+  `(mumble-remove
+    (if test-not
+        (not (funcall test-not item (apply-key key this-element)))
+        (funcall test item (apply-key key this-element)))))
+
+(defmacro normal-mumble-remove-from-end ()
+  `(mumble-remove-from-end
+    (if test-not
+        (not (funcall test-not item (apply-key key this-element)))
+        (funcall test item (apply-key key this-element)))))
+
+(defmacro if-mumble-remove ()
+  `(mumble-remove (funcall predicate (apply-key key this-element))))
+
+(defmacro if-mumble-remove-from-end ()
+  `(mumble-remove-from-end (funcall predicate (apply-key key this-element))))
+
+(defmacro if-not-mumble-remove ()
+  `(mumble-remove (not (funcall predicate (apply-key key this-element)))))
+
+(defmacro if-not-mumble-remove-from-end ()
+  `(mumble-remove-from-end
+    (not (funcall predicate (apply-key key this-element)))))
+
+(defmacro list-remove-macro (pred reverse-p)
+  `(let* ((sequence ,(if reverse-p
+                         '(reverse sequence)
+                         'sequence))
+          (%start ,(if reverse-p '(- length end) 'start))
+          (%end ,(if reverse-p '(- length start) 'end))
+          (splice (list nil))
+          (results (do ((index 0 (1+ index))
+                        (before-start splice))
+                     ((= index %start) before-start)
+                     (setq splice
+                           (cdr (rplacd splice (list (pop sequence))))))))
+     (do ((index %start (1+ index))
+          (this-element)
+          (number-zapped 0))
+       ((or (= index %end) (= number-zapped count))
+        (do ((index index (1+ index)))
+          ((null sequence)
+           ,(if reverse-p
+                '(nreverse (cdr results))
+                '(cdr results)))
+          (setq splice (cdr (rplacd splice (list (pop sequence)))))))
+       (setq this-element (pop sequence))
+       (if ,pred
+           (setq number-zapped (1+ number-zapped))
+           (setq splice (cdr (rplacd splice (list this-element))))))))
+
+
+(defmacro list-remove (pred)
+  `(list-remove-macro ,pred nil))
+
+(defmacro list-remove-from-end (pred)
+  `(list-remove-macro ,pred t))
+
+(defmacro normal-list-remove ()
+  `(list-remove
+    (if test-not
+        (not (funcall test-not item (apply-key key this-element)))
+        (funcall test item (apply-key key this-element)))))
+
+(defmacro normal-list-remove-from-end ()
+  `(list-remove-from-end
+    (if test-not
+        (not (funcall test-not item (apply-key key this-element)))
+        (funcall test item (apply-key key this-element)))))
+
+(defmacro if-list-remove ()
+  `(list-remove
+    (funcall predicate (apply-key key this-element))))
+
+(defmacro if-list-remove-from-end ()
+  `(list-remove-from-end
+    (funcall predicate (apply-key key this-element))))
+
+(defmacro if-not-list-remove ()
+  `(list-remove
+    (not (funcall predicate (apply-key key this-element)))))
+
+(defmacro if-not-list-remove-from-end ()
+  `(list-remove-from-end
+    (not (funcall predicate (apply-key key this-element)))))
+
+(defun remove (item sequence &key from-end (test #'eql) test-not (start 0)
+                    end count key)
+  (let* ((length (length sequence))
+	 (end (or end length))
+	 (count (real-count count)))
+    (if (listp sequence)
+        (if from-end
+            (normal-list-remove-from-end)
+            (normal-list-remove))
+        (if from-end
+            (normal-mumble-remove-from-end)
+            (normal-mumble-remove)))))
+
+(defun remove-if (predicate sequence &key from-end (start 0) end count key)
+  (let* ((length (length sequence))
+	 (end (or end length))
+	 (count (real-count count)))
+    (if (listp sequence)
+        (if from-end
+            (if-list-remove-from-end)
+            (if-list-remove))
+        (if from-end
+            (if-mumble-remove-from-end)
+            (if-mumble-remove)))))
+
+(defun remove-if-not (predicate sequence &key from-end (start 0) end count key)
+  (let* ((length (length sequence))
+	 (end (or end length))
+	 (count (real-count count)))
+    (if (listp sequence)
+        (if from-end
+            (if-not-list-remove-from-end)
+            (if-not-list-remove))
+        (if from-end
+            (if-not-mumble-remove-from-end)
+            (if-not-mumble-remove)))))
