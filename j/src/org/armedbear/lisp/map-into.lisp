2628ad6199965cb44d880ac21a088468e050d5a7
Peter Graves <gnooth@gmail.com>
2003-06-10 19:03:50 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/map-into.lisp
@@ -0,0 +1,44 @@
+;;; map-into.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: map-into.lisp,v 1.1 2003-06-10 19:03:50 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(export 'map-into)
+
+;;; MAP-INTO (from CMUCL)
+
+(defun map-into (result-sequence function &rest sequences)
+  (let* ((fp-result
+	  (and (arrayp result-sequence)
+	       (array-has-fill-pointer-p result-sequence)))
+	 (len (apply #'min
+		     (if fp-result
+			 (array-dimension result-sequence 0)
+			 (length result-sequence))
+		     (mapcar #'length sequences))))
+
+    (when fp-result
+      (setf (fill-pointer result-sequence) len))
+
+    (dotimes (index len)
+      (setf (elt result-sequence index)
+	    (apply function
+		   (mapcar #'(lambda (seq) (elt seq index))
+			   sequences)))))
+  result-sequence)
---
+++ j/src/org/armedbear/lisp/map.lisp
@@ -0,0 +1,41 @@
+;;; map.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: map.lisp,v 1.1 2003-06-10 19:03:50 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(export 'map)
+
+;;; MAP (from ECL)
+
+(defun map (result-type function sequence &rest more-sequences)
+  (setq more-sequences (cons sequence more-sequences))
+  (let ((l (apply #'min (mapcar #'length more-sequences))))
+    (if (null result-type)
+        (do ((i 0 (1+ i))
+             (l l))
+            ((>= i l) nil)
+          (apply function (mapcar #'(lambda (z) (elt z i))
+                                  more-sequences)))
+        (let ((x (make-sequence result-type l)))
+          (do ((i 0 (1+ i))
+               (l l))
+              ((>= i l) x)
+            (setf (elt x i)
+                  (apply function (mapcar #'(lambda (z) (elt z i))
+                                          more-sequences))))))))
