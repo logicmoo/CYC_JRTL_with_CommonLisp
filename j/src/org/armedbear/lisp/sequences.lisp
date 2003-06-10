39e9e1f0c35b89013f80df756f8b1196ed9c3d47
Peter Graves <gnooth@gmail.com>
2003-06-10 01:19:15 +0000

    Autoload SUBSTITUTE, SUBSTITUTE-IF, SUBSTITUTE-IF-NOT.

--- j/src/org/armedbear/lisp/sequences.lisp
+++ j/src/org/armedbear/lisp/sequences.lisp
@@ -1,7 +1,7 @@
 ;;; sequences.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: sequences.lisp,v 1.38 2003-06-10 00:48:46 piso Exp $
+;;; $Id: sequences.lisp,v 1.39 2003-06-10 01:19:15 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -576,120 +576,9 @@
                 (vector-delete-duplicates* sequence test test-not key from-end start end)))
 
 
-(defun list-substitute* (pred new list start end count key test test-not old)
-  (let* ((result (list nil))
-	 elt
-	 (splice result)
-	 (list list))           ; Get a local list for a stepper.
-    (do ((index 0 (1+ index)))
-      ((= index start))
-      (setq splice (cdr (rplacd splice (list (car list)))))
-      (setq list (cdr list)))
-    (do ((index start (1+ index)))
-      ((or (= index end) (null list) (= count 0)))
-      (setq elt (car list))
-      (setq splice
-	    (cdr (rplacd splice
-			 (list
-			  (cond
-			   ((case pred
-                              (normal
-                               (if test-not
-                                   (not
-                                    (funcall test-not old (apply-key key elt)))
-                                   (funcall test old (apply-key key elt))))
-                              (if (funcall test (apply-key key elt)))
-                              (if-not (not (funcall test (apply-key key elt)))))
-			    (setq count (1- count))
-			    new)
-                           (t elt))))))
-      (setq list (cdr list)))
-    (do ()
-      ((null list))
-      (setq splice (cdr (rplacd splice (list (car list)))))
-      (setq list (cdr list)))
-    (cdr result)))
-
-
-;;; Replace old with new in sequence moving from left to right by incrementer
-;;; on each pass through the loop. Called by all three substitute functions.
-(defun vector-substitute* (pred new sequence incrementer left right length
-                                start end count key test test-not old)
-  (let ((result (make-sequence-like sequence length))
-	(index left))
-    (do ()
-      ((= index start))
-      (setf (aref result index) (aref sequence index))
-      (setq index (+ index incrementer)))
-    (do ((elt))
-      ((or (= index end) (= count 0)))
-      (setq elt (aref sequence index))
-      (setf (aref result index)
-	    (cond ((case pred
-                     (normal
-                      (if test-not
-                          (not (funcall test-not old (apply-key key elt)))
-                          (funcall test old (apply-key key elt))))
-                     (if (funcall test (apply-key key elt)))
-                     (if-not (not (funcall test (apply-key key elt)))))
-		   (setq count (1- count))
-		   new)
-		  (t elt)))
-      (setq index (+ index incrementer)))
-    (do ()
-      ((= index right))
-      (setf (aref result index) (aref sequence index))
-      (setq index (+ index incrementer)))
-    result))
-
-(defmacro subst-dispatch (pred)
-  `(if (listp sequence)
-       (if from-end
-           (nreverse (list-substitute* ,pred new (reverse sequence)
-                                       (- length end)
-                                       (- length start)
-                                       count key test test-not old))
-           (list-substitute* ,pred new sequence start end count key test test-not
-                             old))
-       (if from-end
-           (vector-substitute* ,pred new sequence -1 (1- length)
-                               -1 length (1- end)
-                               (1- start) count key test test-not old)
-           (vector-substitute* ,pred new sequence 1 0 length length
-                               start end count key test test-not old))))
-
-
-;;; SUBSTITUTE (from CMUCL)
-
-(defun substitute (new old sequence &key from-end (test #'eql) test-not
-                       (start 0) count end key)
-  (let* ((length (length sequence))
-	 (end (or end length))
-	 (count (real-count count)))
-    (subst-dispatch 'normal)))
-
-
-;;; SUBSTITUTE-IF (from CMUCL)
-
-(defun substitute-if (new test sequence &key from-end (start 0) end count key)
-  (let* ((length (length sequence))
-	 (end (or end length))
-	 (count (real-count count))
-	 test-not
-	 old)
-    (subst-dispatch 'if)))
-
-
-;;; SUBSTITUTE-IF-NOT (from CMUCL)
-
-(defun substitute-if-not (new test sequence &key from-end (start 0)
-                              end count key)
-  (let* ((length (length sequence))
-	 (end (or end length))
-	 (count (real-count count))
-	 test-not
-	 old)
-    (subst-dispatch 'if-not)))
+(autoload 'substitute "substitute.lisp")
+(autoload 'substitute-if "substitute.lisp")
+(autoload 'substitute-if-not "substitute.lisp")
 
 
 ;;; NSUBSTITUTE (from CMUCL)
