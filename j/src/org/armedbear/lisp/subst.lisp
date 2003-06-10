2a1119bc18803e9ef73de8db244bf19fd7de3233
Peter Graves <gnooth@gmail.com>
2003-06-10 17:12:48 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/subst.lisp
@@ -0,0 +1,112 @@
+;;; subst.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: subst.lisp,v 1.1 2003-06-10 17:12:48 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(export '(subst subst-if subst-if-not nsubst nsubst-if nsubst-if-not))
+
+;;; From CMUCL.
+
+(defmacro satisfies-the-test (item elt)
+  (let ((key-tmp (gensym)))
+    `(let ((,key-tmp (apply-key key ,elt)))
+       (cond (testp (funcall test ,item ,key-tmp))
+             (notp (not (funcall test-not ,item ,key-tmp)))
+             (t (funcall test ,item ,key-tmp))))))
+
+(defun subst (new old tree &key key (test #'eql testp) (test-not nil notp))
+  (labels ((s (subtree)
+	      (cond ((satisfies-the-test old subtree) new)
+		    ((atom subtree) subtree)
+		    (t (let ((car (s (car subtree)))
+			     (cdr (s (cdr subtree))))
+			 (if (and (eq car (car subtree))
+				  (eq cdr (cdr subtree)))
+			     subtree
+			     (cons car cdr)))))))
+          (s tree)))
+
+(defun subst-if (new test tree &key key)
+  (labels ((s (subtree)
+	      (cond ((funcall test (apply-key key subtree)) new)
+		    ((atom subtree) subtree)
+		    (t (let ((car (s (car subtree)))
+			     (cdr (s (cdr subtree))))
+			 (if (and (eq car (car subtree))
+				  (eq cdr (cdr subtree)))
+			     subtree
+			     (cons car cdr)))))))
+          (s tree)))
+
+(defun subst-if-not (new test tree &key key)
+  (labels ((s (subtree)
+	      (cond ((not (funcall test (apply-key key subtree))) new)
+		    ((atom subtree) subtree)
+		    (t (let ((car (s (car subtree)))
+			     (cdr (s (cdr subtree))))
+			 (if (and (eq car (car subtree))
+				  (eq cdr (cdr subtree)))
+			     subtree
+			     (cons car cdr)))))))
+          (s tree)))
+
+(defun nsubst (new old tree &key key (test #'eql testp) (test-not nil notp))
+  (labels ((s (subtree)
+	      (cond ((satisfies-the-test old subtree) new)
+		    ((atom subtree) subtree)
+		    (t (do* ((last nil subtree)
+			     (subtree subtree (cdr subtree)))
+                            ((atom subtree)
+                             (if (satisfies-the-test old subtree)
+                                 (setf (cdr last) new)))
+			 (if (satisfies-the-test old subtree)
+			     (return (setf (cdr last) new))
+			     (setf (car subtree) (s (car subtree)))))
+		       subtree))))
+          (s tree)))
+
+(defun nsubst-if (new test tree &key key)
+  (labels ((s (subtree)
+	      (cond ((funcall test (apply-key key subtree)) new)
+		    ((atom subtree) subtree)
+		    (t (do* ((last nil subtree)
+			     (subtree subtree (cdr subtree)))
+                            ((atom subtree)
+                             (if (funcall test (apply-key key subtree))
+                                 (setf (cdr last) new)))
+			 (if (funcall test (apply-key key subtree))
+			     (return (setf (cdr last) new))
+			     (setf (car subtree) (s (car subtree)))))
+		       subtree))))
+          (s tree)))
+
+(defun nsubst-if-not (new test tree &key key)
+  (labels ((s (subtree)
+	      (cond ((not (funcall test (apply-key key subtree))) new)
+		    ((atom subtree) subtree)
+		    (t (do* ((last nil subtree)
+			     (subtree subtree (cdr subtree)))
+                            ((atom subtree)
+                             (if (not (funcall test (apply-key key subtree)))
+                                 (setf (cdr last) new)))
+			 (if (not (funcall test (apply-key key subtree)))
+			     (return (setf (cdr last) new))
+			     (setf (car subtree) (s (car subtree)))))
+		       subtree))))
+          (s tree)))
