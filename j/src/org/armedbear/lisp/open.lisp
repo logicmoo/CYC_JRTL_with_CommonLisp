7221a9519c2f89062d46a68ff080ce9323c880b7
Peter Graves <gnooth@gmail.com>
2003-06-10 17:54:32 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/open.lisp
@@ -0,0 +1,36 @@
+;;; open.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: open.lisp,v 1.1 2003-06-10 17:54:32 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(export 'open)
+
+(defun open (filename
+	     &key
+	     (direction :input)
+	     (element-type 'base-char)
+	     (if-exists nil if-exists-given)
+	     (if-does-not-exist nil if-does-not-exist-given)
+	     (external-format :default))
+  (cond ((eq direction :input)
+         (%open-input-file filename element-type))
+        ((eq direction :output)
+         (%open-output-file filename element-type if-exists))
+        (t
+         (error "operation not supported"))))
