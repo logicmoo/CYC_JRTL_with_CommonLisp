cc20c6163477d58f06444de3d4005bbc2614dafc
Peter Graves <gnooth@gmail.com>
2003-06-10 17:24:25 +0000

    Autoload ASSOC, ASSOC-IF, ASSOC-IF-NOT, RASSOC, RASSOC-IF, RASSOC-IF-NOT.

--- j/src/org/armedbear/lisp/list.lisp
+++ j/src/org/armedbear/lisp/list.lisp
@@ -1,7 +1,7 @@
 ;;; list.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: list.lisp,v 1.31 2003-06-10 17:13:59 piso Exp $
+;;; $Id: list.lisp,v 1.32 2003-06-10 17:24:25 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -399,62 +399,12 @@
 	(error "the lists of keys and data are of unequal length"))
     (setq alist (acons (car x) (car y) alist))))
 
-(defmacro assoc-guts (test-guy)
-  `(do ((alist alist (cdr alist)))
-       ((endp alist))
-     (if (car alist)
-	 (if ,test-guy (return (car alist))))))
-
-(defun assoc (item alist &key key test test-not)
-  (cond (test
-	 (if key
-	     (assoc-guts (funcall test item (funcall key (caar alist))))
-	     (assoc-guts (funcall test item (caar alist)))))
-	(test-not
-	 (if key
-	     (assoc-guts (not (funcall test-not item
-				       (funcall key (caar alist)))))
-	     (assoc-guts (not (funcall test-not item (caar alist))))))
-	(t
-	 (if key
-	     (assoc-guts (eql item (funcall key (caar alist))))
-	     (assoc-guts (eql item (caar alist)))))))
-
-(defun assoc-if (predicate alist &key key)
-  (if key
-      (assoc-guts (funcall predicate (funcall key (caar alist))))
-      (assoc-guts (funcall predicate (caar alist)))))
-
-(defun assoc-if-not (predicate alist &key key)
-  (if key
-      (assoc-guts (not (funcall predicate (funcall key (caar alist)))))
-      (assoc-guts (not (funcall predicate (caar alist))))))
-
-(defun rassoc (item alist &key key test test-not)
-  (cond (test
-	 (if key
-	     (assoc-guts (funcall test item (funcall key (cdar alist))))
-	     (assoc-guts (funcall test item (cdar alist)))))
-	(test-not
-	 (if key
-	     (assoc-guts (not (funcall test-not item
-				       (funcall key (cdar alist)))))
-	     (assoc-guts (not (funcall test-not item (cdar alist))))))
-	(t
-	 (if key
-	     (assoc-guts (eql item (funcall key (cdar alist))))
-	     (assoc-guts (eql item (cdar alist)))))))
-
-(defun rassoc-if (predicate alist &key key)
-  (if key
-      (assoc-guts (funcall predicate (funcall key (cdar alist))))
-      (assoc-guts (funcall predicate (cdar alist)))))
-
-(defun rassoc-if-not (predicate alist &key key)
-  (if key
-      (assoc-guts (not (funcall predicate (funcall key (cdar alist)))))
-      (assoc-guts (not (funcall predicate (cdar alist))))))
-
+(autoload 'assoc "assoc.lisp")
+(autoload 'assoc-if "assoc.lisp")
+(autoload 'assoc-if-not "assoc.lisp")
+(autoload 'rassoc "assoc.lisp")
+(autoload 'rassoc-if "assoc.lisp")
+(autoload 'rassoc-if-not "assoc.lisp")
 
 ;;; Mapping functions (from CMUCL)
 
