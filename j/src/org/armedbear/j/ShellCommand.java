2b85b3fd043a8d98903cc11422083f7364d396e9
Peter Graves <gnooth@gmail.com>
2003-06-16 15:21:29 +0000

    Support working directory.

--- j/src/org/armedbear/j/ShellCommand.java
+++ j/src/org/armedbear/j/ShellCommand.java
@@ -1,8 +1,8 @@
 /*
  * ShellCommand.java
  *
- * Copyright (C) 2000-2002 Peter Graves
- * $Id: ShellCommand.java,v 1.3 2002-10-05 01:32:14 piso Exp $
+ * Copyright (C) 2000-2003 Peter Graves
+ * $Id: ShellCommand.java,v 1.4 2003-06-16 15:21:29 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -29,25 +29,32 @@ import java.util.ArrayList;
 
 public final class ShellCommand implements Runnable
 {
-    private String cmdline;
-    private String input;
-    private StringBuffer sb = new StringBuffer();
+    private final String cmdline;
+    private final File workingDirectory;
+    private final String input;
+    private final StringBuffer output = new StringBuffer();
     private int exitValue = -1;
 
     public ShellCommand(String cmdline)
     {
-        this.cmdline = cmdline;
+        this(cmdline, null, null);
     }
 
-    public ShellCommand(String cmdline, String input)
+    public ShellCommand(String cmdline, File workingDirectory)
+    {
+        this(cmdline, workingDirectory, null);
+    }
+
+    public ShellCommand(String cmdline, File workingDirectory, String input)
     {
         this.cmdline = cmdline;
+        this.workingDirectory = workingDirectory;
         this.input = input;
     }
 
     public final String getOutput()
     {
-        return sb.toString();
+        return output.toString();
     }
 
     public final int exitValue()
@@ -57,23 +64,38 @@ public final class ShellCommand implements Runnable
 
     private void appendOutput(String s)
     {
-        sb.append(s);
+        output.append(s);
     }
 
     public void run()
     {
         Process process = null;
-
         try {
             if (cmdline != null) {
                 if (Platform.isPlatformUnix()) {
-                    String[] cmdarray = {"/bin/sh", "-c", cmdline};
-                    process = Runtime.getRuntime().exec(cmdarray);
+                    if (workingDirectory != null) {
+                        FastStringBuffer sb = new FastStringBuffer("\\cd \"");
+                        sb.append(workingDirectory.canonicalPath());
+                        sb.append("\" && ");
+                        sb.append(cmdline);
+                        String[] cmdarray = {"/bin/sh", "-c", sb.toString()};
+                        process = Runtime.getRuntime().exec(cmdarray);
+                    } else {
+                        String[] cmdarray = {"/bin/sh", "-c", cmdline};
+                        process = Runtime.getRuntime().exec(cmdarray);
+                    }
                 } else if (Platform.isPlatformWindows()) {
                     ArrayList list = new ArrayList();
                     list.add("cmd.exe");
                     list.add("/c");
-                    list.addAll(Utilities.tokenize(cmdline));
+                    if (workingDirectory != null) {
+                        FastStringBuffer sb = new FastStringBuffer("cd /d \"");
+                        sb.append(workingDirectory.canonicalPath());
+                        sb.append("\" && ");
+                        sb.append(cmdline);
+                        list.addAll(Utilities.tokenize(sb.toString()));
+                    } else
+                        list.addAll(Utilities.tokenize(cmdline));
                     final int size = list.size();
                     String[] cmdarray = new String[size];
                     for (int i = 0; i < size; i++)
@@ -85,7 +107,6 @@ public final class ShellCommand implements Runnable
         catch (IOException e) {
             Log.error(e);
         }
-
         if (process != null) {
             ShellCommandReaderThread stdoutThread =
                 new ShellCommandReaderThread(process.getInputStream());
