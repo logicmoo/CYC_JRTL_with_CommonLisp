e649182f37a0ebcbe9786c2d5caeea15ebcf18bb
Peter Graves <gnooth@gmail.com>
2003-06-20 15:59:22 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/room.java
@@ -0,0 +1,67 @@
+/*
+ * room.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: room.java,v 1.1 2003-06-20 15:59:22 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public final class room extends Lisp
+{
+    private static final Primitive ROOM = new Primitive("room") {
+        public LispObject execute(LispObject[] args) throws Condition
+        {
+            Runtime runtime = Runtime.getRuntime();
+            long total = 0;
+            long free = 0;
+            long maxFree = 0;
+            while (true) {
+                try {
+                    runtime.gc();
+                    Thread.currentThread().sleep(100);
+                    runtime.runFinalization();
+                    Thread.currentThread().sleep(100);
+                    runtime.gc();
+                    Thread.currentThread().sleep(100);
+                }
+                catch (InterruptedException e) {}
+                total = runtime.totalMemory();
+                free = runtime.freeMemory();
+                if (free > maxFree)
+                    maxFree = free;
+                else
+                    break;
+            }
+            long used = total - free;
+            CharacterOutputStream out = getStandardOutput();
+            StringBuffer sb = new StringBuffer("Total memory ");
+            sb.append(total);
+            sb.append(" bytes");
+            sb.append(System.getProperty("line.separator"));
+            sb.append(used);
+            sb.append(" bytes used");
+            sb.append(System.getProperty("line.separator"));
+            sb.append(free);
+            sb.append(" bytes free");
+            sb.append(System.getProperty("line.separator"));
+            out.writeString(sb.toString());
+            out.finishOutput();
+            return number(used);
+        }
+    };
+}
