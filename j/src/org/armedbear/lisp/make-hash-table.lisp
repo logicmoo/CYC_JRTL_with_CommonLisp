c081324cac989662a0df00060634a7b98655b2f4
Peter Graves <gnooth@gmail.com>
2003-06-20 16:56:16 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/make-hash-table.lisp
@@ -0,0 +1,32 @@
+;;; make-hash-table.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: make-hash-table.lisp,v 1.1 2003-06-20 16:56:16 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(export 'make-hash-table)
+
+(defun make-hash-table (&key (test 'eql) (size 11) (rehash-size nil)
+			     (rehash-threshold nil))
+  (setq test (coerce test 'function))
+  (unless (and (typep size 'integer) (>= size 0))
+    (error 'type-error "MAKE-HASH-TABLE: ~S is not a non-negative integer" size))
+  ;; %make-hash-table expects size to be a fixnum.
+  (when (> size array-dimension-limit)
+    (setq size array-dimension-limit))
+  (%make-hash-table test size rehash-size rehash-threshold))
