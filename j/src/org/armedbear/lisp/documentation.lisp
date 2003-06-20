d3b1fa129d00efe76c98a35b6fb7231ea4c640da
Peter Graves <gnooth@gmail.com>
2003-06-20 16:40:34 +0000

    (defsetf documentation %set-documentation)

--- j/src/org/armedbear/lisp/documentation.lisp
+++ j/src/org/armedbear/lisp/documentation.lisp
@@ -1,4 +1,21 @@
 ;;; documentation.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: documentation.lisp,v 1.2 2003-06-20 16:40:34 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 (in-package "COMMON-LISP")
 
@@ -18,4 +35,4 @@
           (puthash symbol *documentation* (cons (cons type string) alist))
           string))))
 
-(provide "documentation")
+(defsetf documentation %set-documentation)
