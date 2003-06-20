e528d8f3d209357798ec765b3e10c28408728d4f
Peter Graves <gnooth@gmail.com>
2003-06-20 17:23:32 +0000

    Moved COERCE to coerce.java.

--- j/src/org/armedbear/lisp/Primitives.java
+++ j/src/org/armedbear/lisp/Primitives.java
@@ -2,7 +2,7 @@
  * Primitives.java
  *
  * Copyright (C) 2002-2003 Peter Graves
- * $Id: Primitives.java,v 1.241 2003-06-20 16:01:29 piso Exp $
+ * $Id: Primitives.java,v 1.242 2003-06-20 17:23:32 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -4068,107 +4068,6 @@ public final class Primitives extends Module
             if (args.length < 1)
                 throw new WrongNumberOfArgumentsException(this);
             return LispClass.findClass(checkSymbol(args[0]));
-        }
-    };
-
-    // ### coerce
-    // coerce object result-type => result
-    private static final Primitive2 COERCE = new Primitive2("coerce") {
-        public LispObject execute(LispObject first, LispObject second)
-            throws Condition
-        {
-            if (second == T)
-                return first;
-            if (first.typep(second) == T)
-                return first;
-            if (second == Symbol.CHARACTER) {
-                if (first instanceof LispString) {
-                    if (first.length() == 1)
-                        return ((LispString)first).get(0);
-                } else if (first instanceof Symbol) {
-                    String name = first.getName();
-                    if (name.length() == 1)
-                        return new LispCharacter(name.charAt(0));
-                }
-                throw new TypeError();
-            }
-            if (first instanceof AbstractVector) {
-                if (second == Symbol.BIT_VECTOR ||
-                    second == Symbol.SIMPLE_BIT_VECTOR) {
-                    AbstractVector v1 = (AbstractVector) first;
-                    BitVector v2 = new BitVector(v1.length());
-                    for (int i = v1.length(); i-- > 0;)
-                        v2.set(i, v1.get(i));
-                    return v2;
-                }
-                if (second == Symbol.SIMPLE_VECTOR) {
-                    AbstractVector v1 = (AbstractVector) first;
-                    Vector v2 = new Vector(v1.length());
-                    for (int i = v1.length(); i-- > 0;)
-                        v2.set(i, v1.get(i));
-                    return v2;
-                }
-                if (second == Symbol.LIST) {
-                    AbstractVector v = (AbstractVector) first;
-                    LispObject result = NIL;
-                    for (int i = first.length(); i-- > 0;)
-                        result = new Cons(v.get(i), result);
-                    return result;
-                }
-            } else if (first.listp()) {
-                if (second == Symbol.BIT_VECTOR ||
-                    second == Symbol.SIMPLE_BIT_VECTOR) {
-                    BitVector v = new BitVector(first.length());
-                    int i = 0;
-                    while (first != NIL) {
-                        v.set(i++, first.car());
-                        first = first.cdr();
-                    }
-                    return v;
-                }
-                if (second == Symbol.VECTOR ||
-                    second == Symbol.SIMPLE_VECTOR ||
-                    (second instanceof LispClass &&
-                        second.getName().equals("VECTOR"))) {
-                    Vector v = new Vector(first.length());
-                    int i = 0;
-                    while (first != NIL) {
-                        v.set(i++, first.car());
-                        first = first.cdr();
-                    }
-                    return v;
-                }
-                if (second == Symbol.STRING ||
-                    second == Symbol.SIMPLE_STRING ||
-                    second == Symbol.BASE_STRING ||
-                    second == Symbol.SIMPLE_BASE_STRING) {
-                    LispString string = new LispString(first.length());
-                    int i = 0;
-                    while (first != NIL) {
-                        string.set(i++, first.car());
-                        first = first.cdr();
-                    }
-                    return string;
-                }
-                if (second == Symbol.FUNCTION) {
-                    if (first.car() == Symbol.LAMBDA)
-                        return new Closure(first.cadr(), first.cddr(),
-                            new Environment());
-                }
-            } else if (first instanceof Symbol) {
-                if (second == Symbol.FUNCTION) {
-                    LispObject obj = first.getSymbolFunction();
-                    if (obj instanceof Function) {
-                        if (obj instanceof SpecialOperator)
-                            throw new TypeError();
-                        return obj;
-                    }
-                }
-            }
-            if (second == Symbol.FLOAT || second == Symbol.SINGLE_FLOAT) {
-                return LispFloat.coerceToFloat(first);
-            }
-            throw new TypeError();
         }
     };
 
