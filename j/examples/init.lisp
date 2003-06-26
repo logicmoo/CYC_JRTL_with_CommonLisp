da36dbacc5923d825d12e2e26263de7f77180fc0
Peter Graves <gnooth@gmail.com>
2003-06-26 00:48:23 +0000

    Special case handling for IBM Java 1.4.0/1.4.1.

--- j/examples/init.lisp
+++ j/examples/init.lisp
@@ -1,5 +1,5 @@
 ;;; init.lisp
-;;; $Id: init.lisp,v 1.7 2003-06-25 17:42:21 piso Exp $
+;;; $Id: init.lisp,v 1.8 2003-06-26 00:48:23 piso Exp $
 
 ;;; ~/.j/init.lisp (if it exists) is loaded automatically when j starts up.
 
@@ -29,15 +29,17 @@
       (load-theme "Kodiak")
       (load-theme "AnokhaClassic")))
 
-(defun java-version ()
-  (jstatic "getProperty" "java.lang.System" "java.version"))
+(defun java-vendor ()
+  (jstatic "getProperty" "java.lang.System" "java.vendor"))
 
 (defun java-version ()
   (jstatic "getProperty" "java.lang.System" "java.version"))
 
 (when (member (subseq (java-version) 0 5) '("1.4.0" "1.4.1" "1.4.2") :test #'string=)
-  (set-global-property "adjustAscent" "-1")
-  (set-global-property "adjustLeading" "-2")
+  (set-global-property "adjustAscent"
+                       (if (search "IBM" (java-vendor) :test #'char-equal)
+                           -2 -1))
+  (set-global-property "adjustLeading" -2)
   (reset-display))
 
 (defun reset-incoming-filters ()
