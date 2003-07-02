2bf8dccd1f6e6e545ec0a5ca89756e09d9ca531e
Peter Graves <gnooth@gmail.com>
2003-07-02 16:54:53 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/autoloads.lisp
@@ -0,0 +1,72 @@
+;;; autoloads.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: autoloads.lisp,v 1.1 2003-07-02 16:54:53 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "COMMON-LISP")
+
+(autoload '(digit-char-p alphanumericp
+            char/= char< char> char<= char>=
+            char-not-equal)
+          "chars.lisp")
+(autoload '(string-upcase string-downcase string-capitalize
+            nstring-upcase nstring-downcase nstring-capitalize
+            string= string/= string-equal string-not-equal
+            string< string>
+            string<= string>=
+            string-lessp string-greaterp
+            string-not-lessp string-not-greaterp)
+          "strings.lisp")
+(autoload '(remprop get-properties copy-symbol) "symbol.lisp")
+(autoload '(open sort merge parse-integer))
+(autoload 'tree-equal)
+(autoload 'make-hash-table)
+(autoload 'documentation)
+(autoload 'list-length)
+(autoload 'copy-list)
+(autoload 'revappend)
+(autoload '(butlast nbutlast) "butlast.lisp")
+(autoload 'ldiff)
+(autoload '(subst subst-it subst-if-not nsubst nsubst-if nsubst-if-not)
+          "subst.lisp")
+(autoload '(sublis nsublis) "sublis.lisp")
+(autoload '(member-if member-if-not) "member-if.lisp")
+(autoload 'tailp)
+(autoload 'adjoin)
+(autoload '(union nunion
+            intersection nintersection
+            set-difference nset-difference
+            set-exclusive-or nset-exclusive-or
+            subsetp)
+          "sets.lisp")
+(autoload '(assoc assoc-if assoc-if-not rassoc rassoc-if rassoc-if-not
+            acons pairlis copy-alist)
+          "assoc.lisp")
+(autoload '(mapc mapcan mapl maplist mapcon) "map1.lisp")
+(autoload 'make-sequence)
+(autoload '(copy-seq fill replace))
+(autoload '(map map-into))
+(autoload 'reduce)
+(autoload '(delete delete-if delete-if-not) "delete.lisp")
+(autoload '(remove remove-if remove-if-not) "remove.lisp")
+(autoload '(remove-duplicates delete-duplicates))
+(autoload '(substitute substitute-if substitute-if-not) "substitute.lisp")
+(autoload '(nsubstitute nsubstitute-if nsubstitute-if-not) "nsubstitute.lisp")
+(autoload '(position position-if position-if-not find find-if find-if-not)
+          "find.lisp")
+(autoload '(count count-if count-if-not) "count.lisp")
+(autoload '(mismatch search))
