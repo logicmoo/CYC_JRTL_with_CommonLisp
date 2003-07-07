040617f3344593495162c92042ac0a6e5ebaea15
Peter Graves <gnooth@gmail.com>
2003-07-07 13:01:40 +0000

    getAccessibleSymbols()

--- j/src/org/armedbear/lisp/Package.java
+++ j/src/org/armedbear/lisp/Package.java
@@ -2,7 +2,7 @@
  * Package.java
  *
  * Copyright (C) 2002-2003 Peter Graves
- * $Id: Package.java,v 1.30 2003-07-07 02:13:34 piso Exp $
+ * $Id: Package.java,v 1.31 2003-07-07 13:01:40 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -510,6 +510,23 @@ public final class Package extends LispObject
         for (Iterator it = externalSymbols.values().iterator(); it.hasNext();) {
             Symbol symbol = (Symbol) it.next();
             list.add(symbol);
+        }
+        return list;
+    }
+
+    public synchronized List getAccessibleSymbols()
+    {
+        ArrayList list = new ArrayList();
+        for (Iterator it = externalSymbols.values().iterator(); it.hasNext();) {
+            Symbol symbol = (Symbol) it.next();
+            list.add(symbol);
+        }
+        for (Iterator packageIter = useList.iterator(); packageIter.hasNext();) {
+            Package pkg = (Package) packageIter.next();
+            for (Iterator it = pkg.externalSymbols.values().iterator(); it.hasNext();) {
+                Symbol symbol = (Symbol) it.next();
+                list.add(symbol);
+            }
         }
         return list;
     }
