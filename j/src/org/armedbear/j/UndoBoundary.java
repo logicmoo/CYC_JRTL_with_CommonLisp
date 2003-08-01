f6c5ba00ffd1bc50acd3b23d1906c8d621c1c186
Peter Graves <gnooth@gmail.com>
2003-08-01 16:04:23 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/j/UndoBoundary.java
@@ -0,0 +1,29 @@
+/*
+ * UndoBoundary.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: UndoBoundary.java,v 1.1 2003-08-01 16:04:23 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.j;
+
+import javax.swing.undo.AbstractUndoableEdit;
+
+// A marker class.
+public final class UndoBoundary extends AbstractUndoableEdit
+{
+}
