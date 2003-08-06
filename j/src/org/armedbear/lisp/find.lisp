8e67bc3e1c7a18b0cdb4f4ab725fdf4b071f6299
Peter Graves <gnooth@gmail.com>
2003-08-06 17:45:55 +0000

    JVM-COMPILE if available.

--- j/src/org/armedbear/lisp/find.lisp
+++ j/src/org/armedbear/lisp/find.lisp
@@ -1,7 +1,7 @@
 ;;; find.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: find.lisp,v 1.2 2003-07-02 17:57:53 piso Exp $
+;;; $Id: find.lisp,v 1.3 2003-08-06 17:44:27 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -208,3 +208,15 @@
         (list-find-if-not test sequence)
         (vector-find-if-not test sequence))))
 
+(when (and (find-package "JVM")
+           (fboundp 'jvm:jvm-compile))
+  (mapcar #'jvm:jvm-compile '(position
+                              list-position*
+                              vector-position*
+                              position-if
+                              position-if-not
+                              find
+                              list-find*
+                              vector-find*
+                              find-if
+                              find-if-not)))
