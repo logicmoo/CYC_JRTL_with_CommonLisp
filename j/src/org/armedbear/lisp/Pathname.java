386319d2ad6bbcf2e8d371ffc580868562dd49fc
Peter Graves <gnooth@gmail.com>
2003-08-10 04:27:50 +0000

    PATHNAMEP

--- j/src/org/armedbear/lisp/Pathname.java
+++ j/src/org/armedbear/lisp/Pathname.java
@@ -2,7 +2,7 @@
  * Pathname.java
  *
  * Copyright (C) 2003 Peter Graves
- * $Id: Pathname.java,v 1.4 2003-08-10 01:10:29 piso Exp $
+ * $Id: Pathname.java,v 1.5 2003-08-10 04:27:50 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -129,6 +129,14 @@ public final class Pathname extends LispObject
                 }
             }
             throw new LispError("MAKE-PATHNAME: not implemented");
+        }
+    };
+
+    // ### pathnamep
+    private static final Primitive1 PATHNAMEP = new Primitive1("pathnamep") {
+        public LispObject execute(LispObject arg) throws Condition
+        {
+            return arg instanceof Pathname ? T : NIL;
         }
     };
 }
