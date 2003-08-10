42a90488b4f0ea9f3561112ccb315750eab5c304
Peter Graves <gnooth@gmail.com>
2003-08-10 16:37:44 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/collect.lisp
@@ -0,0 +1,106 @@
+;;; collect.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: collect.lisp,v 1.1 2003-08-10 16:37:44 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "EXT")
+
+(export '(collect))
+
+;;; From CMUCL.
+
+;;;; The Collect macro:
+
+;;; Collect-Normal-Expander  --  Internal
+;;;
+;;;    This function does the real work of macroexpansion for normal collection
+;;; macros.  N-Value is the name of the variable which holds the current
+;;; value.  Fun is the function which does collection.  Forms is the list of
+;;; forms whose values we are supposed to collect.
+;;;
+(defun collect-normal-expander (n-value fun forms)
+  `(progn
+    ,@(mapcar #'(lambda (form) `(setq ,n-value (,fun ,form ,n-value))) forms)
+    ,n-value))
+
+;;; Collect-List-Expander  --  Internal
+;;;
+;;;    This function deals with the list collection case.  N-Tail is the pointer
+;;; to the current tail of the list, which is NIL if the list is empty.
+;;;
+(defun collect-list-expander (n-value n-tail forms)
+  (let ((n-res (gensym)))
+    `(progn
+      ,@(mapcar #'(lambda (form)
+		    `(let ((,n-res (cons ,form nil)))
+		       (cond (,n-tail
+			      (setf (cdr ,n-tail) ,n-res)
+			      (setq ,n-tail ,n-res))
+			     (t
+			      (setq ,n-tail ,n-res  ,n-value ,n-res)))))
+		forms)
+      ,n-value)))
+
+
+;;; Collect  --  Public
+;;;
+;;;    The ultimate collection macro...
+;;;
+(defmacro collect (collections &body body)
+  "Collect ({(Name [Initial-Value] [Function])}*) {Form}*
+  Collect some values somehow.  Each of the collections specifies a bunch of
+  things which collected during the evaluation of the body of the form.  The
+  name of the collection is used to define a local macro, a la MACROLET.
+  Within the body, this macro will evaluate each of its arguments and collect
+  the result, returning the current value after the collection is done.  The
+  body is evaluated as a PROGN; to get the final values when you are done, just
+  call the collection macro with no arguments.
+
+  Initial-Value is the value that the collection starts out with, which
+  defaults to NIL.  Function is the function which does the collection.  It is
+  a function which will accept two arguments: the value to be collected and the
+  current collection.  The result of the function is made the new value for the
+  collection.  As a totally magical special-case, the Function may be Collect,
+  which tells us to build a list in forward order; this is the default.  If an
+  Initial-Value is supplied for Collect, the stuff will be rplacd'd onto the
+  end.  Note that Function may be anything that can appear in the functional
+  position, including macros and lambdas."
+
+  (let ((macros ())
+	(binds ()))
+    (dolist (spec collections)
+      (unless (<= 1 (length spec) 3)
+	(error "Malformed collection specifier: ~S." spec))
+      (let ((n-value (gensym))
+	    (name (first spec))
+	    (default (second spec))
+	    (kind (or (third spec) 'collect)))
+	(push `(,n-value ,default) binds)
+	(if (eq kind 'collect)
+	    (let ((n-tail (gensym)))
+	      (if default
+		  (push `(,n-tail (last ,n-value)) binds)
+		  (push n-tail binds))
+	      (push `(,name (&rest args)
+			    (collect-list-expander ',n-value ',n-tail args))
+		    macros))
+	    (push `(,name (&rest args)
+			  (collect-normal-expander ',n-value ',kind args))
+		  macros))))
+    `(macrolet ,macros (let* ,(nreverse binds) ,@body))))
+
+(provide :collect)
