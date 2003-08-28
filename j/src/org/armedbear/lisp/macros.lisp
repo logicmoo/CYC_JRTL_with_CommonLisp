5b6f1e5b88707b5bf90b5c21802f77bef58f11d8
Peter Graves <gnooth@gmail.com>
2003-08-28 00:23:05 +0000

    RETURN

--- j/src/org/armedbear/lisp/macros.lisp
+++ j/src/org/armedbear/lisp/macros.lisp
@@ -1,6 +1,26 @@
 ;;; macros.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: macros.lisp,v 1.21 2003-08-28 00:23:05 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 (in-package "COMMON-LISP")
+
+(defmacro return (&optional (value nil))
+  `(return-from nil ,value))
 
 (defmacro prog1 (first-form &rest forms)
   (let ((result (gensym)))
