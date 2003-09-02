c2ce60a875b0866815869f954d18f9f8e2d2e533
Peter Graves <gnooth@gmail.com>
2003-09-02 20:02:44 +0000

    multiplyBy()

--- j/src/org/armedbear/lisp/Complex.java
+++ j/src/org/armedbear/lisp/Complex.java
@@ -2,7 +2,7 @@
  * Complex.java
  *
  * Copyright (C) 2003 Peter Graves
- * $Id: Complex.java,v 1.12 2003-08-26 17:17:35 piso Exp $
+ * $Id: Complex.java,v 1.13 2003-09-02 20:02:44 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -122,6 +122,22 @@ public final class Complex extends LispObject
             return getInstance(realpart.subtract(c.realpart), imagpart.subtract(c.imagpart));
         }
         return getInstance(realpart.subtract(obj), imagpart);
+    }
+
+    public LispObject multiplyBy(LispObject obj) throws LispError
+    {
+        if (obj instanceof Complex) {
+            LispObject a = realpart;
+            LispObject b = imagpart;
+            LispObject c = ((Complex)obj).getRealPart();
+            LispObject d = ((Complex)obj).getImaginaryPart();
+            LispObject ac = a.multiplyBy(c);
+            LispObject bd = b.multiplyBy(d);
+            return Complex.getInstance(ac.subtract(bd),
+                                       a.add(b).multiplyBy(c.add(d)).subtract(ac).subtract(bd));
+        }
+        return Complex.getInstance(realpart.multiplyBy(obj),
+                                   imagpart.multiplyBy(obj));
     }
 
     public boolean isEqualTo(LispObject obj) throws LispError
