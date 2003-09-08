190f504ccea23e5fbf8d7f724a96cd61485ce488
Peter Graves <gnooth@gmail.com>
2003-09-08 13:35:25 +0000

    PARSE-INTEGER-ERROR

--- j/src/org/armedbear/lisp/parse-integer.lisp
+++ j/src/org/armedbear/lisp/parse-integer.lisp
@@ -1,7 +1,7 @@
 ;;; parse-integer.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: parse-integer.lisp,v 1.3 2003-09-08 01:15:56 piso Exp $
+;;; $Id: parse-integer.lisp,v 1.4 2003-09-08 13:35:25 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -21,6 +21,9 @@
 
 ;;; From OpenMCL.
 
+(defun parse-integer-error (string)
+  (error 'parse-error "not an integer string: ~S" string))
+
 (defun parse-integer (string &key (start 0) end
                              (radix 10) junk-allowed)
   (when (null end)
@@ -29,7 +32,7 @@
                    ((= i end)
                     (if junk-allowed
                         (return-from parse-integer (values nil end))
-                        (error "not an integer string: ~S" string)))
+                        (parse-integer-error string)))
                  (unless (whitespacep (char string i)) (return i))))
         (minusp nil)
         (found-digit nil)
@@ -51,15 +54,15 @@
               ((whitespacep char)
                (do () ((= (setq index (1+ index)) end))
                  (unless (whitespacep (char string index))
-                   (error "not an integer string: ~S" string)))
+                   (parse-integer-error string)))
                (return nil))
               (t
-               (error "not an integer string: ~S" string))))
+               (parse-integer-error string))))
       (setq index (1+ index)))
     (values
      (if found-digit
          (if minusp (- result) result)
          (if junk-allowed
              nil
-             (error "not an integer string: ~S" string)))
+             (parse-integer-error string)))
      index)))
