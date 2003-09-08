ee36fb74b145581ba6d7617457687f4f4915e3ad
Peter Graves <gnooth@gmail.com>
2003-09-08 02:36:25 +0000

    complex complexp

--- j/src/org/armedbear/lisp/rt.lisp
+++ j/src/org/armedbear/lisp/rt.lisp
@@ -1,7 +1,7 @@
 ;;; rt.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: rt.lisp,v 1.116 2003-09-08 02:21:49 piso Exp $
+;;; $Id: rt.lisp,v 1.117 2003-09-08 02:36:25 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -175,6 +175,8 @@
                               "cl-symbols"
                               "coerce"
                               "complement"
+                              "complex"
+                              "complexp"
                               "concatenate"
                               "cond"
                               "cons"
