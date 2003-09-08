a089df3479d77fb27ded74349c74c27b08c43186
Peter Graves <gnooth@gmail.com>
2003-09-08 02:23:01 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/atan.java
@@ -0,0 +1,43 @@
+/*
+ * atan.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: atan.java,v 1.1 2003-09-08 02:23:01 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public final class atan extends Lisp
+{
+    // ### atan
+    private static final Primitive ATAN = new Primitive("atan") {
+        public LispObject execute(LispObject arg) throws LispError
+        {
+            LispFloat n = LispFloat.coerceToFloat(arg);
+            return new LispFloat(Math.atan(n.getValue()));
+        }
+
+        public LispObject execute(LispObject first, LispObject second)
+            throws LispError
+        {
+            LispFloat n1 = LispFloat.coerceToFloat(first);
+            LispFloat n2 = LispFloat.coerceToFloat(second);
+            LispObject quotient = n1.divideBy(n2);
+            return new LispFloat(Math.atan(((LispFloat)quotient).getValue()));
+        }
+    };
+}
