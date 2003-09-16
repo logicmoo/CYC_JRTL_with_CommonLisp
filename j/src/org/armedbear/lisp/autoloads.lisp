5bd7ddb5175532c3f86451d907592b56e2832747
Peter Graves <gnooth@gmail.com>
2003-09-16 16:52:38 +0000

    (autoload-macro 'remf)

--- j/src/org/armedbear/lisp/autoloads.lisp
+++ j/src/org/armedbear/lisp/autoloads.lisp
@@ -1,7 +1,7 @@
 ;;; autoloads.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: autoloads.lisp,v 1.25 2003-09-16 16:28:15 piso Exp $
+;;; $Id: autoloads.lisp,v 1.26 2003-09-16 16:52:38 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -83,3 +83,4 @@
 
 (autoload-macro 'with-hash-table-iterator)
 (autoload-macro 'with-package-iterator)
+(autoload-macro 'remf)
