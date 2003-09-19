f6487f4ab023851a4d9739fda1cb0c52cf9b22e2
Peter Graves <gnooth@gmail.com>
2003-09-19 00:42:26 +0000

    Work in progress.

--- j/src/org/armedbear/lisp/ConditionThrowable.java
+++ j/src/org/armedbear/lisp/ConditionThrowable.java
@@ -2,7 +2,7 @@
  * ConditionThrowable.java
  *
  * Copyright (C) 2003 Peter Graves
- * $Id: ConditionThrowable.java,v 1.4 2003-09-19 00:36:58 piso Exp $
+ * $Id: ConditionThrowable.java,v 1.5 2003-09-19 00:42:26 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -32,7 +32,7 @@ public class ConditionThrowable extends Throwable
         super(message);
     }
 
-    public LispObject getCondition()
+    public Condition getCondition()
     {
         return new Condition();
     }
