8ffdf525e77e4d4e0975962631c15bf73dd6fdf9
Peter Graves <gnooth@gmail.com>
2003-09-21 13:00:34 +0000

    typep()

--- j/src/org/armedbear/lisp/UnboundVariable.java
+++ j/src/org/armedbear/lisp/UnboundVariable.java
@@ -2,7 +2,7 @@
  * UnboundVariable.java
  *
  * Copyright (C) 2002-2003 Peter Graves
- * $Id: UnboundVariable.java,v 1.3 2003-09-21 01:56:58 piso Exp $
+ * $Id: UnboundVariable.java,v 1.4 2003-09-21 13:00:34 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -47,6 +47,8 @@ public final class UnboundVariable extends CellError
     public LispObject typep(LispObject type) throws ConditionThrowable
     {
         if (type == Symbol.UNBOUND_VARIABLE)
+            return T;
+        if (type == BuiltInClass.UNBOUND_VARIABLE)
             return T;
         return super.typep(type);
     }
