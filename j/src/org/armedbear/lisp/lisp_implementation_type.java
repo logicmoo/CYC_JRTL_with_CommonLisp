3c3a262335ad885fddaa7e56c1d44f3112c80cdd
Peter Graves <gnooth@gmail.com>
2003-09-29 16:58:09 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/lisp_implementation_type.java
@@ -0,0 +1,42 @@
+/*
+ * lisp_implementation_type.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: lisp_implementation_type.java,v 1.1 2003-09-29 16:58:09 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+import java.math.BigInteger;
+
+// ### lisp_implementation_type
+// lisp_implementation_type <no arguments> => description
+public final class lisp_implementation_type extends Primitive0
+{
+    private lisp_implementation_type()
+    {
+        super("lisp-implementation-type");
+    }
+
+    public LispObject execute()
+    {
+        return new LispString("Armed Bear Lisp");
+    }
+
+    private static final lisp_implementation_type LISP_IMPLEMENTATION_TYPE =
+        new lisp_implementation_type();
+}
