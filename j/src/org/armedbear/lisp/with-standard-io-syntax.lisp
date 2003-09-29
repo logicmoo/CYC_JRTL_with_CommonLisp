5d0bb1a7ee29aa5cc214e4b6247bb183ce71dd83
Peter Graves <gnooth@gmail.com>
2003-09-29 18:10:36 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/with-standard-io-syntax.lisp
@@ -0,0 +1,69 @@
+;;; with-standard-io-syntax.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: with-standard-io-syntax.lisp,v 1.1 2003-09-29 18:10:36 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+;;; From CMUCL.
+
+(defun %with-standard-io-syntax (function)
+  (let ((*package* (find-package "CL-USER"))
+        (*print-array* t)
+        (*print-base* 10)
+        (*print-case* :upcase)
+        (*print-circle* nil)
+        (*print-escape* t)
+        (*print-gensym* t)
+        (*print-length* nil)
+        (*print-level* nil)
+        (*print-lines* nil)
+        (*print-miser-width* nil)
+        (*print-pretty* nil)
+        (*print-radix* nil)
+        (*print-readably* t)
+        (*print-right-margin* nil)
+        (*read-base* 10)
+        (*read-default-float-format* 'single-float)
+        (*read-eval* t)
+        (*read-suppress* nil)
+        #+nil ; FIXME
+        (*readtable* std-lisp-readtable))
+    (funcall function)))
+
+(defmacro with-standard-io-syntax (&body body)
+  "Bind the reader and printer control variables to values that enable READ
+   to reliably read the results of PRINT.  These values are:
+   *PACKAGE*                            The COMMON-LISP-USER package
+   *PRINT-ARRAY*                        T
+   *PRINT-BASE*                         10
+   *PRINT-CASE*                         :UPCASE
+   *PRINT-CIRCLE*                       NIL
+   *PRINT-ESCAPE*                       T
+   *PRINT-GENSYM*                       T
+   *PRINT-LENGTH*                       NIL
+   *PRINT-LEVEL*                        NIL
+   *PRINT-LINES*                        NIL
+   *PRINT-MISER-WIDTH*                  NIL
+   *PRINT-PRETTY*                       NIL
+   *PRINT-RADIX*                        NIL
+   *PRINT-READABLY*                     T
+   *PRINT-RIGHT-MARGIN*                 NIL
+   *READ-BASE*                          10
+   *READ-DEFAULT-FLOAT-FORMAT*          SINGLE-FLOAT
+   *READ-EVAL*                          T
+   *READ-SUPPRESS*                      NIL
+   *READTABLE*                          the standard readtable."
+  `(%with-standard-io-syntax #'(lambda () ,@body)))
