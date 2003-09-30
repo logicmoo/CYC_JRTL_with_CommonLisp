ad18980b935159bb6735238241239020129355c3
Peter Graves <gnooth@gmail.com>
2003-09-30 11:11:36 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/do-all-symbols.lisp
@@ -0,0 +1,36 @@
+;;; do-all-symbols.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: do-all-symbols.lisp,v 1.1 2003-09-30 11:11:36 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+;;; Adapted from CMUCL.
+
+(in-package "SYSTEM")
+
+(defmacro do-all-symbols ((var &optional result-form) &body body)
+  (let ((flet-name (gensym "DO-SYMBOLS-")))
+    `(block nil
+       (flet ((,flet-name (,var)
+                          (tagbody ,@body)))
+         (dolist (package (list-all-packages))
+           (flet ((iterate-over-symbols (symbols)
+                                        (dolist (symbol symbols)
+                                          (,flet-name symbol))))
+             (iterate-over-symbols (package-internal-symbols package))
+             (iterate-over-symbols (package-external-symbols package)))))
+       (let ((,var nil))
+         ,result-form))))
