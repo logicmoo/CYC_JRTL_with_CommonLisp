ea65835076826227ba26b4ac509fd38b449c74dd
Peter Graves <gnooth@gmail.com>
2003-10-07 17:21:07 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/gentemp.lisp
@@ -0,0 +1,32 @@
+;;; gentemp.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: gentemp.lisp,v 1.1 2003-10-07 17:21:07 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+;;; Adapted from CMUCL.
+
+(in-package "SYSTEM")
+
+(defvar *gentemp-counter* 0)
+
+(defun gentemp (&optional (prefix "T") (package *package*))
+  (require-type prefix 'string)
+  (loop
+    (let ((name (format nil "~A~D" prefix (incf *gentemp-counter*))))
+      (multiple-value-bind (symbol exists-p) (find-symbol name package)
+	(unless exists-p
+          (return (values (intern name package))))))))
