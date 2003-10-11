e50759c06a7ca10ebb8f8027f9c912e1ee390041
Peter Graves <gnooth@gmail.com>
2003-10-11 17:29:53 +0000

    STANDARD_GENERIC_FUNCTION

--- j/src/org/armedbear/lisp/BuiltInClass.java
+++ j/src/org/armedbear/lisp/BuiltInClass.java
@@ -2,7 +2,7 @@
  * BuiltInClass.java
  *
  * Copyright (C) 2003 Peter Graves
- * $Id: BuiltInClass.java,v 1.7 2003-10-11 02:07:28 piso Exp $
+ * $Id: BuiltInClass.java,v 1.8 2003-10-11 17:29:53 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -120,7 +120,7 @@ public class BuiltInClass extends LispClass
     public static final BuiltInClass SIMPLE_TYPE_ERROR                = addClass(Symbol.SIMPLE_TYPE_ERROR);
     public static final BuiltInClass SIMPLE_WARNING                   = addClass(Symbol.SIMPLE_WARNING);
     public static final BuiltInClass STANDARD_CLASS                   = addClass(Symbol.STANDARD_CLASS);
-    public static final BuiltInClass STANDARD_GENERIC_FUNCTION        = addClass(Symbol.STANDARD_GENERIC_FUNCTION);
+//     public static final BuiltInClass STANDARD_GENERIC_FUNCTION        = addClass(Symbol.STANDARD_GENERIC_FUNCTION);
     public static final BuiltInClass STANDARD_METHOD                  = addClass(Symbol.STANDARD_METHOD);
 //     public static final BuiltInClass STANDARD_OBJECT                  = addClass(Symbol.STANDARD_OBJECT);
     public static final BuiltInClass STORAGE_CONDITION                = addClass(Symbol.STORAGE_CONDITION);
@@ -285,9 +285,9 @@ public class BuiltInClass extends LispClass
                               CONDITION, CLASS_T);
         STANDARD_CLASS.setDirectSuperclass(CLASS);
         STANDARD_CLASS.setCPL(STANDARD_CLASS, CLASS, STANDARD_OBJECT, CLASS_T);
-        STANDARD_GENERIC_FUNCTION.setDirectSuperclass(GENERIC_FUNCTION);
-        STANDARD_GENERIC_FUNCTION.setCPL(STANDARD_GENERIC_FUNCTION,
-                                         GENERIC_FUNCTION, FUNCTION, CLASS_T);
+//         STANDARD_GENERIC_FUNCTION.setDirectSuperclass(GENERIC_FUNCTION);
+//         STANDARD_GENERIC_FUNCTION.setCPL(STANDARD_GENERIC_FUNCTION,
+//                                          GENERIC_FUNCTION, FUNCTION, CLASS_T);
         STANDARD_METHOD.setDirectSuperclass(list2(METHOD, STANDARD_OBJECT));
         STANDARD_METHOD.setCPL(STANDARD_METHOD, METHOD, STANDARD_OBJECT,
                                CLASS_T);
