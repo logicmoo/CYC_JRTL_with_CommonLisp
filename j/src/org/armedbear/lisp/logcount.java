deee4bf11b6bfa31b2c265159a142a587b973a48
Peter Graves <gnooth@gmail.com>
2003-10-16 01:59:32 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/logcount.java
@@ -0,0 +1,49 @@
+/*
+ * logcount.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: logcount.java,v 1.1 2003-10-16 01:59:32 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+import java.math.BigInteger;
+
+// ### logcount
+// logcount integer => number-of-on-bits
+public final class logcount extends Primitive1
+{
+    private logcount()
+    {
+        super("logcount");
+    }
+
+    // FIXME Optimize fixnum case!
+    public LispObject execute(LispObject arg) throws ConditionThrowable
+    {
+        BigInteger n;
+        if (arg instanceof Fixnum)
+            n = ((Fixnum)arg).getBigInteger();
+        else if (arg instanceof Bignum)
+            n = ((Bignum)arg).getValue();
+        else
+            throw new ConditionThrowable(new TypeError(arg, "integer"));
+        return new Fixnum(n.bitCount());
+    }
+
+    private static final logcount LOGCOUNT = new logcount();
+}
