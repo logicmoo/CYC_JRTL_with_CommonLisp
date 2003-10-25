973f08228c6b2282ea26e9dbbe6920cea62e8ca1
Peter Graves <gnooth@gmail.com>
2003-10-25 21:43:20 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/SymbolMacro.java
@@ -0,0 +1,47 @@
+/*
+ * SymbolMacro.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: SymbolMacro.java,v 1.1 2003-10-25 21:43:20 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public final class SymbolMacro extends LispObject
+{
+    private LispObject expansion;
+
+    private SymbolMacro(LispObject expansion)
+    {
+        this.expansion = expansion;
+    }
+
+    public LispObject getExpansion()
+    {
+        return expansion;
+    }
+
+    // ### make-symbol-macro
+    private static final Primitive1 MAKE_MACRO =
+        new Primitive1("make-symbol-macro", PACKAGE_SYS, false)
+    {
+        public LispObject execute(LispObject arg) throws ConditionThrowable
+        {
+            return new SymbolMacro(arg);
+        }
+    };
+}
