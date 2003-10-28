0a52f0767c7a4d7d8ea925972148fcda92c082fd
Peter Graves <gnooth@gmail.com>
2003-10-28 23:25:51 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/mask-field.lisp
@@ -0,0 +1,38 @@
+;;; mask-field.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: mask-field.lisp,v 1.1 2003-10-28 23:25:51 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+;;; From CMUCL.
+
+(defun mask-field (bytespec integer)
+  (let ((size (byte-size bytespec))
+        (pos (byte-position bytespec)))
+    (logand integer (ash (1- (ash 1 size)) pos))))
+
+(define-setf-expander mask-field (bytespec place &environment env)
+  (multiple-value-bind (dummies vals newval setter getter)
+    (get-setf-expansion place env)
+    (let ((btemp (gensym))
+	  (gnuval (gensym)))
+      (values (cons btemp dummies)
+	      (cons bytespec vals)
+	      (list gnuval)
+	      `(let ((,(car newval) (deposit-field ,gnuval ,btemp ,getter)))
+		 ,setter
+		 ,gnuval)
+	      `(mask-field ,btemp ,getter)))))
