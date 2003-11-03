ff72c44b0d5c3511e3f8b7b27a24e90c68880f3a
Peter Graves <gnooth@gmail.com>
2003-11-03 03:01:15 +0000

    Moved PRINT-UNREADABLE-OBJECT to print-unreadable-object.lisp.

--- j/src/org/armedbear/lisp/boot.lisp
+++ j/src/org/armedbear/lisp/boot.lisp
@@ -1,7 +1,7 @@
 ;;; boot.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: boot.lisp,v 1.122 2003-10-28 02:40:42 piso Exp $
+;;; $Id: boot.lisp,v 1.123 2003-11-03 03:01:15 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -390,23 +390,6 @@
           (progn ,@forms)
           (close ,var))
 	 (get-output-stream-string ,var))))
-
-
-(defmacro print-unreadable-object ((object stream &key type identity) &body body)
-  `(let ((s ,stream)
-         (obj ,object))
-     (format s "#<")
-     ,(when type
-        '(format s "~S" (type-of obj)))
-     ,(when (and type (or body identity))
-        '(format s " "))
-     ,@body
-     ,(when (and identity body)
-        '(format s " "))
-     ,(when identity
-        '(format s "@ #x~A" (sys::hashcode-to-string obj)))
-     (format s ">")
-     nil))
 
 
 ;;; MULTIPLE-VALUE-BIND (from CLISP)
