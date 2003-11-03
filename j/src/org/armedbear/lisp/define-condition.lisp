2f8dea31b84781bc5b71312f6c574b13412550d9
Peter Graves <gnooth@gmail.com>
2003-11-03 18:50:04 +0000

    Work in progress.

--- j/src/org/armedbear/lisp/define-condition.lisp
+++ j/src/org/armedbear/lisp/define-condition.lisp
@@ -1,7 +1,7 @@
 ;;; define-condition.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: define-condition.lisp,v 1.4 2003-11-02 00:22:37 piso Exp $
+;;; $Id: define-condition.lisp,v 1.5 2003-11-03 18:50:04 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -24,12 +24,9 @@
 (defmacro define-condition (name (&rest parent-types) (&rest slot-specs)
 				 &body options)
   (let ((parent-types (or parent-types '(condition))))
-    (setf parent-types (mapcar #'(lambda (sym) (find-class sym)) parent-types))
-    `(progn
-       (std-finalize-inheritance (%define-condition ',name ',parent-types
-                                                    ',slot-specs ',options))
+    `(progn (defclass ,name ,parent-types ,slot-specs ,@options)
        ',name)))
 
 (defun make-condition (type &rest initargs)
   (or (%make-condition type initargs)
-      (make-instance (find-class type) initargs)))
+      (apply #'make-instance (find-class type) initargs)))
