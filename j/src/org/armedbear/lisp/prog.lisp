7bee90c5f8aafe68fa460234b789a14bb7c164b5
Peter Graves <gnooth@gmail.com>
2003-11-07 19:14:59 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/prog.lisp
@@ -0,0 +1,40 @@
+;;; prog.lisp
+;;;
+;;; Copyright (C) 2003 Peter Graves
+;;; $Id: prog.lisp,v 1.1 2003-11-07 19:14:59 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+;;; From GCL.
+
+(in-package "COMMON-LISP")
+
+(defmacro prog (vl &rest body &aux (decl nil))
+  (do ()
+      ((or (endp body)
+           (not (consp (car body)))
+           (not (eq (caar body) 'declare)))
+       `(block nil (let ,vl ,@decl (tagbody ,@body))))
+    (push (car body) decl)
+    (pop body)))
+
+(defmacro prog* (vl &rest body &aux (decl nil))
+  (do ()
+      ((or (endp body)
+           (not (consp (car body)))
+           (not (eq (caar body) 'declare)))
+       `(block nil (let* ,vl ,@decl (tagbody ,@body))))
+    (push (car body) decl)
+    (pop body)))
