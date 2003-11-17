7777f7ae93883fd8ae8313dd6183b0e76afc314a
Peter Graves <gnooth@gmail.com>
2003-11-17 16:31:01 +0000

    TYPEP: (typep '(a b) '(cons *)) => T

--- j/src/org/armedbear/lisp/typep.lisp
+++ j/src/org/armedbear/lisp/typep.lisp
@@ -1,7 +1,7 @@
 ;;; typep.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: typep.lisp,v 1.11 2003-11-17 16:04:54 piso Exp $
+;;; $Id: typep.lisp,v 1.12 2003-11-17 16:31:01 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -73,8 +73,8 @@
       (MEMBER (member object i))
       (CONS
        (and (consp object)
-            (or (null (car i)) (typep (car object) (car i)))
-            (or (null (cadr i)) (typep (cdr object) (cadr i)))))
+            (or (null (car i)) (eq (car i) '*) (typep (car object) (car i)))
+            (or (null (cadr i)) (eq (cadr i) '*) (typep (cdr object) (cadr i)))))
       (INTEGER
        (and (integerp object) (in-interval-p object i)))
       (RATIONAL
