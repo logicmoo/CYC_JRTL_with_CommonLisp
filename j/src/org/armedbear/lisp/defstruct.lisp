fe55ecd9f84eb8ca2c1275ecdb668e27a3eb49e2
Peter Graves <gnooth@gmail.com>
2003-11-21 15:58:30 +0000

    Work in progress.

--- j/src/org/armedbear/lisp/defstruct.lisp
+++ j/src/org/armedbear/lisp/defstruct.lisp
@@ -1,7 +1,7 @@
 ;;; defstruct.lisp
 ;;;
 ;;; Copyright (C) 2003 Peter Graves
-;;; $Id: defstruct.lisp,v 1.38 2003-11-21 02:41:22 piso Exp $
+;;; $Id: defstruct.lisp,v 1.39 2003-11-21 15:58:30 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -19,20 +19,22 @@
 
 (in-package "SYSTEM")
 
-(defmacro ds-name (x)           `(aref ,x  0))
-(defmacro ds-conc-name (x)      `(aref ,x  1))
-(defmacro ds-constructors (x)   `(aref ,x  2))
-(defmacro ds-copier (x)         `(aref ,x  3))
-(defmacro ds-include (x)        `(aref ,x  4))
-(defmacro ds-type (x)           `(aref ,x  5))
-(defmacro ds-named (x)          `(aref ,x  6))
-(defmacro ds-initial-offset (x) `(aref ,x  7))
-(defmacro ds-predicate (x)      `(aref ,x  8))
-(defmacro ds-print-function (x) `(aref ,x  9))
-(defmacro ds-direct-slots (x)   `(aref ,x 10))
-(defmacro ds-slots (x)          `(aref ,x 11))
+;;; DEFSTRUCT-DESCRIPTION
 
-(defun make-defstruct-definition (&key name
+(defmacro dd-name (x)           `(aref ,x  0))
+(defmacro dd-conc-name (x)      `(aref ,x  1))
+(defmacro dd-constructors (x)   `(aref ,x  2))
+(defmacro dd-copier (x)         `(aref ,x  3))
+(defmacro dd-include (x)        `(aref ,x  4))
+(defmacro dd-type (x)           `(aref ,x  5))
+(defmacro dd-named (x)          `(aref ,x  6))
+(defmacro dd-initial-offset (x) `(aref ,x  7))
+(defmacro dd-predicate (x)      `(aref ,x  8))
+(defmacro dd-print-function (x) `(aref ,x  9))
+(defmacro dd-direct-slots (x)   `(aref ,x 10))
+(defmacro dd-slots (x)          `(aref ,x 11))
+
+(defun make-defstruct-description (&key name
                                        conc-name
                                        constructors
                                        copier
@@ -44,108 +46,123 @@
                                        print-function
                                        direct-slots
                                        slots)
-  (let ((def (make-array 12)))
-    (setf (ds-name def) name
-          (ds-conc-name def) conc-name
-          (ds-constructors def) constructors
-          (ds-copier def) copier
-          (ds-include def) include
-          (ds-type def) type
-          (ds-named def) named
-          (ds-initial-offset def) initial-offset
-          (ds-predicate def) predicate
-          (ds-print-function def) print-function
-          (ds-direct-slots def) direct-slots
-          (ds-slots def) slots)
-    def))
+  (let ((dd (make-array 12)))
+    (setf (dd-name dd) name
+          (dd-conc-name dd) conc-name
+          (dd-constructors dd) constructors
+          (dd-copier dd) copier
+          (dd-include dd) include
+          (dd-type dd) type
+          (dd-named dd) named
+          (dd-initial-offset dd) initial-offset
+          (dd-predicate dd) predicate
+          (dd-print-function dd) print-function
+          (dd-direct-slots dd) direct-slots
+          (dd-slots dd) slots)
+    dd))
 
-(defvar *ds-name*)
-(defvar *ds-conc-name*)
-(defvar *ds-constructors*)
-(defvar *ds-copier*)
-(defvar *ds-include*)
-(defvar *ds-type*)
-(defvar *ds-named*)
-(defvar *ds-initial-offset*)
-(defvar *ds-predicate*)
-(defvar *ds-print-function*)
-(defvar *ds-direct-slots*)
-(defvar *ds-slots*)
+;;; DEFSTRUCT-SLOT-DESCRIPTION
+
+(defmacro dsd-name (x)     `(aref ,x 0))
+(defmacro dsd-initform (x) `(aref ,x 1))
+(defmacro dsd-index (x)    `(aref ,x 2))
+
+(defun make-defstruct-slot-description (&key name
+                                             initform
+                                             index)
+  (let ((dsd (make-array 3)))
+    (setf (dsd-name dsd) name
+          (dsd-initform dsd) initform
+          (dsd-index dsd) index)
+    dsd))
+
+(defvar *dd-name*)
+(defvar *dd-conc-name*)
+(defvar *dd-constructors*)
+(defvar *dd-copier*)
+(defvar *dd-include*)
+(defvar *dd-type*)
+(defvar *dd-named*)
+(defvar *dd-initial-offset*)
+(defvar *dd-predicate*)
+(defvar *dd-print-function*)
+(defvar *dd-direct-slots*)
+(defvar *dd-slots*)
 
 (defun define-constructor (constructor)
   (let* ((constructor-name (intern (car constructor)))
          (keys ())
          (elements ()))
-    (dolist (slot *ds-slots*)
-      (let ((name (getf slot :name))
-            (initform (getf slot :initform)))
+    (dolist (slot *dd-slots*)
+      (let ((name (dsd-name slot))
+            (initform (dsd-initform slot)))
         (push (list name initform) keys)
         (push name elements)))
     (setf keys (cons '&key (nreverse keys)))
     (setf elements (nreverse elements))
-    (when *ds-named*
-      (push (list 'quote *ds-name*) elements))
-    (when *ds-initial-offset*
-      (dotimes (i *ds-initial-offset*)
+    (when *dd-named*
+      (push (list 'quote *dd-name*) elements))
+    (when *dd-initial-offset*
+      (dotimes (i *dd-initial-offset*)
         (push nil elements)))
-    (cond ((eq *ds-type* 'list)
+    (cond ((eq *dd-type* 'list)
            `((defun ,constructor-name ,keys
                (list ,@elements))))
-          ((or (eq *ds-type* 'vector)
-               (and (consp *ds-type*) (eq (car *ds-type*) 'vector)))
-           (let ((element-type (if (consp *ds-type*) (cadr *ds-type*) t)))
+          ((or (eq *dd-type* 'vector)
+               (and (consp *dd-type*) (eq (car *dd-type*) 'vector)))
+           (let ((element-type (if (consp *dd-type*) (cadr *dd-type*) t)))
              `((defun ,constructor-name ,keys
                  (make-array ,(length elements)
                              :element-type ',element-type
                              :initial-contents (list ,@elements))))))
           (t
            `((defun ,constructor-name ,keys
-               (%make-structure ',*ds-name* (list ,@elements))))))))
+               (%make-structure ',*dd-name* (list ,@elements))))))))
 
 (defun default-constructor-name ()
-  (concatenate 'string "MAKE-" (symbol-name *ds-name*)))
+  (concatenate 'string "MAKE-" (symbol-name *dd-name*)))
 
 (defun define-constructors ()
-  (if *ds-constructors*
+  (if *dd-constructors*
       (let ((results ()))
-        (dolist (constructor *ds-constructors*)
+        (dolist (constructor *dd-constructors*)
           (when (car constructor)
             (setf results (nconc results (define-constructor constructor)))))
         results)
       (define-constructor (cons (default-constructor-name) nil))))
 
 (defun define-predicate ()
-  (when (and *ds-predicate*
-             (or *ds-named* (null *ds-type*)))
-    (let ((pred (intern *ds-predicate*)))
-      (cond ((eq *ds-type* 'list)
-             (if *ds-initial-offset*
+  (when (and *dd-predicate*
+             (or *dd-named* (null *dd-type*)))
+    (let ((pred (intern *dd-predicate*)))
+      (cond ((eq *dd-type* 'list)
+             (if *dd-initial-offset*
                  `((defun ,pred (object)
                      (and (consp object)
-                          (> (length object) ,*ds-initial-offset*)
-                          (eq (elt object ,*ds-initial-offset*) ',*ds-name*))))
+                          (> (length object) ,*dd-initial-offset*)
+                          (eq (elt object ,*dd-initial-offset*) ',*dd-name*))))
                  `((defun ,pred (object)
-                     (and (consp object) (eq (car object) ',*ds-name*))))))
-            ((or (eq *ds-type* 'vector)
-                 (and (consp *ds-type*) (eq (car *ds-type*) 'vector)))
-             (let ((index (or *ds-initial-offset* 0)))
+                     (and (consp object) (eq (car object) ',*dd-name*))))))
+            ((or (eq *dd-type* 'vector)
+                 (and (consp *dd-type*) (eq (car *dd-type*) 'vector)))
+             (let ((index (or *dd-initial-offset* 0)))
                `((defun ,pred (object)
                    (and (vectorp object)
                         (> (length object) ,index)
-                        (eq (aref object ,index) ',*ds-name*))))))
+                        (eq (aref object ,index) ',*dd-name*))))))
             (t
              `((defun ,pred (object)
-                 (typep object ',*ds-name*))))))))
+                 (typep object ',*dd-name*))))))))
 
 (defun get-slot-accessor (slot)
-  (when *ds-initial-offset*
-    (incf slot *ds-initial-offset*))
-  (when *ds-named*
+  (when *dd-initial-offset*
+    (incf slot *dd-initial-offset*))
+  (when *dd-named*
     (incf slot))
-  (cond ((eq *ds-type* 'list)
+  (cond ((eq *dd-type* 'list)
          `(lambda (instance) (elt instance ,slot)))
-        ((or (eq *ds-type* 'vector)
-             (and (consp *ds-type*) (eq (car *ds-type*) 'vector)))
+        ((or (eq *dd-type* 'vector)
+             (and (consp *dd-type*) (eq (car *dd-type*) 'vector)))
          `(lambda (instance) (aref instance ,slot)))
         (t
          (case slot
@@ -156,14 +173,14 @@
             `(lambda (instance) (%structure-ref instance ,slot)))))))
 
 (defun get-slot-mutator (slot)
-  (when *ds-initial-offset*
-    (incf slot *ds-initial-offset*))
-  (when *ds-named*
+  (when *dd-initial-offset*
+    (incf slot *dd-initial-offset*))
+  (when *dd-named*
     (incf slot))
-  (cond ((eq *ds-type* 'list)
+  (cond ((eq *dd-type* 'list)
          `(lambda (instance value) (%set-elt instance ,slot value)))
-        ((or (eq *ds-type* 'vector)
-             (and (consp *ds-type*) (eq (car *ds-type*) 'vector)))
+        ((or (eq *dd-type* 'vector)
+             (and (consp *dd-type*) (eq (car *dd-type*) 'vector)))
          `(lambda (instance value) (%aset instance ,slot value)))
         (t
          (case slot
@@ -175,8 +192,8 @@
 
 (defun define-access-function (slot-name index)
   (let ((accessor
-         (if *ds-conc-name*
-             (intern (concatenate 'string (symbol-name *ds-conc-name*) (symbol-name slot-name)))
+         (if *dd-conc-name*
+             (intern (concatenate 'string (symbol-name *dd-conc-name*) (symbol-name slot-name)))
              slot-name)))
     `((setf (symbol-function ',accessor) ,(get-slot-accessor index))
       (%put ',accessor 'setf-inverse ,(get-slot-mutator index )))))
@@ -184,26 +201,27 @@
 (defun define-access-functions ()
   (let ((index 0)
         (result ()))
-    (dolist (slot *ds-slots*)
-      (let ((slot-name (getf slot :name)))
+    (dolist (slot *dd-slots*)
+;;       (let ((slot-name (getf slot :name)))
+      (let ((slot-name (dsd-name slot)))
         (setf result (nconc result (define-access-function slot-name index))))
       (incf index))
     result))
 
 (defun define-copier ()
-  (when *ds-copier*
-    (cond ((eq *ds-type* 'list)
-           `((setf (fdefinition ',*ds-copier*) #'copy-list)))
-          ((or (eq *ds-type* 'vector)
-               (and (consp *ds-type*) (eq (car *ds-type*) 'vector)))
-           `((setf (fdefinition ',*ds-copier*) #'copy-seq)))
+  (when *dd-copier*
+    (cond ((eq *dd-type* 'list)
+           `((setf (fdefinition ',*dd-copier*) #'copy-list)))
+          ((or (eq *dd-type* 'vector)
+               (and (consp *dd-type*) (eq (car *dd-type*) 'vector)))
+           `((setf (fdefinition ',*dd-copier*) #'copy-seq)))
           (t
-           `((setf (fdefinition ',*ds-copier*) #'copy-structure))))))
+           `((setf (fdefinition ',*dd-copier*) #'copy-structure))))))
 
 (defun parse-1-option (option)
   (case (car option)
     (:conc-name
-     (setf *ds-conc-name* (if (symbolp (cadr option))
+     (setf *dd-conc-name* (if (symbolp (cadr option))
                               (cadr option)
                               (make-symbol (string (cadr option))))))
     (:constructor
@@ -214,44 +232,44 @@
          (0 ; Use default name.
           (setf name (default-constructor-name))
           (setf arglist nil)
-          (push (list name arglist) *ds-constructors*))
+          (push (list name arglist) *dd-constructors*))
          (1
           (if (null (car args))
               (setf name nil) ; No constructor.
               (setf name (symbol-name (car args))))
           (setf arglist nil)
-          (push (list name arglist) *ds-constructors*))
+          (push (list name arglist) *dd-constructors*))
          (2))))
     (:copier
      (let* ((args (cdr option))
             (numargs (length args)))
        (when (= numargs 1)
-          (setf *ds-copier* (car args)))))
+          (setf *dd-copier* (car args)))))
     (:include
-     (setf *ds-include* (cdr option)))
+     (setf *dd-include* (cdr option)))
     (:initial-offset
-     (setf *ds-initial-offset* (cadr option)))
+     (setf *dd-initial-offset* (cadr option)))
     (:predicate
      (when (= (length option) 2)
        (if (null (cadr option))
-           (setf *ds-predicate* nil)
-           (setf *ds-predicate* (symbol-name (cadr option))))))
+           (setf *dd-predicate* nil)
+           (setf *dd-predicate* (symbol-name (cadr option))))))
     (:type
-     (setf *ds-type* (cadr option)))
+     (setf *dd-type* (cadr option)))
     (t
      (format t "unrecognized DEFSTRUCT option: ~S~%" (car option)))))
 
 (defun parse-name-and-options (name-and-options)
-  (setf *ds-name* (car name-and-options))
-  (setf *ds-conc-name* (make-symbol (concatenate 'string (symbol-name *ds-name*) "-")))
-  (setf *ds-copier* (intern (concatenate 'string "COPY-" (symbol-name *ds-name*))))
-  (setf *ds-predicate* (concatenate 'string (symbol-name *ds-name*) "-P"))
+  (setf *dd-name* (car name-and-options))
+  (setf *dd-conc-name* (make-symbol (concatenate 'string (symbol-name *dd-name*) "-")))
+  (setf *dd-copier* (intern (concatenate 'string "COPY-" (symbol-name *dd-name*))))
+  (setf *dd-predicate* (concatenate 'string (symbol-name *dd-name*) "-P"))
   (let ((options (cdr name-and-options)))
     (dolist (option options)
       (cond ((consp option)
              (parse-1-option option))
             ((eq option :named)
-             (setf *ds-named* t))
+             (setf *dd-named* t))
             ((member option '(:constructor :copier :predicate :named
                               :conc-name))
              (parse-1-option (list option)))
@@ -259,51 +277,55 @@
              (error "unrecognized DEFSTRUCT option: ~S" option))))))
 
 (defmacro defstruct (name-and-options &rest slots)
-  (let ((*ds-name* nil)
-        (*ds-conc-name* nil)
-        (*ds-constructors* nil)
-        (*ds-copier* nil)
-        (*ds-include* nil)
-        (*ds-type* nil)
-        (*ds-named* nil)
-        (*ds-initial-offset* nil)
-        (*ds-predicate* nil)
-        (*ds-print-function* nil)
-        (*ds-direct-slots* ())
-        (*ds-slots* ()))
+  (let ((*dd-name* nil)
+        (*dd-conc-name* nil)
+        (*dd-constructors* nil)
+        (*dd-copier* nil)
+        (*dd-include* nil)
+        (*dd-type* nil)
+        (*dd-named* nil)
+        (*dd-initial-offset* nil)
+        (*dd-predicate* nil)
+        (*dd-print-function* nil)
+        (*dd-direct-slots* ())
+        (*dd-slots* ()))
     (parse-name-and-options (if (atom name-and-options)
                                 (list name-and-options)
                                 name-and-options))
     (when (stringp (car slots))
-      (setf (documentation *ds-name* 'structure) (pop slots)))
+      (setf (documentation *dd-name* 'structure) (pop slots)))
     (dolist (slot slots)
       (let ((slot-description (if (atom slot)
-                                  (list :name slot :initform nil)
-                                  (list :name (car slot) :initform (cadr slot)))))
-        (push slot-description *ds-direct-slots*)))
-    (setf *ds-direct-slots* (nreverse *ds-direct-slots*))
-    (if *ds-include*
-        (let* ((def (get (car *ds-include*) 'structure-definition))
-               (included-slots (ds-slots def)))
-          (setf *ds-slots* (append included-slots *ds-direct-slots*)))
-        (setf *ds-slots* *ds-direct-slots*))
+                                  (make-defstruct-slot-description :name slot
+                                                                   :initform nil
+                                                                   :index 0)
+                                  (make-defstruct-slot-description :name (car slot)
+                                                                   :initform (cadr slot)
+                                                                   :index 0))))
+        (push slot-description *dd-direct-slots*)))
+    (setf *dd-direct-slots* (nreverse *dd-direct-slots*))
+    (if *dd-include*
+        (let* ((def (get (car *dd-include*) 'structure-definition))
+               (included-slots (dd-slots def)))
+          (setf *dd-slots* (append included-slots *dd-direct-slots*)))
+        (setf *dd-slots* *dd-direct-slots*))
     `(progn
-       (setf (get ',*ds-name* 'structure-definition)
-             (make-defstruct-definition :name ',*ds-name*
-                                        :conc-name ',*ds-conc-name*
-                                        :constructors ',*ds-constructors*
-                                        :copier ',*ds-copier*
-                                        :include ',*ds-include*
-                                        :type ',*ds-type*
-                                        :named ,*ds-named*
-                                        :initial-offset ,*ds-initial-offset*
-                                        :predicate ,*ds-predicate*
-                                        :print-function ,*ds-print-function*
-                                        :direct-slots ',*ds-direct-slots*
-                                        :slots ',*ds-slots*))
-       (make-structure-class ',*ds-name* ',*ds-direct-slots* ',*ds-slots*)
+       (setf (get ',*dd-name* 'structure-definition)
+             (make-defstruct-description :name ',*dd-name*
+                                         :conc-name ',*dd-conc-name*
+                                         :constructors ',*dd-constructors*
+                                         :copier ',*dd-copier*
+                                         :include ',*dd-include*
+                                         :type ',*dd-type*
+                                         :named ,*dd-named*
+                                         :initial-offset ,*dd-initial-offset*
+                                         :predicate ,*dd-predicate*
+                                         :print-function ,*dd-print-function*
+                                         :direct-slots ',*dd-direct-slots*
+                                         :slots ',*dd-slots*))
+       (make-structure-class ',*dd-name* ',*dd-direct-slots* ',*dd-slots*)
        ,@(define-constructors)
        ,@(define-predicate)
        ,@(define-access-functions)
        ,@(define-copier)
-       ',*ds-name*)))
+       ',*dd-name*)))
