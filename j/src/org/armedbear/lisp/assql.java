b632184a76a7a310da20a0da36fec4ae416ec622
Peter Graves <gnooth@gmail.com>
2003-11-23 18:56:40 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/assql.java
@@ -0,0 +1,48 @@
+/*
+ * assql.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: assql.java,v 1.1 2003-11-23 18:56:40 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+// ### assql item alist => entry
+public final class assql extends Primitive2
+{
+    private assql()
+    {
+        super("assql", PACKAGE_EXT);
+    }
+
+    public LispObject execute(LispObject item, LispObject alist)
+        throws ConditionThrowable
+    {
+        while (alist != NIL) {
+            LispObject cons = alist.car();
+            if (cons instanceof Cons) {
+                if (cons.car().eql(item))
+                    return cons;
+            } else if (cons != NIL)
+                throw new ConditionThrowable(new TypeError(cons, "list"));
+            alist = alist.cdr();
+        }
+        return NIL;
+    }
+
+    private static final assql ASSQL = new assql();
+}
