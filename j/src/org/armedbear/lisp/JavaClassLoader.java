dfc191fa968087e460aa69204777f04eae50c113
Peter Graves <gnooth@gmail.com>
2003-12-02 19:50:51 +0000

    loadClassFromFile(String) => loadClassFromFile(File)

--- j/src/org/armedbear/lisp/JavaClassLoader.java
+++ j/src/org/armedbear/lisp/JavaClassLoader.java
@@ -2,7 +2,7 @@
  * JavaClassLoader.java
  *
  * Copyright (C) 2003 Peter Graves
- * $Id: JavaClassLoader.java,v 1.1 2003-04-14 15:42:06 piso Exp $
+ * $Id: JavaClassLoader.java,v 1.2 2003-12-02 19:50:51 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -27,26 +27,22 @@ import java.io.FileInputStream;
 
 public class JavaClassLoader extends ClassLoader
 {
-    protected Class loadClassFromFile(String filename)
+    protected Class loadClassFromFile(File file)
     {
         try {
-            File file = new File(filename);
-            if (file.isFile()) {
-                long length = file.length();
-                if (length < Integer.MAX_VALUE) {
-                    byte[] classbytes = new byte[(int)length];
-                    DataInputStream in =
-                        new DataInputStream(new FileInputStream(file));
-                    in.readFully(classbytes);
-                    in.close();
-                    Class c = defineClass(null, classbytes, 0, (int) length);
-                    if (c != null) {
-                        resolveClass(c);
-                        return c;
-                    }
+            long length = file.length();
+            if (length < Integer.MAX_VALUE) {
+                byte[] classbytes = new byte[(int)length];
+                DataInputStream in =
+                    new DataInputStream(new FileInputStream(file));
+                in.readFully(classbytes);
+                in.close();
+                Class c = defineClass(null, classbytes, 0, (int) length);
+                if (c != null) {
+                    resolveClass(c);
+                    return c;
                 }
-            } else
-                Debug.trace("file not found: " + filename);
+            }
         }
         catch (Throwable t) {
             Debug.trace(t);
