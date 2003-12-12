d6f91d0e11dcef9216c4e40e08fa7a31ccdbd9a5
Peter Graves <gnooth@gmail.com>
2003-12-12 15:51:35 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/SimpleWarning.java
@@ -0,0 +1,53 @@
+/*
+ * SimpleWarning.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: SimpleWarning.java,v 1.1 2003-12-12 15:51:19 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public class SimpleWarning extends Warning
+{
+    public SimpleWarning(LispObject initArgs) throws ConditionThrowable
+    {
+        super(initArgs);
+    }
+
+    public LispObject typeOf()
+    {
+        return Symbol.SIMPLE_WARNING;
+    }
+
+    public LispClass classOf()
+    {
+        return BuiltInClass.SIMPLE_WARNING;
+    }
+
+    public LispObject typep(LispObject type) throws ConditionThrowable
+    {
+        if (type == Symbol.SIMPLE_WARNING)
+            return T;
+        if (type == BuiltInClass.SIMPLE_WARNING)
+            return T;
+        if (type == Symbol.SIMPLE_CONDITION)
+            return T;
+        if (type == BuiltInClass.SIMPLE_CONDITION)
+            return T;
+        return super.typep(type);
+    }
+}
---
+++ j/src/org/armedbear/lisp/Warning.java
@@ -0,0 +1,49 @@
+/*
+ * Warning.java
+ *
+ * Copyright (C) 2003 Peter Graves
+ * $Id: Warning.java,v 1.1 2003-12-12 15:51:35 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public class Warning extends Condition
+{
+    public Warning(LispObject initArgs) throws ConditionThrowable
+    {
+        super(initArgs);
+    }
+
+    public LispObject typeOf()
+    {
+        return Symbol.WARNING;
+    }
+
+    public LispClass classOf()
+    {
+        return BuiltInClass.WARNING;
+    }
+
+    public LispObject typep(LispObject type) throws ConditionThrowable
+    {
+        if (type == Symbol.WARNING)
+            return T;
+        if (type == BuiltInClass.WARNING)
+            return T;
+        return super.typep(type);
+    }
+}
