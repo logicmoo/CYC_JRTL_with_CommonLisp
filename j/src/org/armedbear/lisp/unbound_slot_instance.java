9433228c224468c2982cfa1c8ad5386e1b5c5998
Peter Graves <gnooth@gmail.com>
2004-01-02 01:19:12 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/unbound_slot_instance.java
@@ -0,0 +1,41 @@
+/*
+ * unbound_slot_instance.java
+ *
+ * Copyright (C) 2004 Peter Graves
+ * $Id: unbound_slot_instance.java,v 1.1 2004-01-02 01:19:12 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+// ### unbound-slot-instance
+public final class unbound_slot_instance extends Primitive1
+{
+    private unbound_slot_instance()
+    {
+        super("unbound-slot-instance");
+    }
+
+    public LispObject execute(LispObject arg) throws ConditionThrowable
+    {
+        if (arg instanceof UnboundSlot)
+            return ((UnboundSlot)arg).getInstance();
+        return signal(new TypeError(arg, Symbol.UNBOUND_SLOT));
+    }
+
+    private static final unbound_slot_instance CELL_ERROR_NAME =
+        new unbound_slot_instance();
+}
