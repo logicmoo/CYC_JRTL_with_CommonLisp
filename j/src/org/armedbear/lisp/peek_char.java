1e0b9b3c6e29774ced5abf0176b1bc1a87d57815
Peter Graves <gnooth@gmail.com>
2004-01-05 19:05:01 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/peek_char.java
@@ -0,0 +1,81 @@
+/*
+ * peek_char.java
+ *
+ * Copyright (C) 2004 Peter Graves
+ * $Id: peek_char.java,v 1.1 2004-01-05 19:01:36 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+// ### peek-char
+public final class peek_char extends Primitive
+{
+    private peek_char()
+    {
+        super("peek-char",
+              "&optional peek-type input-stream eof-error-p eof-value recursive-p");
+    }
+
+    public LispObject execute(LispObject[] args) throws ConditionThrowable
+    {
+        int length = args.length;
+        if (length > 5)
+            signal(new WrongNumberOfArgumentsException(this));
+        LispObject peekType = length > 0 ? args[0] : NIL;
+        CharacterInputStream stream =
+            length > 1 ? checkCharacterInputStream(args[1]) : getStandardInput();
+        boolean eofError = length > 2 ? (args[2] != NIL) : true;
+        LispObject eofValue = length > 3 ? args[3] : NIL;
+        boolean recursive = length > 4 ? (args[4] != NIL) : false;
+        if (peekType == NIL) {
+            LispObject result = stream.readChar(eofError, eofValue);
+            if (result instanceof LispCharacter)
+                stream.unreadChar((LispCharacter)result);
+            return result;
+        }
+        if (peekType == T) {
+            while (true) {
+                LispObject result = stream.readChar(eofError, eofValue);
+                if (result instanceof LispCharacter) {
+                    char c = ((LispCharacter)result).getValue();
+                    if (!Character.isWhitespace(c)) {
+                        stream.unreadChar((LispCharacter)result);
+                        return result;
+                    }
+                } else
+                    return result;
+            }
+        }
+        if (peekType instanceof LispCharacter) {
+            char c = ((LispCharacter)peekType).getValue();
+            while (true) {
+                LispObject result = stream.readChar(eofError, eofValue);
+                if (result instanceof LispCharacter) {
+                    if (((LispCharacter)result).getValue() == c) {
+                        stream.unreadChar((LispCharacter)result);
+                        return result;
+                    }
+                } else
+                    return result;
+            }
+        }
+        return signal(new SimpleError(String.valueOf(peekType) +
+                                      " is an illegal peek-type."));
+    }
+
+    private static final Primitive PEEK_CHAR = new peek_char();
+}
---
+++ j/src/org/armedbear/lisp/with-input-from-string.lisp
@@ -0,0 +1,43 @@
+;;; with-input-from-string.lisp
+;;;
+;;; Copyright (C) 2004 Peter Graves
+;;; $Id: with-input-from-string.lisp,v 1.1 2004-01-05 19:05:01 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+;;; Adapted from CMUCL.
+
+(in-package "SYSTEM")
+
+(defmacro with-input-from-string ((var string &key index start end) &body forms)
+  `(let ((,var
+          ,(cond ((null end)
+                  `(make-string-input-stream ,string ,(or start 0)))
+                 ((symbolp end)
+                  `(if ,end
+                       (make-string-input-stream ,string
+                                                 ,(or start 0)
+                                                 ,end)
+                       (make-string-input-stream ,string
+                                                 ,(or start 0))))
+                 (t
+                  `(make-string-input-stream ,string
+                                             ,(or start 0)
+                                             ,end)))))
+     (unwind-protect
+      (progn ,@forms)
+      (close ,var)
+      ,@(when index
+          `((setf ,index (string-input-stream-current ,var)))))))
