7418d740c4cef6f5ffa62fa97630e22e65634691
Peter Graves <gnooth@gmail.com>
2004-01-20 00:56:52 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/read_char_no_hang.java
@@ -0,0 +1,48 @@
+/*
+ * read_char_no_hang.java
+ *
+ * Copyright (C) 2004 Peter Graves
+ * $Id: read_char_no_hang.java,v 1.1 2004-01-20 00:56:52 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+// ### read-char-no-hang
+// read-char-no-hang &optional input-stream eof-error-p eof-value recursive-p => char
+public final class read_char_no_hang extends Primitive
+{
+    private read_char_no_hang()
+    {
+        super("read-char-no-hang",
+              "&optional input-stream eof-error-p eof-value recursive-p");
+    }
+
+    public LispObject execute(LispObject[] args) throws ConditionThrowable
+    {
+        int length = args.length;
+        if (length > 4)
+            signal(new WrongNumberOfArgumentsException(this));
+        CharacterInputStream stream =
+            length > 0 ? checkCharacterInputStream(args[0]) : getStandardInput();
+        boolean eofError = length > 1 ? (args[1] != NIL) : true;
+        LispObject eofValue = length > 2 ? args[2] : NIL;
+        boolean recursive = length > 3 ? (args[3] != NIL) : false;
+        return stream.readCharNoHang(eofError, eofValue);
+    }
+
+    private static final Primitive READ_CHAR_NO_HANG = new read_char_no_hang();
+}
