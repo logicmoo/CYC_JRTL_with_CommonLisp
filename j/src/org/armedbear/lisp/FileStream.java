9311eb17bcac185e30a1543c777e76b5bd7b2322
Peter Graves <gnooth@gmail.com>
2004-01-25 15:52:06 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/FileStream.java
@@ -0,0 +1,54 @@
+/*
+ * FileStream.java
+ *
+ * Copyright (C) 2004 Peter Graves
+ * $Id: FileStream.java,v 1.1 2004-01-25 15:52:06 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public final class FileStream extends Stream
+{
+    public FileStream(InputStream inputStream, LispObject elementType,
+                      Pathname pathname)
+    {
+        super(inputStream, elementType, pathname);
+    }
+
+    public FileStream(OutputStream outputStream, LispObject elementType,
+                      Pathname pathname)
+    {
+        super(outputStream, elementType, pathname);
+    }
+
+    public LispObject typeOf()
+    {
+        return Symbol.FILE_STREAM;
+    }
+
+    public LispObject typep(LispObject typeSpecifier) throws ConditionThrowable
+    {
+        if (typeSpecifier == Symbol.FILE_STREAM)
+            return T;
+        if (typeSpecifier == BuiltInClass.FILE_STREAM)
+            return T;
+        return super.typep(typeSpecifier);
+    }
+}
