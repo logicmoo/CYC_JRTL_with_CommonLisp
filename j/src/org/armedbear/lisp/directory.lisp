fa44899b06cf2ef15b1babc0cfe65413d7fe0ade
Peter Graves <gnooth@gmail.com>
2004-01-26 14:24:31 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/directory.lisp
@@ -0,0 +1,52 @@
+;;; directory.lisp
+;;;
+;;; Copyright (C) 2004 Peter Graves
+;;; $Id: directory.lisp,v 1.1 2004-01-26 14:24:31 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package "SYSTEM")
+
+#-windows
+(defun pathname-match-p (pathname wildcard)
+  (let ((pathname (pathname pathname))
+        (wildcard (pathname wildcard)))
+    (unless (eq (pathname-name wildcard) :wild)
+      (unless (equal (pathname-name pathname) (pathname-name wildcard))
+        (return-from pathname-match-p nil)))
+    (unless (eq (pathname-directory wildcard) :wild)
+      (unless (equal (pathname-directory pathname) (pathname-directory wildcard))
+        (return-from pathname-match-p nil)))
+    (unless (eq (pathname-type wildcard) :wild)
+      (unless (equal (pathname-type pathname) (pathname-type wildcard))
+        (return-from pathname-match-p nil)))
+    t))
+
+(defun directory (pathname &key)
+  (let ((merged-pathname (merge-pathnames pathname)))
+    (if (wild-pathname-p merged-pathname)
+        (let ((namestring (directory-namestring merged-pathname)))
+          (when (and namestring (length namestring))
+            (let ((all-files (list-directory namestring))
+                  (matching-files ()))
+              (dolist (file all-files)
+                (when (pathname-match-p file merged-pathname)
+                  (push file matching-files)))
+              matching-files)))
+        ;; Not wild.
+        (let ((truename (probe-file merged-pathname)))
+          (if truename
+              (list (pathname truename))
+              nil)))))
