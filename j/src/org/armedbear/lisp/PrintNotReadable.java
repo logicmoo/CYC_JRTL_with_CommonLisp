80a5d3b20351c07e86e35be3c6755cf52c658021
Peter Graves <gnooth@gmail.com>
2004-02-02 19:12:00 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/PrintNotReadable.java
@@ -0,0 +1,58 @@
+/*
+ * PrintNotReadable.java
+ *
+ * Copyright (C) 2004 Peter Graves
+ * $Id: PrintNotReadable.java,v 1.1 2004-02-02 19:12:00 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public class PrintNotReadable extends LispError
+{
+    public PrintNotReadable()
+    {
+    }
+
+    public PrintNotReadable(LispObject initArgs) throws ConditionThrowable
+    {
+        super(initArgs);
+    }
+
+    public PrintNotReadable(String message)
+    {
+        super(message);
+    }
+
+    public LispObject typeOf()
+    {
+        return Symbol.PRINT_NOT_READABLE;
+    }
+
+    public LispClass classOf()
+    {
+        return BuiltInClass.PRINT_NOT_READABLE;
+    }
+
+    public LispObject typep(LispObject type) throws ConditionThrowable
+    {
+        if (type == Symbol.PRINT_NOT_READABLE)
+            return T;
+        if (type == BuiltInClass.PRINT_NOT_READABLE)
+            return T;
+        return super.typep(type);
+    }
+}
