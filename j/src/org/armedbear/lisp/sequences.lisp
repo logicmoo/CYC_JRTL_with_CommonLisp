1eda2bd4b601650f5f40dae91675d904ab5f7ec3
Peter Graves <gnooth@gmail.com>
2004-02-12 01:18:20 +0000

    2004

--- j/src/org/armedbear/lisp/sequences.lisp
+++ j/src/org/armedbear/lisp/sequences.lisp
@@ -1,7 +1,7 @@
 ;;; sequences.lisp
 ;;;
-;;; Copyright (C) 2003 Peter Graves
-;;; $Id: sequences.lisp,v 1.55 2004-02-12 01:18:01 piso Exp $
+;;; Copyright (C) 2003-2004 Peter Graves
+;;; $Id: sequences.lisp,v 1.56 2004-02-12 01:18:20 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
