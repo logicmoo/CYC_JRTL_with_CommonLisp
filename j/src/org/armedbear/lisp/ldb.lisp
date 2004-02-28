f3f769b072f865fb4b3e60787642c5a2224caeff
Peter Graves <gnooth@gmail.com>
2004-02-28 19:08:56 +0000

    SETF LDB

--- j/src/org/armedbear/lisp/ldb.lisp
+++ j/src/org/armedbear/lisp/ldb.lisp
@@ -1,7 +1,7 @@
 ;;; ldb.lisp
 ;;;
 ;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: ldb.lisp,v 1.3 2004-02-28 18:40:08 piso Exp $
+;;; $Id: ldb.lisp,v 1.4 2004-02-28 19:08:56 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -46,3 +46,29 @@
          (mask (1- (ash 1 size))))
     (logior (logand integer (lognot (ash mask position)))
 	    (ash (logand newbyte mask) position))))
+
+;; From SBCL.
+(define-setf-expander ldb (bytespec place &environment env)
+  (multiple-value-bind (dummies vals newval setter getter)
+    (get-setf-expansion place env)
+    (if (and (consp bytespec) (eq (car bytespec) 'byte))
+	(let ((n-size (gensym))
+	      (n-pos (gensym))
+	      (n-new (gensym)))
+	  (values (list* n-size n-pos dummies)
+		  (list* (second bytespec) (third bytespec) vals)
+		  (list n-new)
+		  `(let ((,(car newval) (dpb ,n-new (byte ,n-size ,n-pos)
+					     ,getter)))
+		     ,setter
+		     ,n-new)
+		  `(ldb (byte ,n-size ,n-pos) ,getter)))
+	(let ((btemp (gensym))
+	      (gnuval (gensym)))
+	  (values (cons btemp dummies)
+		  (cons bytespec vals)
+		  (list gnuval)
+		  `(let ((,(car newval) (dpb ,gnuval ,btemp ,getter)))
+		     ,setter
+		     ,gnuval)
+		  `(ldb ,btemp ,getter))))))
