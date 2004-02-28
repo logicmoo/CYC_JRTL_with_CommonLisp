7362ec2f24f9f21d94273bd0c757891dbe806363
Peter Graves <gnooth@gmail.com>
2004-02-28 16:55:32 +0000

    Work in progress.

--- j/src/org/armedbear/lisp/logior.java
+++ j/src/org/armedbear/lisp/logior.java
@@ -1,8 +1,8 @@
 /*
  * logior.java
  *
- * Copyright (C) 2003 Peter Graves
- * $Id: logior.java,v 1.5 2003-12-13 00:58:51 piso Exp $
+ * Copyright (C) 2003-2004 Peter Graves
+ * $Id: logior.java,v 1.6 2004-02-28 16:55:32 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -27,9 +27,9 @@ import java.math.BigInteger;
 // logior &rest integers => result-integer
 public final class logior extends Primitive
 {
-    private logior(String name, String arglist)
+    private logior()
     {
-        super(name,arglist);
+        super("logior", "&rest integers");
     }
 
     public LispObject execute()
@@ -37,44 +37,64 @@ public final class logior extends Primitive
         return Fixnum.ZERO;
     }
 
+    public LispObject execute(LispObject arg) throws ConditionThrowable
+    {
+        if (arg instanceof Fixnum || arg instanceof Bignum)
+            return arg;
+        return signal(new TypeError(arg, Symbol.INTEGER));
+    }
+
     public LispObject execute(LispObject first, LispObject second)
         throws ConditionThrowable
     {
-        if (first instanceof Fixnum && second instanceof Fixnum) {
-            return new Fixnum(((Fixnum)first).getValue() |
-                              ((Fixnum)second).getValue());
-        } else {
-            BigInteger n1, n2;
-            if (first instanceof Fixnum)
-                n1 = ((Fixnum)first).getBigInteger();
-            else if (first instanceof Bignum)
-                n1 = ((Bignum)first).getValue();
-            else
-                return signal(new TypeError(first, "integer"));
-            if (second instanceof Fixnum)
-                n2 = ((Fixnum)second).getBigInteger();
-            else if (second instanceof Bignum)
-                n2 = ((Bignum)second).getValue();
-            else
-                return signal(new TypeError(second, "integer"));
-            return number(n1.or(n2));
-        }
+        if (first instanceof Fixnum && second instanceof Fixnum)
+            return new Fixnum(((Fixnum)first).value | ((Fixnum)second).value);
+        BigInteger n1, n2;
+        if (first instanceof Fixnum)
+            n1 = ((Fixnum)first).getBigInteger();
+        else if (first instanceof Bignum)
+            n1 = ((Bignum)first).value;
+        else
+            return signal(new TypeError(first, Symbol.INTEGER));
+        if (second instanceof Fixnum)
+            n2 = ((Fixnum)second).getBigInteger();
+        else if (second instanceof Bignum)
+            n2 = ((Bignum)second).value;
+        else
+            return signal(new TypeError(second, Symbol.INTEGER));
+        return number(n1.or(n2));
     }
+
     public LispObject execute(LispObject[] args) throws ConditionThrowable
     {
-        BigInteger result = BigInteger.ZERO;
-        for (int i = 0; i < args.length; i++) {
+        final int limit = args.length;
+        int i = 0;
+        // Maybe all the arguments are fixnums.
+        int r = 0;
+        do {
+            if (args[i] instanceof Fixnum) {
+                r |= ((Fixnum)args[i]).value;
+                ++i;
+            } else
+                break;
+        } while (i < limit);
+        if (i == limit)
+            return number(r);
+        // Not all fixnums.
+        BigInteger result = BigInteger.valueOf(r);
+        while (i < limit) {
             BigInteger n;
             if (args[i] instanceof Fixnum)
                 n = ((Fixnum)args[i]).getBigInteger();
             else if (args[i] instanceof Bignum)
-                n = ((Bignum)args[i]).getValue();
+                n = ((Bignum)args[i]).value;
             else
-                return signal(new TypeError(args[i], "integer"));
+                return signal(new TypeError(args[i], Symbol.INTEGER));
             result = result.or(n);
+            ++i;
         }
         return number(result);
     }
 
-    private static final logior LOGIOR = new logior("logior","&rest integers");
+    private static final Primitive LOGIOR = new logior();
 }
