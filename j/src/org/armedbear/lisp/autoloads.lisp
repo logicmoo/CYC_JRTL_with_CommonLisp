b93780233476685fe8a484f48d6df630630d62bb
Peter Graves <gnooth@gmail.com>
2004-03-13 19:35:41 +0000

    Moved REM to rem.java.

--- j/src/org/armedbear/lisp/autoloads.lisp
+++ j/src/org/armedbear/lisp/autoloads.lisp
@@ -1,7 +1,7 @@
 ;;; autoloads.lisp
 ;;;
 ;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: autoloads.lisp,v 1.123 2004-03-09 19:00:49 piso Exp $
+;;; $Id: autoloads.lisp,v 1.124 2004-03-13 19:35:41 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -76,7 +76,7 @@
             pathname-type wild-pathname-p)
           "pathnames.lisp")
 (autoload 'directory "directory.lisp")
-(autoload '(signum ceiling round rem mod ffloor fceiling fround
+(autoload '(signum ceiling round mod ffloor fceiling fround
             rational rationalize gcd isqrt float-precision float-sign
             decode-float conjugate phase cis)
           "numbers.lisp")
--- j/src/org/armedbear/lisp/numbers.lisp
+++ j/src/org/armedbear/lisp/numbers.lisp
@@ -1,7 +1,7 @@
 ;;; numbers.lisp
 ;;;
 ;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: numbers.lisp,v 1.23 2004-03-09 11:53:43 piso Exp $
+;;; $Id: numbers.lisp,v 1.24 2004-03-13 19:35:18 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -29,11 +29,9 @@
 	  (if (plusp number) 1 -1)
 	  (/ number (abs number)))))
 
-
 ;;; If the numbers do not divide exactly and the result of (/ number divisor)
 ;;; would be positive then increment the quotient and decrement the remainder by
 ;;; the divisor.
-;;;
 (defun ceiling (number &optional (divisor 1))
   "Returns the smallest integer not less than number, or number/divisor.
   The second returned value is the remainder."
@@ -44,7 +42,6 @@
 		 (plusp number)))
 	(values (+ tru 1) (- rem divisor))
 	(values tru rem))))
-
 
 (defun round (number &optional (divisor 1))
   "Rounds number (or number/divisor) to nearest integer.
@@ -66,14 +63,6 @@
                      (values (- tru 1) (+ rem divisor))))
                 (t (values tru rem)))))))
 
-
-(defun rem (number divisor)
-  "Returns second result of TRUNCATE."
-  (multiple-value-bind (tru rem) (truncate number divisor)
-    (declare (ignore tru))
-    rem))
-
-
 (defun mod (number divisor)
   "Returns second result of FLOOR."
   (let ((rem (rem number divisor)))
@@ -83,7 +72,6 @@
 		 (minusp number)))
 	(+ rem divisor)
 	rem)))
-
 
 (defun ffloor (number &optional (divisor 1))
   "Same as FLOOR, but returns first value as a float."
@@ -95,7 +83,6 @@
 	(values (1- tru) (+ rem divisor))
 	(values tru rem))))
 
-
 (defun fceiling (number &optional (divisor 1))
   "Same as CEILING, but returns first value as a float."
   (multiple-value-bind (tru rem) (ftruncate number divisor)
@@ -106,13 +93,11 @@
 	(values (+ tru 1) (- rem divisor))
 	(values tru rem))))
 
-
 (defun fround (number &optional (divisor 1))
   "Same as ROUND, but returns first value as a float."
   (multiple-value-bind (res rem)
     (round number divisor)
     (values (float res (if (floatp rem) rem 1.0)) rem)))
-
 
 (defun rational (x)
   "RATIONAL produces a rational number for any real numeric argument.  This is
@@ -133,7 +118,6 @@
          x)
         (t
          (error 'type-error "wrong type: ~S is not a real number" x))))
-
 
 ;;; FIXME
 (defun rationalize (x)
@@ -238,7 +222,6 @@
 (when (and (fboundp 'jvm::jvm-compile) (not (autoloadp 'jvm::jvm-compile)))
   (mapcar #'jvm::jvm-compile '(ceiling
                                round
-                               rem
                                ftruncate
                                ffloor
                                fceiling
