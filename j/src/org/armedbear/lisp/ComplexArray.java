7047a01f0780fc7a563e62307a0566e3de4a21cb
Peter Graves <gnooth@gmail.com>
2004-03-15 17:03:31 +0000

    typeOf(): SIMPLE-ARRAY => ARRAY

--- j/src/org/armedbear/lisp/ComplexArray.java
+++ j/src/org/armedbear/lisp/ComplexArray.java
@@ -2,7 +2,7 @@
  * ComplexArray.java
  *
  * Copyright (C) 2003-2004 Peter Graves
- * $Id: ComplexArray.java,v 1.4 2004-03-04 02:01:45 piso Exp $
+ * $Id: ComplexArray.java,v 1.5 2004-03-15 17:03:31 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -115,7 +115,7 @@ public final class ComplexArray extends AbstractArray
 
     public LispObject typeOf()
     {
-        return list3(Symbol.SIMPLE_ARRAY, T, getDimensions());
+        return list3(Symbol.ARRAY, T, getDimensions());
     }
 
     public LispClass classOf()
