ecad2d068ecef45faeb0762fcf40b1e80efe4384
Peter Graves <gnooth@gmail.com>
2004-03-18 02:13:53 +0000

    (defvar *compile-print* t) => (setq *compile-print* t)

--- j/src/org/armedbear/lisp/jvm.lisp
+++ j/src/org/armedbear/lisp/jvm.lisp
@@ -1,7 +1,7 @@
 ;;; jvm.lisp
 ;;;
 ;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: jvm.lisp,v 1.80 2004-03-14 02:17:26 piso Exp $
+;;; $Id: jvm.lisp,v 1.81 2004-03-18 02:13:53 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -2523,7 +2523,7 @@
     (setf (char s 0) #\;)
     s))
 
-(defvar *compile-print* t)
+(setq *compile-print* t)
 
 (defun jvm-compile (name &optional definition)
   (let ((prefix (load-verbose-prefix)))
