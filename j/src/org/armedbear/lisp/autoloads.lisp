f562c4c65733d925eda30cd144192e7e8f32bede
Peter Graves <gnooth@gmail.com>
2004-03-18 00:35:20 +0000

    Fixed a typo.

--- j/src/org/armedbear/lisp/autoloads.lisp
+++ j/src/org/armedbear/lisp/autoloads.lisp
@@ -1,7 +1,7 @@
 ;;; autoloads.lisp
 ;;;
 ;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: autoloads.lisp,v 1.126 2004-03-17 17:55:50 piso Exp $
+;;; $Id: autoloads.lisp,v 1.127 2004-03-18 00:35:20 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -19,7 +19,7 @@
 
 (in-package "SYSTEM")
 
-(autoload '(digit-char-p alphanumericp char/= char< char> char>=char-not-equal)
+(autoload '(digit-char-p alphanumericp char/= char< char> char>= char-not-equal)
           "chars.lisp")
 (autoload '(string-upcase string-downcase string-capitalize
             nstring-upcase nstring-downcase nstring-capitalize
