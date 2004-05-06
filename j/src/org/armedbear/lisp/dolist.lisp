2dcbec3852da55b1a60e6722bb4707ab4628575a
Peter Graves <gnooth@gmail.com>
2004-05-06 11:06:12 +0000

    Use SBCL's implementation.

--- j/src/org/armedbear/lisp/dolist.lisp
+++ j/src/org/armedbear/lisp/dolist.lisp
@@ -1,7 +1,7 @@
 ;;; dolist.lisp
 ;;;
 ;;; Copyright (C) 2004 Peter Graves
-;;; $Id: dolist.lisp,v 1.1 2004-03-31 02:45:06 piso Exp $
+;;; $Id: dolist.lisp,v 1.2 2004-05-06 11:06:12 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,28 +17,26 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-;;; Adapted from CMUCL.
+;;; Adapted from SBCL.
 
 (in-package "SYSTEM")
 
-;;; We repeatedly bind the var instead of setting it so that we never give the
-;;; var a random value such as NIL (which might conflict with a declaration).
-;;; If there is a result form, we introduce a gratitous binding of the variable
-;;; to NIL w/o the declarations, then evaluate the result form in that
-;;; environment.  We spuriously reference the gratuitous variable, since we
-;;; don't want to use IGNORABLE on what might be a special var.
 (defmacro dolist ((var list &optional (result nil)) &body body)
   (multiple-value-bind (forms decls) (parse-body body nil)
-    (let ((n-list (gensym)))
-      `(do* ((,n-list ,list (cdr ,n-list)))
-	    ((endp ,n-list)
-	     ,@(if (constantp result)
-		   `(,result)
-		   `((let ((,var nil))
-		       ,@decls
-		       ,var
-		       ,result))))
-         (let ((,var (car ,n-list)))
-           ,@decls
+    (let ((rest (gensym "REST"))
+          (start (gensym "START")))
+      `(block nil
+         (let ((,rest ,list))
            (tagbody
-            ,@forms))))))
+            ,start
+            (unless (endp ,rest)
+              (let ((,var (car ,rest)))
+                ,@decls
+                (setq ,rest (cdr ,rest))
+                (tagbody ,@forms))
+              (go ,start))))
+         ,(if result
+              `(let ((,var nil))
+                 ,var
+                 ,result)
+              nil)))))
