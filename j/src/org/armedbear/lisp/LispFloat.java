41d769a2422fdb66ea4598a7878b2f6ed51b0019
Peter Graves <gnooth@gmail.com>
2004-06-04 16:17:09 +0000

    psxhash()

--- j/src/org/armedbear/lisp/LispFloat.java
+++ j/src/org/armedbear/lisp/LispFloat.java
@@ -2,7 +2,7 @@
  * LispFloat.java
  *
  * Copyright (C) 2003-2004 Peter Graves
- * $Id: LispFloat.java,v 1.62 2004-06-02 21:18:42 piso Exp $
+ * $Id: LispFloat.java,v 1.63 2004-06-04 16:17:09 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -393,6 +393,14 @@ public final class LispFloat extends LispObject
     {
         long bits = Double.doubleToLongBits(value);
         return (int) (bits ^ (bits >>> 32));
+    }
+
+    public int psxhash() throws ConditionThrowable
+    {
+        if ((value % 1) == 0)
+            return (((int)value) & 0x7fffffff);
+        else
+            return (hashCode() & 0x7fffffff);
     }
 
     public String toString()
