bf70a6a85c2846c60a10d870647514655abb13a2
Peter Graves <gnooth@gmail.com>
2004-06-08 23:03:45 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/CapitalizeStream.java
@@ -0,0 +1,67 @@
+/*
+ * CapitalizeStream.java
+ *
+ * Copyright (C) 2004 Peter Graves
+ * $Id: CapitalizeStream.java,v 1.1 2004-06-08 23:03:45 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public final class CapitalizeStream extends CaseFrobStream
+{
+    private boolean inWord;
+
+    public CapitalizeStream(Stream target) throws ConditionThrowable
+    {
+        super(target);
+    }
+
+    public void _writeChar(char c) throws ConditionThrowable
+    {
+        if (inWord) {
+            if (Character.isUpperCase(c)) {
+                c = Utilities.toLowerCase(c);
+            } else if (!Character.isLowerCase(c) && !Character.isDigit(c)) {
+                inWord = false;
+            }
+        } else {
+            // Not in a word.
+            if (Character.isUpperCase(c)) {
+                inWord = true;
+            } else if (Character.isLowerCase(c)) {
+                c = Utilities.toUpperCase(c);
+                inWord = true;
+            } else if (Character.isDigit(c)) {
+                inWord = true;
+            }
+        }
+        target._writeChar(c);
+    }
+
+    public void _writeString(String s) throws ConditionThrowable
+    {
+        final int limit = s.length();
+        for (int i = 0; i < limit; i++)
+            _writeChar(s.charAt(i));
+    }
+
+    public void _writeLine(String s) throws ConditionThrowable
+    {
+        target._writeString(s);
+        target._writeChar('\n');
+    }
+}
