6417c5a6362ee80a0a0ece65e64bdd19544d1aa0
Peter Graves <gnooth@gmail.com>
2004-08-02 15:07:38 +0000

    Use traditional implementation of M-V-B in terms of M-V-C (from CMUCL).

--- j/src/org/armedbear/lisp/multiple-value-bind.lisp
+++ j/src/org/armedbear/lisp/multiple-value-bind.lisp
@@ -1,7 +1,7 @@
 ;;; multiple-value-bind.lisp
 ;;;
 ;;; Copyright (C) 2004 Peter Graves
-;;; $Id: multiple-value-bind.lisp,v 1.1 2004-03-31 03:03:38 piso Exp $
+;;; $Id: multiple-value-bind.lisp,v 1.2 2004-08-02 15:07:38 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,13 +17,24 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-;;; Adapted from CLISP.
+;;; Adapted from CMUCL.
 
 (in-package "SYSTEM")
 
-(defmacro multiple-value-bind (varlist form &body body)
-  (let ((g (gensym))
-        (poplist nil))
-    (dolist (var varlist) (setq poplist (cons `(,var (pop ,g)) poplist)))
-    `(let* ((,g (multiple-value-list ,form)) ,@(nreverse poplist))
-       ,@body)))
+;; The traditional implementation of M-V-B in terms of M-V-C. ABCL implements
+;; M-V-B as a special form in the interpreter, and ABCL's compiler handles it
+;; specifically too, so this code is only here to support code walkers and the
+;; like, as required by ANSI.
+(defmacro multiple-value-bind (varlist value-form &body body)
+  (unless (and (listp varlist) (every #'symbolp varlist))
+    (error 'program-error
+           :format-control "Variable list is not a list of symbols: ~S."
+           :format-arguments (list varlist)))
+  (if (= (length varlist) 1)
+      `(let ((,(car varlist) ,value-form))
+	 ,@body)
+      (let ((ignore (gensym)))
+	`(multiple-value-call #'(lambda (&optional ,@(mapcar #'list varlist) &rest ,ignore)
+                                 (declare (ignore ,ignore))
+                                 ,@body)
+                              ,value-form))))
