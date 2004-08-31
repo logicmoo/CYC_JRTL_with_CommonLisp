26d785a0a81d259b01ceb211e20b5558526af5ad
Peter Graves <gnooth@gmail.com>
2004-08-31 23:23:41 +0000

    No-arg constructor: title is "jlisp".

--- j/src/org/armedbear/j/LispShell.java
+++ j/src/org/armedbear/j/LispShell.java
@@ -2,7 +2,7 @@
  * LispShell.java
  *
  * Copyright (C) 2002-2004 Peter Graves
- * $Id: LispShell.java,v 1.57 2004-05-02 14:55:14 piso Exp $
+ * $Id: LispShell.java,v 1.58 2004-08-31 23:23:41 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -56,7 +56,7 @@ public class LispShell extends Shell
     {
         setPromptRE(ARMEDBEAR_PROMPT_PATTERN);
         setResetCommand(":reset");
-        this.title = title;
+        title = "jlisp";
     }
 
     private LispShell(String shellCommand, String title)
