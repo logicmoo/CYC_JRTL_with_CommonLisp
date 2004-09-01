b5680bda370a422a9be3c5cd80316e2c56c4485a
Peter Graves <gnooth@gmail.com>
2004-09-01 18:01:26 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/compile-file-pathname.lisp
@@ -0,0 +1,33 @@
+;;; compile-file-pathname.lisp
+;;;
+;;; Copyright (C) 2004 Peter Graves
+;;; $Id: compile-file-pathname.lisp,v 1.1 2004-09-01 18:01:26 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package #:system)
+
+;; Adapted from SBCL.
+(defun cfp-output-file-default (input-file)
+  (let* ((defaults (merge-pathnames input-file *default-pathname-defaults*))
+	 (retyped (make-pathname :type *compile-file-type* :defaults defaults)))
+    retyped))
+
+(defun compile-file-pathname (input-file
+                              &key
+                              (output-file (cfp-output-file-default
+                                            input-file))
+                              &allow-other-keys)
+  (merge-pathnames output-file (merge-pathnames input-file)))
