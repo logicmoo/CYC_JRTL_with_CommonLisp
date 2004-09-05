09ff8b31708c75f5c255973c4b0d281a130934a8
Peter Graves <gnooth@gmail.com>
2004-09-05 19:59:25 +0000

    FIND-DEFINITIONS

--- j/src/org/armedbear/lisp/swank-sbcl.lisp
+++ j/src/org/armedbear/lisp/swank-sbcl.lisp
@@ -54,3 +54,69 @@
 
 (defun arglist (function-name)
   (sb-introspect:function-arglist function-name))
+
+;;;; Definitions
+
+(defvar *debug-definition-finding* nil
+  "When true don't handle errors while looking for definitions.
+   This is useful when debugging the definition-finding code.")
+
+(defun make-location (buffer position)
+  (list :location buffer position))
+
+;;; FIXME we don't handle the compiled-interactively case yet.  That
+;;; should have NIL :filename & :position, and non-NIL :source-form
+(defun function-source-location (function &optional name)
+  "Try to find the canonical source location of FUNCTION."
+  (let* ((def (sb-introspect:find-definition-source function))
+         (pathname (sb-introspect:definition-source-pathname def))
+         (path (sb-introspect:definition-source-form-path def))
+         (position (sb-introspect:definition-source-character-offset def)))
+    (unless pathname
+      (return-from function-source-location
+                   (list :error (format nil "No filename for: ~S" function))))
+    (multiple-value-bind (truename condition)
+      (ignore-errors (truename pathname))
+      (when condition
+        (return-from function-source-location
+                     (list :error (format nil "~A" condition))))
+      (make-location
+       (list :file (namestring truename))
+       ;; source-paths depend on the file having been compiled with
+       ;; lotsa debugging.  If not present, return the function name
+       ;; for emacs to attempt to find with a regex
+       (cond (path (list :source-path path position))
+             (t (list :function-name
+                      (or (and name (string name))
+                          (string (sb-kernel:%fun-name function))))))))))
+
+(defun safe-function-source-location (fun name)
+  (if *debug-definition-finding*
+      (function-source-location fun name)
+      (handler-case (function-source-location fun name)
+        (error (e)
+               (list (list :error (format nil "Error: ~A" e)))))))
+
+(defun method-definitions (gf)
+  (let ((methods (sb-mop:generic-function-methods gf))
+        (name (sb-mop:generic-function-name gf)))
+    (loop for method in methods
+      collect (list `(method ,name ,(sb-pcl::unparse-specializers method))
+                    (safe-function-source-location method name)))))
+
+(defun function-definitions (name)
+  (flet ((loc (fn name) (safe-function-source-location fn name)))
+    (cond ((and (symbolp name) (macro-function name))
+           (list (list `(defmacro ,name)
+                       (loc (macro-function name) name))))
+          ((fboundp name)
+           (let ((fn (fdefinition name)))
+             (typecase fn
+               (generic-function
+                (cons (list `(defgeneric ,name) (loc fn name))
+                      (method-definitions fn)))
+               (t
+                (list (list `(function ,name) (loc fn name))))))))))
+
+(defun find-definitions (name)
+  (function-definitions name))
