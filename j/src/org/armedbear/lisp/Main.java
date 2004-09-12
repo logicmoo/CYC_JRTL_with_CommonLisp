8bfe529cbe0cac4473aa95c6912a694cd63474ea
Peter Graves <gnooth@gmail.com>
2004-09-12 01:44:06 +0000

    Moved processing of command line arguments to Interpreter.java.

--- j/src/org/armedbear/lisp/Main.java
+++ j/src/org/armedbear/lisp/Main.java
@@ -2,7 +2,7 @@
  * Main.java
  *
  * Copyright (C) 2002-2004 Peter Graves
- * $Id: Main.java,v 1.4 2004-03-25 01:20:46 piso Exp $
+ * $Id: Main.java,v 1.5 2004-09-12 01:44:06 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -28,63 +28,7 @@ public final class Main
     public static void main(String[] args)
     {
         Interpreter interpreter = Interpreter.createInstance();
-
-        boolean print = true;
-
-        // check for command-line args
-        if (args.length > 0) {
-            interpreter.initializeLisp(false);
-
-            for (int i = 0; i < args.length; ++i) {
-                if (args[i].equals("--noprint")) {
-                    print = false;
-                } else if (args[i].equals("--eval")) {
-                    if (i+1 < args.length) {
-                        LispObject result = null;
-                        try {
-                            result = interpreter.evaluate(args[i+1]);
-                        }
-                        catch (ConditionThrowable c) {
-                            System.err.println("Caught condition: " +
-                                               c.getCondition().toString() +
-                                               " while evaluating: " +
-                                               args[i+1]);
-                            System.exit(2);
-                        }
-                        ++i;
-                        if (print)
-                            System.out.println(String.valueOf(result)); // TODO: multiple values
-                    } else {
-                        System.err.println("No argument supplied to --eval");
-                        System.exit(1);
-                    }
-                } else if (args[i].equals("--load")) {
-                    if (i+1 < args.length) {
-                        LispObject result = null;
-                        try {
-                            // String loadStr = "(load \"" + String.valueOf(args[i+1]) + "\")";
-                            result = Load.load(args[i+1], false, false, true);
-                        }
-                        catch (ConditionThrowable c) {
-                            System.err.println("Caught condition: " +
-                                               c.getCondition().toString() +
-                                               " while loading: " +
-                                               args[i+1]);
-                            System.exit(2);
-                        }
-                        ++i;
-                        if (print)
-                            System.out.println(String.valueOf(result));
-                    } else {
-                        System.err.println("No argument supplied to --load");
-                        System.exit(1);
-                    }
-                }
-            }
-        }
-        // Now the toplevel will run unless we got an error or the user called
-        // EXTENSIONS:EXIT.
         if (interpreter != null)
-            interpreter.run();
+            interpreter.run(args);
     }
 }
