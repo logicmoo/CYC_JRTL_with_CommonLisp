0ad6a9d4fb2285e4bed862e44e3ec95c60db203c
Peter Graves <gnooth@gmail.com>
2004-09-15 20:01:53 +0000

    *FASL-VERSION* => 12

--- j/src/org/armedbear/lisp/Load.java
+++ j/src/org/armedbear/lisp/Load.java
@@ -2,7 +2,7 @@
  * Load.java
  *
  * Copyright (C) 2002-2004 Peter Graves
- * $Id: Load.java,v 1.73 2004-09-15 17:51:55 piso Exp $
+ * $Id: Load.java,v 1.74 2004-09-15 20:01:53 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -232,7 +232,7 @@ public final class Load extends Lisp
     // ### *fasl-version*
     // internal symbol
     private static final Symbol _FASL_VERSION_ =
-        internConstant("*FASL-VERSION*", PACKAGE_SYS, new Fixnum(11));
+        internConstant("*FASL-VERSION*", PACKAGE_SYS, new Fixnum(12));
 
     // ### *fasl-anonymous-package*
     // internal symbol
