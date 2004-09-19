643c3df7e475bff6c9f4259ae1f65ccac1e94483
Peter Graves <gnooth@gmail.com>
2004-09-19 13:35:21 +0000

    Make sure size > 0.

--- j/src/org/armedbear/lisp/make-hash-table.lisp
+++ j/src/org/armedbear/lisp/make-hash-table.lisp
@@ -1,7 +1,7 @@
 ;;; make-hash-table.lisp
 ;;;
-;;; Copyright (C) 2003 Peter Graves
-;;; $Id: make-hash-table.lisp,v 1.4 2004-01-01 18:24:03 piso Exp $
+;;; Copyright (C) 2003-2004 Peter Graves
+;;; $Id: make-hash-table.lisp,v 1.5 2004-09-19 13:35:21 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,14 +17,12 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-(in-package "SYSTEM")
+(in-package #:system)
 
 (defun make-hash-table (&key (test 'eql) (size 11) (rehash-size 1.5)
 			     (rehash-threshold 0.75))
   (setq test (coerce-to-function test))
   (unless (and (integerp size) (>= size 0))
-    (error 'type-error "MAKE-HASH-TABLE: ~S is not a non-negative integer" size))
-  ;; %make-hash-table expects size to be a fixnum.
-  (when (> size array-dimension-limit)
-    (setq size array-dimension-limit))
-  (%make-hash-table test size rehash-size rehash-threshold))
+    (error 'type-error "MAKE-HASH-TABLE: ~S is not a non-negative integer." size))
+  (let ((size (max 11 (min size array-dimension-limit))))
+    (%make-hash-table test size rehash-size rehash-threshold)))
