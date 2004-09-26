82bac1059fb3302d2280a279e38fbf9f2455289a
Andreas Simon <asimon@users.noreply.github.com>
2004-09-26 08:32:51 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/SlimeInputStream.java
@@ -0,0 +1,145 @@
+/*
+ * StringInputStream.java
+ *
+ * Copyright (C) 2004 Peter Graves
+ * $Id: SlimeInputStream.java,v 1.1 2004-09-26 08:32:51 asimon Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public class SlimeInputStream extends Stream
+{
+    String s;
+    int length;
+    final Function f;
+    final Stream ostream;
+
+    public SlimeInputStream(Function f, Stream ostream)
+    {
+        elementType = Symbol.CHARACTER;
+        isInputStream = true;
+        isOutputStream = false;
+        isCharacterStream = true;
+        isBinaryStream = false;
+        this.f = f;
+        this.ostream = ostream;
+    }
+
+    public LispObject typeOf()
+    {
+        return Symbol.SLIME_INPUT_STREAM;
+    }
+
+    public LispClass classOf()
+    {
+        return BuiltInClass.SLIME_INPUT_STREAM;
+    }
+
+    public LispObject typep(LispObject type) throws ConditionThrowable
+    {
+        if (type == Symbol.SLIME_INPUT_STREAM)
+            return T;
+        if (type == Symbol.STRING_STREAM)
+            return T;
+        if (type == BuiltInClass.SLIME_INPUT_STREAM)
+            return T;
+        if (type == BuiltInClass.STRING_STREAM)
+            return T;
+        return super.typep(type);
+    }
+
+    public LispObject close(LispObject abort) throws ConditionThrowable
+    {
+        setOpen(false);
+        return T;
+    }
+
+    public LispObject listen()
+    {
+        return offset < length ? T : NIL;
+    }
+
+    protected int _readChar()
+    {
+        if (offset >= length) {
+            try {
+                ostream.finishOutput();
+                s = funcall0(f, LispThread.currentThread()).getStringValue();
+            }
+            catch (Throwable t) {
+                return -1;
+            }
+            if (s.length() == 0)
+                return -1;
+            offset = 0;
+            length = s.length();
+        }
+        int n = s.charAt(offset);
+        ++offset;
+        if (n == '\n')
+            ++lineNumber; 
+        return n;
+    }
+
+    protected void _unreadChar(int n)
+    {
+        if (offset > 0) {
+            --offset;
+            if (n == '\n')
+                --lineNumber;
+        }
+    }
+
+    protected boolean _charReady()
+    {
+        return offset < length ? true : false;
+    }
+
+
+    public void _clearInput() throws ConditionThrowable
+    {
+        super._clearInput();
+        s = "";
+        offset = 0;
+        length = 0;
+        lineNumber = 0;
+    }
+
+
+    public String toString()
+    {
+        return unreadableString("SLIME-INPUT-STREAM");
+    }
+
+    // ### make-slime-input-stream
+    // make-slime-input-stream function output-stream => slime-input-stream
+    private static final Primitive2 MAKE_SLIME_INPUT_STREAM =
+        new Primitive2("make-slime-input-stream", PACKAGE_EXT, true, "function output-stream")
+    {
+        public LispObject execute(LispObject first, LispObject second) throws ConditionThrowable
+        {
+            final Function fun;
+            final Stream os;
+            if (first instanceof Symbol)
+                fun = (Function)first.getSymbolFunction();
+            else
+                fun = (Function)first;
+            os = checkCharacterOutputStream(second);
+            return new SlimeInputStream(fun, os);
+        }
+    };
+}
---
+++ j/src/org/armedbear/lisp/SlimeOutputStream.java
@@ -0,0 +1,148 @@
+/*
+ * SlimeOutputStream.java
+ *
+ * Copyright (C) 2002-2004 Peter Graves
+ * $Id: SlimeOutputStream.java,v 1.1 2004-09-26 08:32:51 asimon Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+import java.io.StringWriter;
+
+public final class SlimeOutputStream extends Stream
+{
+    private final StringWriter stringWriter;
+    final Function f;
+
+    private SlimeOutputStream(Function f)
+    {
+        this.elementType = Symbol.CHARACTER;
+        isInputStream = false;
+        isOutputStream = true;
+        isCharacterStream = true;
+        isBinaryStream = false;
+        setWriter(stringWriter = new StringWriter());
+	this.f = f;
+    }
+
+    public LispObject typeOf()
+    {
+        return Symbol.SLIME_OUTPUT_STREAM;
+    }
+
+    public LispClass classOf()
+    {
+        return BuiltInClass.SLIME_OUTPUT_STREAM;
+    }
+
+    public LispObject typep(LispObject type) throws ConditionThrowable
+    {
+        if (type == Symbol.SLIME_OUTPUT_STREAM)
+            return T;
+        if (type == Symbol.STRING_STREAM)
+            return T;
+        if (type == BuiltInClass.SLIME_OUTPUT_STREAM)
+            return T;
+        if (type == BuiltInClass.STRING_STREAM)
+            return T;
+        return super.typep(type);
+    }
+
+    public void _writeChar(char c) throws ConditionThrowable
+    {
+        if (elementType == NIL)
+            writeError();
+        super._writeChar(c);
+    }
+
+    public void _writeChars(char[] chars, int start, int end)
+        throws ConditionThrowable
+    {
+        if (elementType == NIL)
+            writeError();
+        super._writeChars(chars, start, end);
+    }
+
+    public void _writeString(String s) throws ConditionThrowable
+    {
+        if (elementType == NIL)
+            writeError();
+        super._writeString(s);
+    }
+
+    public void _writeLine(String s) throws ConditionThrowable
+    {
+        if (elementType == NIL)
+            writeError();
+        super._writeLine(s);
+    }
+
+    private void writeError() throws ConditionThrowable
+    {
+        signal(new TypeError("Attempt to write to a string output stream of element type NIL."));
+    }
+
+    protected long _getFilePosition() throws ConditionThrowable
+    {
+        if (elementType == NIL)
+            return 0;
+        return stringWriter.toString().length();
+    }
+
+    public void _finishOutput() throws ConditionThrowable
+    { 
+        try {
+            funcall1(f, new SimpleString(stringWriter.toString()), LispThread.currentThread());
+        }
+        catch (Throwable t) {
+        }
+        super._finishOutput();
+    }
+
+    /*
+    public LispObject getString() throws ConditionThrowable
+    {
+        if (elementType == NIL)
+            return new NilVector(0);
+        StringBuffer sb = stringWriter.getBuffer();
+        SimpleString s = new SimpleString(sb);
+        sb.setLength(0);
+        return s;
+    }
+    */
+
+    public String toString()
+    {
+        return unreadableString("SLIME-OUTPUT-STREAM");
+    }
+
+    // ### %make-string-output-stream
+    // %make-string-output-stream element-type => string-stream
+    private static final Primitive1 MAKE_SLIME_OUTPUT_STREAM =
+        new Primitive1("make-slime-output-stream", PACKAGE_EXT, true, "function")
+    {
+        public LispObject execute(LispObject arg) throws ConditionThrowable
+        {
+            final Function fun;
+            if (arg instanceof Symbol)
+                fun = (Function)arg.getSymbolFunction();
+            else
+                fun = (Function)arg;
+            return new SlimeOutputStream(fun);
+        }
+    };
+}
