57d54327250b0b5c7739e24c690a21518bfefb98
Peter Graves <gnooth@gmail.com>
2004-10-11 19:06:40 +0000

    %METHOD-GENERIC-FUNCTION, %SET-METHOD-GENERIC-FUNCTION

--- j/src/org/armedbear/lisp/Method.java
+++ j/src/org/armedbear/lisp/Method.java
@@ -2,7 +2,7 @@
  * Method.java
  *
  * Copyright (C) 2004 Peter Graves
- * $Id: Method.java,v 1.1 2004-10-10 17:10:04 piso Exp $
+ * $Id: Method.java,v 1.2 2004-10-11 19:06:40 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,11 +23,22 @@ package org.armedbear.lisp;
 
 public final class Method extends StandardObject
 {
+    private LispObject genericFunction;
     private LispObject function;
 
     public Method(LispClass cls, SimpleVector slots)
     {
         super(cls, slots);
+    }
+
+    public LispObject getGenericFunction()
+    {
+        return genericFunction;
+    }
+
+    public void setGenericFunction(LispObject genericFunction)
+    {
+        this.genericFunction = genericFunction;
     }
 
     public LispObject getFunction()
@@ -39,6 +50,36 @@ public final class Method extends StandardObject
     {
         this.function = function;
     }
+
+    private static final Primitive1 _METHOD_GENERIC_FUNCTION =
+        new Primitive1("%method-generic-function", PACKAGE_SYS, false)
+    {
+        public LispObject execute(LispObject arg) throws ConditionThrowable
+        {
+            try {
+                return ((Method)arg).getGenericFunction();
+            }
+            catch (ClassCastException e) {
+                return signal(new TypeError(arg, Symbol.METHOD));
+            }
+        }
+    };
+
+    private static final Primitive1 _SET_METHOD_GENERICFUNCTION =
+        new Primitive1("%set-method-generic-function", PACKAGE_SYS, false)
+    {
+        public LispObject execute(LispObject first, LispObject second)
+            throws ConditionThrowable
+        {
+            try {
+                ((Method)first).setGenericFunction(second);
+                return second;
+            }
+            catch (ClassCastException e) {
+                return signal(new TypeError(first, Symbol.METHOD));
+            }
+        }
+    };
 
     private static final Primitive1 METHOD_FUNCTION =
         new Primitive1("method-function", PACKAGE_SYS, false)
