4350a48614855b4fb60519876bee1045af743c37
Peter Graves <gnooth@gmail.com>
2004-11-06 20:05:23 +0000

    setLayout() => setClassLayout()

--- j/src/org/armedbear/lisp/StandardClass.java
+++ j/src/org/armedbear/lisp/StandardClass.java
@@ -2,7 +2,7 @@
  * StandardClass.java
  *
  * Copyright (C) 2003-2004 Peter Graves
- * $Id: StandardClass.java,v 1.19 2004-11-06 18:52:12 piso Exp $
+ * $Id: StandardClass.java,v 1.20 2004-11-06 20:05:23 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -25,13 +25,13 @@ public class StandardClass extends SlotClass
 {
     public StandardClass()
     {
-        setLayout(new Layout(this, NIL, NIL));
+        setClassLayout(new Layout(this, NIL, NIL));
     }
 
     public StandardClass(Symbol symbol, LispObject directSuperclasses)
     {
         super(symbol, directSuperclasses);
-        setLayout(new Layout(this, NIL, NIL));
+        setClassLayout(new Layout(this, NIL, NIL));
     }
 
     public LispObject typeOf()
