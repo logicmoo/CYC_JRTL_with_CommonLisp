bdf0b9408c7b763a9004e90122d6355bef641c40
Peter Graves <gnooth@gmail.com>
2004-11-13 00:33:40 +0000

    Work in progress.

--- j/src/org/armedbear/lisp/inspect.lisp
+++ j/src/org/armedbear/lisp/inspect.lisp
@@ -1,7 +1,7 @@
 ;;; inspect.lisp
 ;;;
 ;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: inspect.lisp,v 1.8 2004-05-23 17:42:35 piso Exp $
+;;; $Id: inspect.lisp,v 1.9 2004-11-13 00:33:40 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,14 +17,14 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-(in-package "SYSTEM")
+(in-package #:system)
 
-;; (require 'clos)
+(require 'clos)
 
 (require 'format)
 
 (defun leader (name)
-  (let ((size (max 0 (- 12 (length name)))))
+  (let ((size (max 0 (- 12 (length (string name))))))
     (concatenate 'string (make-string size :initial-element #\-) "->")))
 
 (defun safe-length (x)
@@ -44,71 +44,76 @@
       (return (values nil :circular)))))
 
 (defun display-object (obj display-parts-p)
-  (cond
-;;    ((typep obj 'standard-object)
-;;          (format t "standard-object case~%")
-;;          (format t "~A at #x~X~%" (inspected-description obj) (identity-hash-code obj))
-;;          (do ((slots (class-slots (class-of obj)) (cdr slots))
-;;               (i 0 (1+ i)))
-;;              ((null slots))
-;;            (let* ((slot (car slots))
-;;                   (name (slot-definition-name slot)))
-;;              (format t "~4D ~A -> ~S~%" i name
-;;                      (if (slot-boundp obj name) (slot-value obj name) +slot-unbound+)))))
-        ((vectorp obj)
-         (format t "~A at #x~X~%" (inspected-description obj) (identity-hash-code obj))
-         (let ((limit (min (length obj) 25)))
-           (dotimes (i limit)
-             (format t "~4D-> ~A~%" i (inspected-description (aref obj i))))))
-        ((consp obj)
-         (multiple-value-bind (len kind) (safe-length obj)
-           (case kind
-             (:proper
-              (format t "A proper list with ~D elements at #x~X~%"
-                      len
-                      (identity-hash-code obj))
-              (let ((i 0))
-                (dolist (item obj)
-                  (cond ((< i 25)
-                         (format t "~4D-> ~A~%" i (inspected-description item)))
-                        ((= i 25)
-                         (format t "    ...~%"))
-                        ((= i (1- len))
-                         (format t "~4D-> ~A~%" i (inspected-description item))))
-                  (incf i))))
-             (:dotted
-              (format t "A dotted list with ~D elements at #x~X~%"
-                      len
-                      (identity-hash-code obj))
-              (let* ((rest obj)
-                     (item (car rest))
-                     (i 0))
-                (loop
-                  (cond ((< i 25)
-                         (format t "~4D-> ~A~%" i (inspected-description item)))
-                        ((= i 25)
-                         (format t "    ...~%")))
-                  (incf i)
-                  (setf rest (cdr rest))
-                  (when (atom rest)
-                    (return))
-                  (setf item (car rest)))
-                (format t "tail-> ~A~%" (inspected-description rest))))
-             (:circular
-              (format t "A circular list at #x~X~%" (identity-hash-code obj))))))
-        (t
-         (format t "~A at #x~X~%" (inspected-description obj) (identity-hash-code obj))
-         (let ((parts (inspected-parts obj))
-               (i 0))
-           (dolist (part parts)
-             (let ((name (string (car part)))
-                   (value (cdr part)))
-               (format t "~4D ~A ~A ~A~%" i
-                       name
-                       (leader name)
-                       (inspected-description value)
-                       )
-               (incf i))))))
+  (let ((*print-length* 1)
+        (*print-level* 1))
+    (cond ((typep obj 'standard-object)
+           (let ((parts (inspected-parts obj))
+                 (i 0))
+             (dolist (part parts)
+               (let ((name (car part))
+                     (value (cdr part)))
+                 (format t "~4D ~A ~A ~S~%"
+                         i
+                         name
+                         (leader name)
+                         value)
+                 (incf i)))))
+          ((vectorp obj)
+           (format t "~A at #x~X~%" (inspected-description obj) (identity-hash-code obj))
+           (let ((limit (min (length obj) 25)))
+             (dotimes (i limit)
+               (format t "~4D-> ~A~%" i (inspected-description (aref obj i))))))
+          ((consp obj)
+           (multiple-value-bind (len kind) (safe-length obj)
+             (case kind
+               (:proper
+                (format t "A proper list with ~D elements at #x~X~%"
+                        len
+                        (identity-hash-code obj))
+                (let ((i 0))
+                  (dolist (item obj)
+                    (cond ((< i 25)
+                           (format t "~4D-> ~A~%" i item))
+                          ((= i 25)
+                           (format t "    ...~%"))
+                          ((= i (1- len))
+                           (format t "~4D-> ~A~%" i item)))
+                    (incf i))))
+               (:dotted
+                (format t "A dotted list with ~D elements at #x~X~%"
+                        len
+                        (identity-hash-code obj))
+                (let* ((rest obj)
+                       (item (car rest))
+                       (i 0))
+                  (loop
+                    (cond ((< i 25)
+                           (format t "~4D-> ~A~%" i (inspected-description item)))
+                          ((= i 25)
+                           (format t "    ...~%")))
+                    (incf i)
+                    (setf rest (cdr rest))
+                    (when (atom rest)
+                      (return))
+                    (setf item (car rest)))
+                  (format t "tail-> ~A~%" (inspected-description rest))))
+               (:circular
+                (format t "A circular list at #x~X~%" (identity-hash-code obj))))))
+          (t
+           (format t "~A at #x~X~%" (inspected-description obj) (identity-hash-code obj))
+           (let ((parts (inspected-parts obj))
+                 (i 0)
+                 (limit 25))
+             (dolist (part parts)
+               (let ((name (string (car part)))
+                     (value (cdr part)))
+                 (format t "~4D ~A ~A ~A~%" i
+                         name
+                         (leader name)
+                         value)
+                 (incf i)
+                 (when (> i limit)
+                   return)))))))
   (values))
 
 (defun display-current ()
