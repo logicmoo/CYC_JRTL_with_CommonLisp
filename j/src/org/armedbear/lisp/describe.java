4a6ac1859bf78d4625a0a9ec424a385f80c97f01
Peter Graves <gnooth@gmail.com>
2005-01-13 19:45:26 +0000

    Refactoring.

--- j/src/org/armedbear/lisp/describe.java
+++ j/src/org/armedbear/lisp/describe.java
@@ -1,8 +1,8 @@
 /*
  * describe.java
  *
- * Copyright (C) 2003-2004 Peter Graves
- * $Id: describe.java,v 1.16 2004-11-13 15:02:01 piso Exp $
+ * Copyright (C) 2003-2005 Peter Graves
+ * $Id: describe.java,v 1.17 2005-01-13 19:45:26 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -28,89 +28,21 @@ public final class describe extends Lisp
     private static final Primitive DESCRIBE =
         new Primitive("describe", "object &optional stream")
     {
-        public LispObject execute(LispObject[] args) throws ConditionThrowable
+        public LispObject execute(LispObject arg) throws ConditionThrowable
         {
-            if (args.length != 1)
-                return signal(new WrongNumberOfArgumentsException(this));
-            LispObject obj = args[0];
-            StringBuffer sb = new StringBuffer(obj.writeToString());
-            if (obj instanceof Symbol) {
-                Symbol symbol = (Symbol) obj;
-                LispObject pkg = symbol.getPackage();
-                sb.append(" is an ");
-                if (pkg == NIL)
-                    sb.append("uninterned");
-                else if (((Package)pkg).findExternalSymbol(symbol.getName()) == symbol)
-                    sb.append("external");
-                else
-                    sb.append("internal");
-                sb.append(" symbol");
-                if (pkg != NIL) {
-                    sb.append(" in the ");
-                    sb.append(pkg.getName());
-                    sb.append(" package");
-                }
-                sb.append(".\n");
-                LispObject value = symbol.getSymbolValue();
-                if (symbol.isSpecialVariable()) {
-                    sb.append("It is a ");
-                    sb.append(symbol.isConstant() ? "constant" : "special variable");
-                    sb.append("; ");
-                    if (value != null) {
-                        sb.append("its value is ");
-                        sb.append(value.writeToString());
-                    } else
-                        sb.append("it is unbound");
-                    sb.append(".\n");
-                } else if (value != null) {
-                    sb.append("It is an undefined variable; its value is ");
-                    sb.append(value.writeToString());
-                    sb.append(".\n");
-                }
-                LispObject function = symbol.getSymbolFunction();
-                if (function != null) {
-                    sb.append("Its function binding is ");
-                    sb.append(function.writeToString());
-                    sb.append(".\n");
-                    if (function instanceof Function) {
-                        LispObject arglist = ((Function)function).getArglist();
-                        if (arglist != null) {
-                            LispThread thread = LispThread.currentThread();
-                            Binding lastSpecialBinding = thread.lastSpecialBinding;
-                            thread.bindSpecial(_PRINT_ESCAPE_, NIL);
-                            sb.append("Function argument list:\n  ");
-                            if (arglist instanceof AbstractString)
-                                sb.append(arglist.getStringValue());
-                            else
-                                sb.append(arglist.writeToString());
-                            sb.append('\n');
-                            thread.lastSpecialBinding = lastSpecialBinding;
-                        }
-                    }
-                    LispObject documentation =
-                        symbol.getFunctionDocumentation();
-                    if (documentation instanceof AbstractString) {
-                        sb.append("Function documentation:\n  ");
-                        sb.append(documentation.getStringValue());
-                        sb.append('\n');
-                    }
-                }
-                LispObject plist = symbol.getPropertyList();
-                if (plist != NIL) {
-                    sb.append("Its property list has these indicator/value pairs:\n");
-                    LispObject[] array = plist.copyToArray();
-                    for (int i = 0; i < array.length; i += 2) {
-                        sb.append("  ");
-                        sb.append(array[i].writeToString());
-                        sb.append(' ');
-                        sb.append(array[i+1].writeToString());
-                        sb.append('\n');
-                    }
-                }
-            }
             Stream out = getStandardOutput();
             out.freshLine();
-            out._writeString(sb.toString());
+            out._writeString(arg.describe());
+            out.freshLine();
+            return LispThread.currentThread().nothing();
+        }
+
+        public LispObject execute(LispObject first, LispObject second)
+            throws ConditionThrowable
+        {
+            Stream out = outSynonymOf(second);
+            out.freshLine();
+            out._writeString(first.describe());
             out.freshLine();
             return LispThread.currentThread().nothing();
         }
