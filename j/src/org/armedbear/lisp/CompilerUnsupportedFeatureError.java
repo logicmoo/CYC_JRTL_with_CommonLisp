8e28f774549a4500bab14ec28a7f1e436c732670
Peter Graves <gnooth@gmail.com>
2005-01-31 05:50:28 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/CompilerUnsupportedFeatureError.java
@@ -0,0 +1,42 @@
+/*
+ * CompilerUnsupportedFeatureError.java
+ *
+ * Copyright (C) 2005 Peter Graves
+ * $Id: CompilerUnsupportedFeatureError.java,v 1.1 2005-01-31 05:50:28 piso Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.armedbear.lisp;
+
+public class CompilerUnsupportedFeatureError extends Condition
+{
+    public CompilerUnsupportedFeatureError(LispObject initArgs) throws ConditionThrowable
+    {
+        super(initArgs);
+    }
+
+    public LispObject typeOf()
+    {
+        return Symbol.COMPILER_UNSUPPORTED_FEATURE_ERROR;
+    }
+
+    public LispObject typep(LispObject type) throws ConditionThrowable
+    {
+        if (type == Symbol.COMPILER_UNSUPPORTED_FEATURE_ERROR)
+            return T;
+        return super.typep(type);
+    }
+}
