a83717b16b4fb1e062e2555ea93ee0b185fc58a1
Peter Graves <gnooth@gmail.com>
2005-02-06 02:00:39 +0000

    StreamError(LispObject initArgs): call super(initArgs).

--- j/src/org/armedbear/lisp/StreamError.java
+++ j/src/org/armedbear/lisp/StreamError.java
@@ -1,8 +1,8 @@
 /*
  * StreamError.java
  *
- * Copyright (C) 2002-2004 Peter Graves
- * $Id: StreamError.java,v 1.14 2004-11-03 15:39:01 piso Exp $
+ * Copyright (C) 2002-2005 Peter Graves
+ * $Id: StreamError.java,v 1.15 2005-02-06 02:00:39 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -41,6 +41,7 @@ public class StreamError extends LispError
 
     public StreamError(LispObject initArgs) throws ConditionThrowable
     {
+        super(initArgs);
         LispObject stream = NIL;
         LispObject first, second;
         while (initArgs != NIL) {
