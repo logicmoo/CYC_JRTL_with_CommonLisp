eebcec8b61331e1a838079b7b840005d8694df1f
Peter Graves <gnooth@gmail.com>
2005-02-07 17:48:02 +0000

    Section 5.1.3.

--- j/src/org/armedbear/lisp/remf.lisp
+++ j/src/org/armedbear/lisp/remf.lisp
@@ -1,7 +1,7 @@
 ;;; remf.lisp
 ;;;
-;;; Copyright (C) 2003 Peter Graves
-;;; $Id: remf.lisp,v 1.1 2003-09-16 16:47:49 piso Exp $
+;;; Copyright (C) 2003-2005 Peter Graves
+;;; $Id: remf.lisp,v 1.2 2005-02-07 17:48:02 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,34 +17,36 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-;;; REMF (from OpenMCL)
+;;; Adapted from SBCL.
+
 (defmacro remf (place indicator &environment env)
   "Place may be any place expression acceptable to SETF, and is expected
-   to hold a property list or ().  This list is destructively altered to
-   remove the property specified by the indicator.  Returns T if such a
+   to hold a property list or (). This list is destructively altered to
+   remove the property specified by the indicator. Returns T if such a
    property was present, NIL if not."
   (multiple-value-bind (dummies vals newval setter getter)
-    (get-setf-expansion place env)
+      (get-setf-expansion place env)
     (do* ((d dummies (cdr d))
-          (v vals (cdr v))
-          (let-list nil)
-          (ind-temp (gensym))
-          (local1 (gensym))
-          (local2 (gensym)))
-         ((null d)
-          (push (list (car newval) getter) let-list)
-          (push (list ind-temp indicator) let-list)
-          `(let* ,(nreverse let-list)
-             (do ((,local1 ,(car newval) (cddr ,local1))
-                  (,local2 nil ,local1))
-                 ((atom ,local1) nil)
-               (cond ((atom (cdr ,local1))
-                      (error "odd-length property list in REMF"))
-                     ((eq (car ,local1) ,ind-temp)
-                      (cond (,local2
-                             (rplacd (cdr ,local2) (cddr ,local1))
-                             (return t))
-                            (t (setq ,(car newval) (cddr ,(car newval)))
-                               ,setter
-                               (return t))))))))
+	  (v vals (cdr v))
+	  (let-list nil)
+	  (ind-temp (gensym))
+	  (local1 (gensym))
+	  (local2 (gensym)))
+	 ((null d)
+          ;; See ANSI 5.1.3 for why we do out-of-order evaluation
+	  (push (list ind-temp indicator) let-list)
+	  (push (list (car newval) getter) let-list)
+	  `(let* ,(nreverse let-list)
+	     (do ((,local1 ,(car newval) (cddr ,local1))
+		  (,local2 nil ,local1))
+		 ((atom ,local1) nil)
+	       (cond ((atom (cdr ,local1))
+		      (error "Odd-length property list in REMF."))
+		     ((eq (car ,local1) ,ind-temp)
+		      (cond (,local2
+			     (rplacd (cdr ,local2) (cddr ,local1))
+			     (return t))
+			    (t (setq ,(car newval) (cddr ,(car newval)))
+			       ,setter
+			       (return t))))))))
       (push (list (car d) (car v)) let-list))))
