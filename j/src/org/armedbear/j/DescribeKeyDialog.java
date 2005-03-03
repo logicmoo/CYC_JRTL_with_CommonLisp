a7319bd63c12e076d91f668410d8e3c897074ccc
Peter Graves <gnooth@gmail.com>
2005-03-03 14:05:00 +0000

    Support emacs-style key bindings.

--- j/src/org/armedbear/j/DescribeKeyDialog.java
+++ j/src/org/armedbear/j/DescribeKeyDialog.java
@@ -1,8 +1,8 @@
 /*
  * DescribeKeyDialog.java
  *
- * Copyright (C) 2000-2004 Peter Graves
- * $Id: DescribeKeyDialog.java,v 1.6 2004-08-08 01:46:48 piso Exp $
+ * Copyright (C) 2000-2005 Peter Graves
+ * $Id: DescribeKeyDialog.java,v 1.7 2005-03-03 14:05:00 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -34,16 +34,22 @@ public final class DescribeKeyDialog extends AbstractDialog
 
     private final Editor editor;
     private final Buffer buffer;
+
+    private JTextField textField;
     private boolean seenKeyPressed;
-    private boolean disposed;
     private String keyStrokeText;
+    private boolean eventHandled;
+    private boolean failed;
+    private boolean disposed;
+    private KeyMap requestedKeyMap;
+    private boolean local;
 
     private DescribeKeyDialog(Editor editor)
     {
         super(editor, title, true); // Modal.
         this.editor = editor;
         buffer = editor.getBuffer();
-        JTextField textField = new JTextField(20);
+        textField = new JTextField(20);
         addLabelAndTextField(new JLabel(prompt), textField);
         addVerticalStrut();
         addCancel();
@@ -61,83 +67,123 @@ public final class DescribeKeyDialog extends AbstractDialog
             return;
         seenKeyPressed = true;
         final int modifiers = e.getModifiers();
-        KeyMapping mapping = new KeyMapping(keycode, modifiers, null);
-        keyStrokeText = mapping.getKeyText();
-        describeKey(e.getKeyChar(), keycode, modifiers);
+        Object command = describeKey(e.getKeyChar(), keycode, modifiers);
+        if (eventHandled || failed) {
+            KeyMapping mapping = new KeyMapping(keycode, modifiers, null);
+            if (keyStrokeText == null)
+                keyStrokeText = mapping.getKeyText();
+            else
+                keyStrokeText = keyStrokeText + " " + mapping.getKeyText();
+            textField.setText(keyStrokeText);
+        }
+        if (command != null)
+            report(command);
     }
 
     public void keyTyped(KeyEvent e)
     {
-        final char c = e.getKeyChar();
-        final int modifiers = e.getModifiers();
-        if (modifiers == 0 || modifiers == InputEvent.SHIFT_MASK) {
-            // Ignore whitespace key chars (e.g. Space, Shift Tab).
-            if (c > ' ') {
-                FastStringBuffer sb = new FastStringBuffer('\'');
-                sb.append(c);
-                sb.append('\'');
-                keyStrokeText = sb.toString();
+        if (!eventHandled) {
+            final char c = e.getKeyChar();
+            final int modifiers = e.getModifiers();
+            if (modifiers == 0 || modifiers == InputEvent.SHIFT_MASK) {
+                // Ignore whitespace key chars (e.g. Space, Shift Tab).
+                if (c > ' ') {
+                    FastStringBuffer sb = new FastStringBuffer('\'');
+                    sb.append(c);
+                    sb.append('\'');
+                    if (keyStrokeText == null)
+                        keyStrokeText = sb.toString();
+                    else
+                        keyStrokeText = keyStrokeText + " " + sb.toString();
+                    textField.setText(keyStrokeText);
+                }
             }
+            Object command = describeKey(c, e.getKeyCode(), modifiers);
+            if (!eventHandled) {
+                Log.debug("failed => true");
+                failed = true;
+            }
+            if (command != null)
+                report(command);
         }
-        describeKey(c, e.getKeyCode(), modifiers);
     }
 
     public void keyReleased(KeyEvent e)
     {
-        if (seenKeyPressed && !disposed) {
+        final int keycode = e.getKeyCode();
+        if (keycode == KeyEvent.VK_SHIFT || keycode == KeyEvent.VK_CONTROL ||
+            keycode == KeyEvent.VK_ALT || keycode == KeyEvent.VK_META)
+            return;
+        if (failed && !disposed) {
             dispose();
             // Use invokeLater() so message dialog will get focus.
             Runnable r = new Runnable() {
                 public void run()
                 {
-                    MessageDialog.showMessageDialog(editor,
+                    MessageDialog.showMessageDialog(
+                        editor,
                         keyStrokeText + " is not mapped", title);
                 }
             };
             SwingUtilities.invokeLater(r);
         }
+        eventHandled = false; // Start over.
     }
 
-    private void describeKey(char keyChar, int keyCode, int modifiers)
+    private Object describeKey(char keyChar, int keyCode, int modifiers)
     {
         if (disposed)
-            return;
+            return null;
         // Mask off the bits we don't care about (Java 1.4).
         modifiers &= 0x0f;
         if (keyCode == 0 && modifiers == InputEvent.SHIFT_MASK) // Shift only.
             modifiers = 0; // Ignore modifier.
-        boolean local = false;
-        final Mode mode = buffer.getMode();
-        KeyMapping mapping =
-            mode.getKeyMap().lookup(keyChar, keyCode, modifiers);
-        if (mapping != null)
-            local = true;
-        else {
-            mapping =
-                KeyMap.getGlobalKeyMap().lookup(keyChar, keyCode, modifiers);
+        KeyMapping mapping = null;
+        if (requestedKeyMap != null) {
+            mapping = requestedKeyMap.lookup(keyChar, keyCode, modifiers);
+        } else {
+            final Mode mode = buffer.getMode();
+            mapping = mode.getKeyMap().lookup(keyChar, keyCode, modifiers);
+            if (mapping != null)
+                local = true;
+            else
+                mapping = KeyMap.getGlobalKeyMap().lookup(keyChar, keyCode,
+                                                          modifiers);
         }
+        if (mapping == null)
+            return null;
+        eventHandled = true;
+        Object command = mapping.getCommand();
+        if (command instanceof KeyMap) {
+            requestedKeyMap = (KeyMap) command;
+            return null;
+        }
+        return command;
+    }
+
+    private void report(Object command)
+    {
         final FastStringBuffer sb = new FastStringBuffer();
-        if (mapping != null) {
-            sb.append(mapping.getKeyText());
-            sb.append(" is mapped to ");
-            sb.append(mapping.getCommand());
-            if (local) {
-                sb.append(" (");
-                sb.append(mode.toString());
-                sb.append(" mode)");
-            } else
-                sb.append(" (global mapping)");
-            dispose();
-            // Use invokeLater() so message dialog will get focus.
-            Runnable r = new Runnable() {
-                public void run()
-                {
-                    MessageDialog.showMessageDialog(editor, sb.toString(),
-                        "Describe Key");
-                }
-            };
-            SwingUtilities.invokeLater(r);
-        }
+        sb.append(keyStrokeText);
+        sb.append(" is mapped to ");
+        sb.append(command);
+        if (local) {
+            sb.append(" (");
+            final Mode mode = buffer.getMode();
+            sb.append(mode.toString());
+            sb.append(" mode)");
+        } else
+            sb.append(" (global mapping)");
+        dispose();
+        // Use invokeLater() so message dialog will get focus.
+        Runnable r = new Runnable() {
+            public void run()
+            {
+                MessageDialog.showMessageDialog(editor, sb.toString(),
+                                                "Describe Key");
+            }
+        };
+        SwingUtilities.invokeLater(r);
     }
 
     public void dispose()
