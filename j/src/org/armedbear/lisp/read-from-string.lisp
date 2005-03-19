1a225619a521d3fa11cf03073fde36b5145735af
Peter Graves <gnooth@gmail.com>
2005-03-19 19:56:30 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/read-from-string.lisp
@@ -0,0 +1,24 @@
+;;; read-from-string.lisp
+;;;
+;;; Copyright (C) 2003-2005 Peter Graves
+;;; $Id: read-from-string.lisp,v 1.1 2005-03-19 19:56:30 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package #:system)
+
+(defun read-from-string (string &optional (eof-error-p t) eof-value
+                                &key (start 0) end preserve-whitespace)
+  (sys::%read-from-string string eof-error-p eof-value start end preserve-whitespace))
