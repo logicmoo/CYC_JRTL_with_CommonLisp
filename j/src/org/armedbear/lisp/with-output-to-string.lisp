c4037bb630b8c7ffc5dcadc706735895335a508c
Peter Graves <gnooth@gmail.com>
2005-04-11 22:34:35 +0000

    WITH-OUTPUT-TO-STRING.16

--- j/src/org/armedbear/lisp/with-output-to-string.lisp
+++ j/src/org/armedbear/lisp/with-output-to-string.lisp
@@ -1,7 +1,7 @@
 ;;; with-output-to-string.lisp
 ;;;
-;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: with-output-to-string.lisp,v 1.1 2004-02-01 16:48:13 piso Exp $
+;;; Copyright (C) 2003-2005 Peter Graves
+;;; $Id: with-output-to-string.lisp,v 1.2 2005-04-11 22:34:35 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,25 +17,27 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-(in-package "SYSTEM")
+(in-package #:system)
 
-;;; From CMUCL.
-(defmacro with-output-to-string ((var &optional string &key element-type)
+;;; From SBCL.
+(defmacro with-output-to-string ((var &optional string &key (element-type ''character))
 				 &body body)
   "If STRING is specified, it must be a string with a fill pointer;
    the output is incrementally appended to the string (as if by use of
    VECTOR-PUSH-EXTEND)."
-  (declare (ignore element-type))
   (multiple-value-bind (forms decls) (parse-body body)
     (if string
-        `(let ((,var (make-fill-pointer-output-stream ,string)))
+        (let ((ignored (gensym)))
+          `(let ((,var (make-fill-pointer-output-stream ,string))
+                 (,ignored ,element-type))
+             (declare (ignore ,ignored))
+             ,@decls
+             (unwind-protect
+                 (progn ,@forms)
+               (close ,var))))
+        `(let ((,var (make-string-output-stream :element-type ,element-type)))
            ,@decls
            (unwind-protect
-            (progn ,@forms)
-            (close ,var)))
-        `(let ((,var (make-string-output-stream)))
-           ,@decls
-           (unwind-protect
-            (progn ,@forms)
-            (close ,var))
+               (progn ,@forms)
+             (close ,var))
            (get-output-stream-string ,var)))))
