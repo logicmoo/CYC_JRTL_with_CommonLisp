9f19424fdbed80f2667dd16625407ea499773504
Peter Graves <gnooth@gmail.com>
2005-05-06 17:21:35 +0000

    REQUIRE-TYPE-PATHNAME-DESIGNATOR

--- j/src/org/armedbear/lisp/pathnames.lisp
+++ j/src/org/armedbear/lisp/pathnames.lisp
@@ -1,7 +1,7 @@
 ;;; pathnames.lisp
 ;;;
-;;; Copyright (C) 2003-2004 Peter Graves
-;;; $Id: pathnames.lisp,v 1.5 2004-01-26 14:25:53 piso Exp $
+;;; Copyright (C) 2003-2005 Peter Graves
+;;; $Id: pathnames.lisp,v 1.6 2005-05-06 17:21:35 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,22 +17,31 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-(in-package "SYSTEM")
+(in-package #:system)
 
 (defun pathname-host (pathname &key (case :local))
+  (require-type-pathname-designator pathname)
   (%pathname-host pathname case))
 
 (defun pathname-device (pathname &key (case :local))
+  (require-type-pathname-designator pathname)
   (%pathname-device pathname case))
 
 (defun pathname-directory (pathname &key (case :local))
+  (require-type-pathname-designator pathname)
   (%pathname-directory pathname case))
 
 (defun pathname-name (pathname &key (case :local))
+  (require-type-pathname-designator pathname)
   (%pathname-name pathname case))
 
 (defun pathname-type (pathname &key (case :local))
+  (require-type-pathname-designator pathname)
   (%pathname-type pathname case))
 
 (defun wild-pathname-p (pathname &optional field-key)
+  (require-type-pathname-designator pathname)
   (%wild-pathname-p pathname field-key))
+
+(defun require-type-pathname-designator (pathname)
+  (require-type pathname '(or pathname string file-stream)))
