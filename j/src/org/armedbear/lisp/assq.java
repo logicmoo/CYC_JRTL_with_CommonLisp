01b0a2ad7194c1651fcb9dcd619d9a02ac5f9b68
Peter Graves <gnooth@gmail.com>
2005-05-09 15:11:07 +0000

    Refactoring.

--- j/src/org/armedbear/lisp/assq.java
+++ j/src/org/armedbear/lisp/assq.java
@@ -1,8 +1,8 @@
 /*
  * assq.java
  *
- * Copyright (C) 2003 Peter Graves
- * $Id: assq.java,v 1.3 2004-11-03 15:27:23 piso Exp $
+ * Copyright (C) 2003-2005 Peter Graves
+ * $Id: assq.java,v 1.4 2005-05-09 15:11:07 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -26,23 +26,14 @@ public final class assq extends Primitive
 {
     private assq()
     {
-        super("assq", PACKAGE_EXT);
+        super("assq", PACKAGE_EXT, true);
     }
 
     public LispObject execute(LispObject item, LispObject alist)
         throws ConditionThrowable
     {
-        while (alist != NIL) {
-            LispObject cons = alist.car();
-            if (cons instanceof Cons) {
-                if (cons.car() == item)
-                    return cons;
-            } else if (cons != NIL)
-                return signal(new TypeError(cons, "list"));
-            alist = alist.cdr();
-        }
-        return NIL;
+        return assq(item, alist);
     }
 
-    private static final assq ASSQ = new assq();
+    private static final Primitive ASSQ = new assq();
 }
