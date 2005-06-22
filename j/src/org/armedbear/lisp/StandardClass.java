eca6a34024bd3cc2bdfb94848118daf9a03a3ff0
Peter Graves <gnooth@gmail.com>
2005-06-22 00:18:17 +0000

    Work in progress.

--- j/src/org/armedbear/lisp/StandardClass.java
+++ j/src/org/armedbear/lisp/StandardClass.java
@@ -2,7 +2,7 @@
  * StandardClass.java
  *
  * Copyright (C) 2003-2005 Peter Graves
- * $Id: StandardClass.java,v 1.22 2005-06-21 18:42:14 piso Exp $
+ * $Id: StandardClass.java,v 1.23 2005-06-22 00:18:17 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -71,11 +71,19 @@ public class StandardClass extends SlotClass
         return c;
     }
 
+    // At this point, BuiltInClass.java has not been completely loaded yet, and
+    // BuiltInClass.CLASS_T is null. So we need to call setDirectSuperclass()
+    // for STANDARD_CLASS and STANDARD_OBJECT in initializeStandardClasses()
+    // below.
+    public static final StandardClass STANDARD_CLASS =
+        addStandardClass(Symbol.STANDARD_CLASS, list1(BuiltInClass.CLASS_T));
     public static final StandardClass STANDARD_OBJECT =
         addStandardClass(Symbol.STANDARD_OBJECT, list1(BuiltInClass.CLASS_T));
 
-    public static final StandardClass STANDARD_CLASS =
-        addStandardClass(Symbol.STANDARD_CLASS, list1(BuiltInClass.CLASS_T));
+    // BuiltInClass.FUNCTION is also null here (see previous comment).
+    public static final StandardClass GENERIC_FUNCTION =
+        addStandardClass(Symbol.GENERIC_FUNCTION, list2(BuiltInClass.FUNCTION,
+                                                        STANDARD_OBJECT));
 
     public static final StandardClass CLASS =
         addStandardClass(Symbol.CLASS, list1(STANDARD_OBJECT));
@@ -180,9 +188,6 @@ public class StandardClass extends SlotClass
     public static final StandardClass UNDEFINED_FUNCTION =
         addStandardClass(Symbol.UNDEFINED_FUNCTION, list1(CELL_ERROR));
 
-    public static final StandardClass GENERIC_FUNCTION =
-        addStandardClass(Symbol.GENERIC_FUNCTION, list2(BuiltInClass.FUNCTION, STANDARD_OBJECT));
-
     public static final StandardClass METHOD =
         addStandardClass(Symbol.METHOD, list1(STANDARD_OBJECT));
 
@@ -205,15 +210,21 @@ public class StandardClass extends SlotClass
 
     public static void initializeStandardClasses()
     {
+        // We need to call setDirectSuperclass() here for classes that have a
+        // BuiltInClass as a superclass. See comment above (at first mention of
+        // STANDARD_OBJECT).
+        STANDARD_CLASS.setDirectSuperclass(CLASS);
+        STANDARD_OBJECT.setDirectSuperclass(BuiltInClass.CLASS_T);
+        GENERIC_FUNCTION.setDirectSuperclasses(list2(BuiltInClass.FUNCTION,
+                                                     STANDARD_OBJECT));
+
         ARITHMETIC_ERROR.setCPL(ARITHMETIC_ERROR, ERROR, SERIOUS_CONDITION,
                                 CONDITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        BUILT_IN_CLASS.setDirectSuperclass(CLASS);
-        BUILT_IN_CLASS.setCPL(BUILT_IN_CLASS, CLASS, STANDARD_OBJECT, BuiltInClass.CLASS_T);
+        BUILT_IN_CLASS.setCPL(BUILT_IN_CLASS, CLASS, STANDARD_OBJECT,
+                              BuiltInClass.CLASS_T);
         CELL_ERROR.setCPL(CELL_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
                           STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        CLASS.setDirectSuperclass(BuiltInClass.CLASS_T);
         CLASS.setCPL(CLASS, STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        CONDITION.setDirectSuperclass(STANDARD_OBJECT);
         CONDITION.setCPL(CONDITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
         CONDITION.setDirectSlots(
             list2(new SlotDefinition(Symbol.FORMAT_CONTROL,
@@ -222,41 +233,33 @@ public class StandardClass extends SlotClass
                                      list1(PACKAGE_CL.intern("SIMPLE-CONDITION-FORMAT-ARGUMENTS")))));
         CONTROL_ERROR.setCPL(CONTROL_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
                              STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        DIVISION_BY_ZERO.setDirectSuperclass(ARITHMETIC_ERROR);
         DIVISION_BY_ZERO.setCPL(DIVISION_BY_ZERO, ARITHMETIC_ERROR, ERROR,
                                 SERIOUS_CONDITION, CONDITION, STANDARD_OBJECT,
                                 BuiltInClass.CLASS_T);
-        END_OF_FILE.setDirectSuperclass(STREAM_ERROR);
         END_OF_FILE.setCPL(END_OF_FILE, STREAM_ERROR, ERROR, SERIOUS_CONDITION,
                            CONDITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        ERROR.setDirectSuperclass(SERIOUS_CONDITION);
         ERROR.setCPL(ERROR, SERIOUS_CONDITION, CONDITION, STANDARD_OBJECT,
                      BuiltInClass.CLASS_T);
         FILE_ERROR.setCPL(FILE_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
                           STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        FLOATING_POINT_INEXACT.setDirectSuperclass(ARITHMETIC_ERROR);
         FLOATING_POINT_INEXACT.setCPL(FLOATING_POINT_INEXACT, ARITHMETIC_ERROR,
                                       ERROR, SERIOUS_CONDITION, CONDITION,
                                       STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        FLOATING_POINT_INVALID_OPERATION.setDirectSuperclass(ARITHMETIC_ERROR);
         FLOATING_POINT_INVALID_OPERATION.setCPL(FLOATING_POINT_INVALID_OPERATION,
                                                 ARITHMETIC_ERROR, ERROR,
                                                 SERIOUS_CONDITION, CONDITION,
                                                 STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        FLOATING_POINT_OVERFLOW.setDirectSuperclass(ARITHMETIC_ERROR);
         FLOATING_POINT_OVERFLOW.setCPL(FLOATING_POINT_OVERFLOW, ARITHMETIC_ERROR,
                                        ERROR, SERIOUS_CONDITION, CONDITION,
                                        STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        FLOATING_POINT_UNDERFLOW.setDirectSuperclass(ARITHMETIC_ERROR);
         FLOATING_POINT_UNDERFLOW.setCPL(FLOATING_POINT_UNDERFLOW, ARITHMETIC_ERROR,
                                         ERROR, SERIOUS_CONDITION, CONDITION,
                                         STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        FORWARD_REFERENCED_CLASS.setDirectSuperclass(CLASS);
-        FORWARD_REFERENCED_CLASS.setCPL(FORWARD_REFERENCED_CLASS, CLASS, BuiltInClass.CLASS_T);
-        GENERIC_FUNCTION.setDirectSuperclasses(list2(BuiltInClass.FUNCTION, STANDARD_OBJECT));
-        GENERIC_FUNCTION.setCPL(GENERIC_FUNCTION, STANDARD_OBJECT, BuiltInClass.FUNCTION,
+        FORWARD_REFERENCED_CLASS.setCPL(FORWARD_REFERENCED_CLASS, CLASS,
+                                        BuiltInClass.CLASS_T);
+        GENERIC_FUNCTION.setCPL(GENERIC_FUNCTION, STANDARD_OBJECT,
+                                BuiltInClass.FUNCTION,
                                 BuiltInClass.CLASS_T);
-        METHOD.setDirectSuperclass(STANDARD_OBJECT);
         METHOD.setCPL(METHOD, STANDARD_OBJECT, BuiltInClass.CLASS_T);
         PACKAGE_ERROR.setCPL(PACKAGE_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
                              STANDARD_OBJECT, BuiltInClass.CLASS_T);
@@ -265,7 +268,6 @@ public class StandardClass extends SlotClass
                                      list1(PACKAGE_CL.intern("PACKAGE-ERROR-PACKAGE")))));
         PARSE_ERROR.setCPL(PARSE_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
                            STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        PRINT_NOT_READABLE.setDirectSuperclass(ERROR);
         PRINT_NOT_READABLE.setCPL(PRINT_NOT_READABLE, ERROR, SERIOUS_CONDITION,
                                   CONDITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
         PROGRAM_ERROR.setCPL(PROGRAM_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
@@ -273,44 +275,38 @@ public class StandardClass extends SlotClass
         READER_ERROR.setCPL(READER_ERROR, PARSE_ERROR, STREAM_ERROR, ERROR,
                             SERIOUS_CONDITION, CONDITION, STANDARD_OBJECT,
                             BuiltInClass.CLASS_T);
-        SERIOUS_CONDITION.setDirectSuperclass(CONDITION);
         SERIOUS_CONDITION.setCPL(SERIOUS_CONDITION, CONDITION, STANDARD_OBJECT,
                                  BuiltInClass.CLASS_T);
-        SIMPLE_CONDITION.setDirectSuperclass(CONDITION);
         SIMPLE_CONDITION.setCPL(SIMPLE_CONDITION, CONDITION, STANDARD_OBJECT,
                                 BuiltInClass.CLASS_T);
         SIMPLE_ERROR.setCPL(SIMPLE_ERROR, SIMPLE_CONDITION, ERROR,
                             SERIOUS_CONDITION, CONDITION, STANDARD_OBJECT,
                             BuiltInClass.CLASS_T);
-        SIMPLE_TYPE_ERROR.setDirectSuperclasses(list2(SIMPLE_CONDITION, TYPE_ERROR));
+        SIMPLE_TYPE_ERROR.setDirectSuperclasses(list2(SIMPLE_CONDITION,
+                                                      TYPE_ERROR));
         SIMPLE_TYPE_ERROR.setCPL(SIMPLE_TYPE_ERROR, SIMPLE_CONDITION,
                                  TYPE_ERROR, ERROR, SERIOUS_CONDITION,
                                  CONDITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
         SIMPLE_WARNING.setDirectSuperclasses(list2(SIMPLE_CONDITION, WARNING));
         SIMPLE_WARNING.setCPL(SIMPLE_WARNING, SIMPLE_CONDITION, WARNING,
                               CONDITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        SLOT_DEFINITION.setDirectSuperclass(STANDARD_OBJECT);
-        SLOT_DEFINITION.setCPL(SLOT_DEFINITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        STANDARD_CLASS.setDirectSuperclass(CLASS);
+        SLOT_DEFINITION.setCPL(SLOT_DEFINITION, STANDARD_OBJECT,
+                               BuiltInClass.CLASS_T);
         STANDARD_CLASS.setCPL(STANDARD_CLASS, CLASS,
                               STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        STANDARD_GENERIC_FUNCTION.setDirectSuperclass(GENERIC_FUNCTION);
         STANDARD_GENERIC_FUNCTION.setCPL(STANDARD_GENERIC_FUNCTION,
                                          GENERIC_FUNCTION, STANDARD_OBJECT,
-                                         BuiltInClass.FUNCTION, BuiltInClass.CLASS_T);
-        STANDARD_METHOD.setDirectSuperclass(METHOD);
-        STANDARD_METHOD.setCPL(STANDARD_METHOD, METHOD, STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        STANDARD_OBJECT.setDirectSuperclass(BuiltInClass.CLASS_T);
+                                         BuiltInClass.FUNCTION,
+                                         BuiltInClass.CLASS_T);
+        STANDARD_METHOD.setCPL(STANDARD_METHOD, METHOD, STANDARD_OBJECT,
+                               BuiltInClass.CLASS_T);
         STANDARD_OBJECT.setCPL(STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        STORAGE_CONDITION.setDirectSuperclass(SERIOUS_CONDITION);
         STORAGE_CONDITION.setCPL(STORAGE_CONDITION, SERIOUS_CONDITION, CONDITION,
                                  STANDARD_OBJECT, BuiltInClass.CLASS_T);
         STREAM_ERROR.setCPL(STREAM_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
                             STANDARD_OBJECT, BuiltInClass.CLASS_T);
-        STRUCTURE_CLASS.setDirectSuperclass(CLASS);
         STRUCTURE_CLASS.setCPL(STRUCTURE_CLASS, CLASS, STANDARD_OBJECT,
                                BuiltInClass.CLASS_T);
-        STYLE_WARNING.setDirectSuperclass(WARNING);
         STYLE_WARNING.setCPL(STYLE_WARNING, WARNING, CONDITION, STANDARD_OBJECT,
                              BuiltInClass.CLASS_T);
         TYPE_ERROR.setCPL(TYPE_ERROR, ERROR, SERIOUS_CONDITION, CONDITION,
@@ -328,7 +324,6 @@ public class StandardClass extends SlotClass
         UNDEFINED_FUNCTION.setCPL(UNDEFINED_FUNCTION, CELL_ERROR, ERROR,
                                   SERIOUS_CONDITION, CONDITION, STANDARD_OBJECT,
                                   BuiltInClass.CLASS_T);
-        WARNING.setDirectSuperclass(CONDITION);
         WARNING.setCPL(WARNING, CONDITION, STANDARD_OBJECT, BuiltInClass.CLASS_T);
 
 
