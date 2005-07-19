d18d269ab3febf423e9e3fada3cc55cce096f430
Peter Graves <gnooth@gmail.com>
2005-07-19 00:29:00 +0000

    Initial checkin.

---
+++ j/src/org/armedbear/lisp/require.lisp
@@ -0,0 +1,37 @@
+;;; require.lisp
+;;;
+;;; Copyright (C) 2003-2005 Peter Graves
+;;; $Id: require.lisp,v 1.1 2005-07-19 00:29:00 piso Exp $
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(in-package #:system)
+
+;;; PROVIDE, REQUIRE (from SBCL)
+(defun provide (module-name)
+  (pushnew (string module-name) *modules* :test #'string=)
+  t)
+
+(defun require (module-name &optional pathnames)
+  (unless (member (string module-name) *modules* :test #'string=)
+    (let ((saved-modules (copy-list *modules*)))
+      (cond (pathnames
+             (unless (listp pathnames) (setf pathnames (list pathnames)))
+             (dolist (x pathnames)
+               (load x)))
+            (t
+             (let ((*readtable* (copy-readtable nil)))
+               (load-system-file (string-downcase (string module-name))))))
+      (set-difference *modules* saved-modules))))
