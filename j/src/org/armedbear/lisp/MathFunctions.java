3452f06e52eeb4b6c93ceafa481f41c5f7b11859
Peter Graves <gnooth@gmail.com>
2005-08-24 13:14:24 +0000

    ATAN: work in progress.

--- j/src/org/armedbear/lisp/MathFunctions.java
+++ j/src/org/armedbear/lisp/MathFunctions.java
@@ -2,7 +2,7 @@
  * MathFunctions.java
  *
  * Copyright (C) 2004-2005 Peter Graves
- * $Id: MathFunctions.java,v 1.23 2005-08-23 20:53:09 piso Exp $
+ * $Id: MathFunctions.java,v 1.24 2005-08-24 13:14:24 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -180,55 +180,52 @@ public final class MathFunctions extends Lisp
                 return atan(arg);
             return signalTypeError(arg, Symbol.NUMBER);
         }
+
         // "If both number1 and number2 are supplied for atan, the result is
         // the arc tangent of number1/number2."
+
         // y = +0     x = +0       +0
         // y = -0     x = +0       -0
         // y = +0     x = -0       +<PI>
         // y = -0     x = -0       -<PI>
-        public LispObject execute(LispObject first, LispObject second)
+        public LispObject execute(LispObject y, LispObject x)
             throws ConditionThrowable
         {
-            if (!first.realp())
-                return signalTypeError(first, Symbol.REAL);
-            if (!second.realp())
-                return signalTypeError(second, Symbol.REAL);
-            if (first.floatp() && second.floatp()) {
-                if (first.zerop() && second.zerop()) {
-                    int sign1, sign2;
-                    if (first instanceof SingleFloat) {
-                        int bits = Float.floatToRawIntBits(((SingleFloat)first).value);
-                        sign1 = ((bits & 0x80000000) == 0) ? 1 : -1;
-                    } else {
-                        long bits = Double.doubleToRawLongBits(((DoubleFloat)first).value);
-                        sign1 = ((bits & 0x8000000000000000L) == 0) ? 1 : -1;
-                    }
-                    if (second instanceof SingleFloat) {
-                        int bits = Float.floatToRawIntBits(((SingleFloat)second).value);
-                        sign2 = ((bits & 0x80000000) == 0) ? 1 : -1;
-                    } else {
-                        long bits = Double.doubleToRawLongBits(((DoubleFloat)second).value);
-                        sign2 = ((bits & 0x8000000000000000L) == 0) ? 1 : -1;
-                    }
-                    double result;
-                    if (sign1 > 0) {
-                        if (sign2 > 0)
-                            result = 0.0;
-                        else
-                            result = -0.0;
-                    } else {
-                        if (sign2 < 0)
-                            result = - Math.PI;
-                        else
-                            result = Math.PI;
-                    }
-                    if (first instanceof DoubleFloat || second instanceof DoubleFloat)
+            if (!y.realp())
+                return signalTypeError(y, Symbol.REAL);
+            if (!x.realp())
+                return signalTypeError(x, Symbol.REAL);
+            if (y.floatp() && x.floatp()) {
+                int sign_y, sign_x;
+                if (y instanceof SingleFloat) {
+                    int bits = Float.floatToRawIntBits(((SingleFloat)y).value);
+                    sign_y = ((bits & 0x80000000) == 0) ? 1 : -1;
+                } else {
+                    long bits = Double.doubleToRawLongBits(((DoubleFloat)y).value);
+                    sign_y = ((bits & 0x8000000000000000L) == 0) ? 1 : -1;
+                }
+                if (x instanceof SingleFloat) {
+                    int bits = Float.floatToRawIntBits(((SingleFloat)x).value);
+                    sign_x = ((bits & 0x80000000) == 0) ? 1 : -1;
+                } else {
+                    long bits = Double.doubleToRawLongBits(((DoubleFloat)x).value);
+                    sign_x = ((bits & 0x8000000000000000L) == 0) ? 1 : -1;
+                }
+                double result;
+                if (y.zerop() && x.zerop()) {
+                    if (sign_x > 0)
+                        return y;
+                    if (sign_y > 0)
+                        result = Math.PI;
+                    else
+                        result = - Math.PI;
+                    if (y instanceof DoubleFloat || x instanceof DoubleFloat)
                         return new DoubleFloat(result);
                     else
                         return new SingleFloat((float)result);
                 }
             }
-            return atan(first.divideBy(second));
+            return atan(y.divideBy(x));
         }
     };
 
