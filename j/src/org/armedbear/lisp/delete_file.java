a04c5ffdfd0bffb97c589453b3f1572234573dc6
Peter Graves <gnooth@gmail.com>
2005-09-25 20:47:59 +0000

    Don't follow symlinks!

--- j/src/org/armedbear/lisp/delete_file.java
+++ j/src/org/armedbear/lisp/delete_file.java
@@ -1,8 +1,8 @@
 /*
  * delete_file.java
  *
- * Copyright (C) 2003-2004 Peter Graves
- * $Id: delete_file.java,v 1.7 2005-05-05 14:33:44 piso Exp $
+ * Copyright (C) 2003-2005 Peter Graves
+ * $Id: delete_file.java,v 1.8 2005-09-25 20:47:59 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,6 +22,7 @@
 package org.armedbear.lisp;
 
 import java.io.File;
+import java.io.IOException;
 
 public final class delete_file extends Primitive
 {
@@ -33,19 +34,34 @@ public final class delete_file extends Primitive
     // ### delete-file filespec => t
     public LispObject execute(LispObject arg) throws ConditionThrowable
     {
-        final LispObject truename = Pathname.truename(arg, false);
+        // Don't dereference symlinks!
+        Pathname pathname = Pathname.coerceToPathname(arg);
         if (arg instanceof Stream)
             ((Stream)arg)._close();
-        if (truename instanceof Pathname) {
+        if (pathname instanceof LogicalPathname)
+            pathname = LogicalPathname.translateLogicalPathname((LogicalPathname)pathname);
+        if (pathname.isWild())
+            return signal(new FileError("Bad place for a wild pathname.",
+                                        pathname));
+        final Pathname defaultedPathname =
+            Pathname.mergePathnames(pathname,
+                                    Pathname.coerceToPathname(_DEFAULT_PATHNAME_DEFAULTS_.symbolValue()),
+                                    NIL);
+        final String namestring = defaultedPathname.getNamestring();
+        if (namestring == null)
+            return signal(new FileError("Pathname has no namestring: " + defaultedPathname.writeToString(),
+                                        defaultedPathname));
+        final File file = new File(namestring);
+        if (file.exists()) {
             // File exists.
-            File file = Utilities.getFile((Pathname)truename);
             for (int i = 0; i < 5; i++) {
                 if (file.delete())
                     return T;
                 System.gc();
                 Thread.yield();
             }
-            StringBuffer sb = new StringBuffer("Unable to delete ");
+            Pathname truename = new Pathname(file.getAbsolutePath());
+            FastStringBuffer sb = new FastStringBuffer("Unable to delete ");
             sb.append(file.isDirectory() ? "directory " : "file ");
             sb.append(truename.writeToString());
             sb.append('.');
