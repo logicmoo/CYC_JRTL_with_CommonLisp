0cb5040ea63222aa56349d53ae1e8d8162d0fdf4
Peter Graves <gnooth@gmail.com>
2005-11-01 00:55:43 +0000

    -(load "test-utilities.lisp") +(load (merge-pathnames "test-utilities.lisp" *load-truename*))

--- j/src/org/armedbear/lisp/tests/condition-tests.lisp
+++ j/src/org/armedbear/lisp/tests/condition-tests.lisp
@@ -16,7 +16,7 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-(load "test-utilities.lisp")
+(load (merge-pathnames "test-utilities.lisp" *load-truename*))
 
 (in-package #:test)
 
