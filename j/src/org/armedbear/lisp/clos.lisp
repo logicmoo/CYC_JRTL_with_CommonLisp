6b10c1e6e3456b9886cb3f2465e900a913404c33
Peter Graves <gnooth@gmail.com>
2005-11-02 01:13:48 +0000

    MAKE-CONDITION

--- j/src/org/armedbear/lisp/clos.lisp
+++ j/src/org/armedbear/lisp/clos.lisp
@@ -1,7 +1,7 @@
 ;;; clos.lisp
 ;;;
 ;;; Copyright (C) 2003-2005 Peter Graves
-;;; $Id: clos.lisp,v 1.188 2005-11-01 17:50:50 piso Exp $
+;;; $Id: clos.lisp,v 1.189 2005-11-02 01:13:48 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -2158,8 +2158,8 @@
            ',name))))
 
 (defun make-condition (type &rest initargs)
-  (or (%make-condition type initargs)
-      (apply #'make-instance (find-class type) initargs)))
+  (let ((class (if (symbolp type) (find-class type) type)))
+    (apply #'make-instance class initargs)))
 
 ;; Adapted from SBCL.
 ;; Originally defined in signal.lisp. Redefined here now that we have MAKE-CONDITION.
