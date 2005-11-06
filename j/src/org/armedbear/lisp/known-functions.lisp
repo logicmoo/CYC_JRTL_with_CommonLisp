e341ec247c190c9a324c944710ef40988cee07e0
Peter Graves <gnooth@gmail.com>
2005-11-06 19:01:25 +0000

    Work in progress (tested).

--- j/src/org/armedbear/lisp/known-functions.lisp
+++ j/src/org/armedbear/lisp/known-functions.lisp
@@ -1,7 +1,7 @@
 ;;; known-functions.lisp
 ;;;
 ;;; Copyright (C) 2005 Peter Graves
-;;; $Id: known-functions.lisp,v 1.30 2005-11-06 02:19:41 piso Exp $
+;;; $Id: known-functions.lisp,v 1.31 2005-11-06 19:01:25 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -21,219 +21,247 @@
 
 (require '#:compiler-types)
 
-(declaim (ftype (function * symbol) copy-symbol gensym))
-(declaim (ftype (function * symbol) fdefinition-block-name))
+;; (declaim (ftype (function * symbol) copy-symbol gensym))
+;; (declaim (ftype (function * symbol) fdefinition-block-name))
+(defknown (copy-symbol gensym fdefinition-block-name) * symbol)
 
-(declaim (ftype (function (t t) t) gethash-2op-1ret))
-(declaim (ftype (function (t) symbol) make-keyword))
+;; (declaim (ftype (function (t t) t) gethash-2op-1ret))
+(defknown gethash-2op-1ret (t t) t)
+;; (declaim (ftype (function (t) symbol) make-keyword))
+(defknown make-keyword (t) symbol)
 
-(declaim (ftype (function * list)
-                backq-list backq-list* backq-append backq-nconc
-                %class-precedence-list))
+;; (declaim (ftype (function * list)
+;;                 backq-list backq-list* backq-append backq-nconc
+;;                 %class-precedence-list))
+(defknown (backq-list backq-list* backq-append backq-nconc %class-precedence-list)
+  * list)
 
-(declaim (ftype (function * cons) backq-cons))
+;; (declaim (ftype (function * cons) backq-cons))
+(defknown backq-cons * cons)
 
-(declaim (ftype (function (character) character) char-downcase char-upcase))
+;; (declaim (ftype (function (character) character) char-downcase char-upcase))
+(defknown (char-downcase char-upcase) (character) character)
 
-(declaim (ftype (function * t) finish-output force-output clear-output terpri fresh-line))
+;; (declaim (ftype (function * t) finish-output force-output clear-output terpri fresh-line))
+(defknown (finish-output force-output clear-output terpri fresh-line) * t)
 
-(declaim (ftype (function (symbol) string) symbol-name))
+;; (declaim (ftype (function (symbol) string) symbol-name))
+(defknown symbol-name (symbol) string)
 
-(declaim
- (ftype (function * string)
-        get-output-stream-string
-        nstring-capitalize
-        nstring-downcase
-        nstring-upcase
-        string-capitalize
-        string-downcase
-        string-upcase
-        write-line
-        write-string
-        ))
+;; (declaim
+;;  (ftype (function * string)
+;;         get-output-stream-string
+;;         nstring-capitalize
+;;         nstring-downcase
+;;         nstring-upcase
+;;         string-capitalize
+;;         string-downcase
+;;         string-upcase
+;;         write-line
+;;         write-string
+;;         ))
+(defknown (get-output-stream-string
+           nstring-capitalize
+           nstring-downcase
+           nstring-upcase
+           string-capitalize
+           string-downcase
+           string-upcase
+           write-line
+           write-string) * string)
 
-(declaim
- (ftype (function * t)
-        %failed-aver
-        %ldb
-        %make-structure
-        %method-function
-        %put
-        %set-cddr
-        %set-schar
-        %stream-terpri
-        %stream-write-char
-        alphanumericp
-        array-has-fill-pointer-p
-        aset
-        bit-and
-        bit-andc1
-        bit-andc2
-        bit-eqv
-        bit-ior
-        bit-nand
-        bit-nor
-        bit-not
-        bit-orc1
-        bit-orc2
-        bit-xor
-        both-case-p
-        built-in-function-p
-        caadr
-        char-equal
-        characterp
-        charpos
-        close
-        coerce
-        coerce-to-function
-        compile-file-pathname
-        complex
-        conjugate
-        delete-file
-        directory-namestring
-        eighth
-        enough-namestring
-        every
-        fifth
-        file-directory-p
-        file-namestring
-        file-position
-        fill
-        first
-        float
-        fmakunbound
-        fourth
-        fset
-        ftype-result-type
-        get-internal-real-time
-        getf
-        hash-table-count
-        hash-table-p
-        host-namestring
-        intersection
-        ldb
-        ldb-test
-        list-all-packages
-        load-compiled-function
-        lower-case-p
-        make-string-output-stream
-        map
-        merge-pathnames
-        namestring
-        neq
-        nintersection
-        ninth
-        normalize-type
-        nsubst
-        nsubst-if
-        nsubst-if-not
-        nth
-        oddp
-        pathname-type
-        pathname-type
-        pathnamep
-        phase
-        probe-file
-        proclaimed-ftype
-        random
-        read
-        read-char
-        read-sequence
-        reduce
-        replace
-        rest
-        scale-float
-        second
-        set
-        set-std-slot-value
-        setf-function-name-p
-        seventh
-        simple-condition-format-arguments
-        simple-condition-format-control
-        simple-search
-        simple-typep
-        sixth
-        some
-        sort
-        stable-sort
-        std-slot-value
-        stream-element-type
-        stream-line-number
-        string<=
-        structure-ref
-        structure-set
-        subst
-        subst-if
-        subst-if-not
-        svref
-        svset
-        tenth
-        third
-        truename
-        upper-case-p
-        vector
-        vectorp
-        write-byte
-        write-sequence
-        zerop
-        ))
+(defknown (%failed-aver
+           %ldb
+           %make-structure
+           %method-function
+           %put
+           %set-cddr
+           %set-schar
+           %stream-terpri
+           %stream-write-char
+           alphanumericp
+           array-has-fill-pointer-p
+           aset
+           bit-and
+           bit-andc1
+           bit-andc2
+           bit-eqv
+           bit-ior
+           bit-nand
+           bit-nor
+           bit-not
+           bit-orc1
+           bit-orc2
+           bit-xor
+           both-case-p
+           built-in-function-p
+           caadr
+           char-equal
+           characterp
+           charpos
+           close
+           coerce
+           coerce-to-function
+           compile-file-pathname
+           complex
+           conjugate
+           delete-file
+           directory-namestring
+           eighth
+           enough-namestring
+           every
+           fifth
+           file-directory-p
+           file-namestring
+           file-position
+           fill
+           first
+           float
+           fmakunbound
+           fourth
+           fset
+           ftype-result-type
+           get-internal-real-time
+           getf
+           hash-table-count
+           hash-table-p
+           host-namestring
+           intersection
+           ldb
+           ldb-test
+           list-all-packages
+           load-compiled-function
+           lower-case-p
+           make-string-output-stream
+           map
+           merge-pathnames
+           namestring
+           neq
+           nintersection
+           ninth
+           normalize-type
+           nsubst
+           nsubst-if
+           nsubst-if-not
+           nth
+           oddp
+           pathname-type
+           pathname-type
+           pathnamep
+           phase
+           probe-file
+           proclaimed-ftype
+           random
+           read
+           read-char
+           read-sequence
+           reduce
+           replace
+           rest
+           scale-float
+           second
+           set
+           set-std-slot-value
+           setf-function-name-p
+           seventh
+           simple-condition-format-arguments
+           simple-condition-format-control
+           simple-search
+           simple-typep
+           sixth
+           some
+           sort
+           stable-sort
+           std-slot-value
+           stream-element-type
+           stream-line-number
+           string<=
+           structure-ref
+           structure-set
+           subst
+           subst-if
+           subst-if-not
+           svref
+           svset
+           tenth
+           third
+           truename
+           upper-case-p
+           vector
+           vectorp
+           write-byte
+           write-sequence
+           zerop)
+  * t)
 
-(declaim
- (ftype (function * integer)
-        deposit-field dpb logand logcount lognor
-        mask-field
-        numerator denominator
-        boole
-        array-dimension
-        %dpb
-        ))
+(defknown (deposit-field dpb logand logcount lognor
+                         mask-field
+                         numerator denominator
+                         boole
+                         array-dimension
+                         %dpb)
+  * integer)
 
-(declaim (ftype (function (t) (integer 0 2147483647)) sxhash))
+;; (declaim (ftype (function (t) (integer 0 2147483647)) sxhash))
+(defknown sxhash (t) (integer 0 2147483647))
 
-(declaim (ftype (function (character) (unsigned-byte 16)) char-code))
+;; (declaim (ftype (function (character) (unsigned-byte 16)) char-code))
+(defknown char-code (character) (unsigned-byte 16))
 
-(declaim (ftype (function (simple-string index) character) schar))
+;; (declaim (ftype (function (simple-string index) character) schar))
+(defknown schar (simple-string index) character)
 
-(declaim (ftype (function * character) char write-char))
+;; (declaim (ftype (function * character) char write-char))
+(defknown (char write-char) * character)
 
-(declaim
- (ftype (function * t)
-        char= char/= char< char> char<= char>= char-equal char-not-equal
-        char-lessp char-greaterp char-not-greaterp char-not-lessp))
+(defknown (char= char/= char< char> char<= char>= char-equal char-not-equal
+                 char-lessp char-greaterp char-not-greaterp char-not-lessp)
+  * t)
 
-(declaim
- (ftype (function (real real) real)
-        mod rem))
+;; (declaim
+;;  (ftype (function (real real) real)
+;;         mod rem))
+(defknown (mod rem) (real real) real)
 
-(declaim (ftype (function (number) rational) rational rationalize))
+;; (declaim (ftype (function (number) rational) rational rationalize))
+(defknown (rational rationalize) (number) rational)
 
-(declaim (ftype (function * bit) bit sbit))
+;; (declaim (ftype (function * bit) bit sbit))
+(defknown (bit sbit) * bit)
 
-(declaim (ftype (function * function) make-macro))
+;; (declaim (ftype (function * function) make-macro))
+(defknown make-macro * function)
 
-(declaim (ftype (function * t) %set-arglist))
+;; (declaim (ftype (function * t) %set-arglist))
+(defknown %set-arglist * t)
 
-(declaim (ftype (function * t) %type-error check-sequence-bounds))
+;; (declaim (ftype (function * t) %type-error check-sequence-bounds))
+(defknown (%type-error check-sequence-bounds) * t)
 
-(declaim (ftype (function * t) out-synonym-of))
+;; (declaim (ftype (function * t) out-synonym-of))
+(defknown out-synonym-of * t)
 
-(declaim
- (ftype (function * t)
-        error
-        compiler-style-warn
-        compiler-warn
-        compiler-error
-        compiler-unsupported))
+(defknown (error
+           compiler-style-warn
+           compiler-warn
+           compiler-error
+           compiler-unsupported)
+  * t)
 
-(declaim (ftype (function (symbol) function) resolve))
+;; (declaim (ftype (function (symbol) function) resolve))
+(defknown resolve (symbol) function)
 
-(declaim (ftype (function (symbol) t) boundp))
+;; (declaim (ftype (function (symbol) t) boundp))
+(defknown boundp (symbol) t)
 
-(declaim (ftype (function (string fixnum character) character) %set-char))
+;; (declaim (ftype (function (string fixnum character) character) %set-char))
+(defknown %set-char (string index character) character)
 
-(declaim (ftype (function (t t t) t) set-function-info-value))
+;; (declaim (ftype (function (t t t) t) set-function-info-value))
+(defknown set-function-info-value (t t t) t)
 
-(declaim (ftype (function * hash-table) make-hash-table))
+;; (declaim (ftype (function * hash-table) make-hash-table))
+(defknown make-hash-table * hash-table)
 
 (defknown %class-slots (class) t)
 (defknown set-class-slots (class list) t)
@@ -245,5 +273,112 @@
 (defknown set-std-slot-value * t)
 (defknown open * (or stream null))
 (defknown make-string-input-stream * stream)
+
+;; Moved here from jvm.lisp.
+(defknown (+ - * /
+             1+ 1- < > <= >= = /=
+             car cdr caar cadr cdar cddr cadar caddr cdddr cddddr
+             first second third
+             eq eql equal equalp
+             length
+             constantp symbolp
+             list list*
+             macro-function
+             compiler-macro-function
+             sys::%defun
+             get
+             atom
+             compiled-function-p
+             fdefinition
+             special-operator-p keywordp functionp fboundp zerop consp listp
+             numberp integerp floatp
+             plusp minusp
+             complexp arrayp readtablep packagep
+             array-dimensions array-rank array-total-size
+             array-element-type upgraded-array-element-type
+             simple-vector-p simple-string-p bit-vector-p simple-bit-vector-p
+             stringp
+             row-major-aref
+             quote function
+             mapcar
+             find position
+             append nconc subseq adjoin
+             revappend nreconc
+             copy-seq
+             assoc assoc-if assoc-if-not acons assq assql
+             code-char char-int digit-char-p
+             member ext:memq
+             remove remove-if remove-if-not delete delete-if delete-if-not
+             special-variable-p
+             symbol-function
+             coerce
+             reverse nreverse
+             last
+             cons rplaca rplacd
+             sys::set-car sys::set-cdr
+             copy-list copy-tree
+             make-sequence make-list make-array make-package
+             make-string
+             find-package
+             pathname make-pathname pathname-name directory
+             package-used-by-list package-shadowing-symbols
+             nthcdr
+             aref elt
+             not null endp
+             concatenate
+             format sys::%format
+             prin1 princ print write
+             compute-restarts find-restart restart-name
+             string
+             string=
+             setq
+             multiple-value-list push pop
+             type-of class-of
+             typep sys::%typep
+             abs
+             ash
+             float-radix
+             logand logandc1 logandc2 logeqv logior lognand
+             lognot logorc1 logorc2 logxor
+             logbitp
+             slot-boundp slot-value slot-exists-p
+             allocate-instance
+             find-class
+             class-name
+             constantly
+             exp expt log
+             min max
+             realpart imagpart
+             integer-length
+             sqrt isqrt gcd lcm signum
+             open
+             svref
+             fill-pointer
+             symbol-value symbol-package package-name
+             fourth
+             vector-push vector-push-extend
+             union nunion
+             remove-duplicates delete-duplicates
+             read-byte
+             fresh-line terpri
+             lambda
+             ext:classp
+             ext:fixnump
+             ext:memql
+             sys:%generic-function-name
+             sys::puthash
+             precompiler::precompile1
+             declare
+             go
+             inst
+             emit
+             label
+             maybe-emit-clear-values
+             single-valued-p
+             sys:read-8-bits
+             sys:write-8-bits
+             sys::require-type
+             sys::arg-count-error)
+  * t)
 
 (provide '#:known-functions)
