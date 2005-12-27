ed6c19f5bb27c27af7749875109099fd6d8fcf66
Peter Graves <gnooth@gmail.com>
2005-12-27 20:00:58 +0000

    MOP::READER-METHOD-SLOT-NAME MOP::SET-READER-METHOD-SLOT-NAME

--- j/src/org/armedbear/lisp/StandardReaderMethod.java
+++ j/src/org/armedbear/lisp/StandardReaderMethod.java
@@ -2,7 +2,7 @@
  * StandardReaderMethod.java
  *
  * Copyright (C) 2005 Peter Graves
- * $Id: StandardReaderMethod.java,v 1.1 2005-12-27 19:03:29 piso Exp $
+ * $Id: StandardReaderMethod.java,v 1.2 2005-12-27 20:00:58 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -28,4 +28,41 @@ public final class StandardReaderMethod extends StandardMethod
     super(StandardClass.STANDARD_READER_METHOD,
           StandardClass.STANDARD_READER_METHOD.getClassLayout().getLength());
   }
+
+  // ### reader-method-slot-name
+  private static final Primitive READER_METHOD_SLOT_NAME =
+      new Primitive("reader-method-slot-name", PACKAGE_MOP, false, "reader-method")
+  {
+      public LispObject execute(LispObject arg) throws ConditionThrowable
+      {
+          try
+          {
+              return ((StandardReaderMethod)arg).slots[StandardReaderMethodClass.SLOT_INDEX_SLOT_NAME];
+          }
+          catch (ClassCastException e)
+          {
+              return signalTypeError(arg, Symbol.STANDARD_READER_METHOD);
+          }
+      }
+  };
+
+  // ### set-reader-method-slot-name
+  private static final Primitive SET_READER_METHOD_SLOT_NAME =
+      new Primitive("set-reader-method-slot-name", PACKAGE_MOP, false,
+                    "reader-method slot-name")
+  {
+      public LispObject execute(LispObject first, LispObject second)
+          throws ConditionThrowable
+      {
+          try
+          {
+              ((StandardReaderMethod)first).slots[StandardReaderMethodClass.SLOT_INDEX_SLOT_NAME] = second;
+              return second;
+          }
+          catch (ClassCastException e)
+          {
+              return signalTypeError(first, Symbol.STANDARD_READER_METHOD);
+          }
+      }
+  };
 }
