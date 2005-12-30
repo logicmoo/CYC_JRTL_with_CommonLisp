9c50cdc5a6dc6108d4fd7f7f25ed4950bd88d598
Peter Graves <gnooth@gmail.com>
2005-12-30 20:24:49 +0000

    Work in progress (tested).

--- j/src/org/armedbear/lisp/map.lisp
+++ j/src/org/armedbear/lisp/map.lisp
@@ -1,7 +1,7 @@
 ;;; map.lisp
 ;;;
-;;; Copyright (C) 2003 Peter Graves
-;;; $Id: map.lisp,v 1.1 2003-06-10 19:03:50 piso Exp $
+;;; Copyright (C) 2005 Peter Graves
+;;; $Id: map.lisp,v 1.2 2005-12-30 20:24:49 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -17,25 +17,28 @@
 ;;; along with this program; if not, write to the Free Software
 ;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-(in-package "COMMON-LISP")
-
-(export 'map)
-
-;;; MAP (from ECL)
+(in-package #:system)
 
 (defun map (result-type function sequence &rest more-sequences)
-  (setq more-sequences (cons sequence more-sequences))
-  (let ((l (apply #'min (mapcar #'length more-sequences))))
-    (if (null result-type)
-        (do ((i 0 (1+ i))
-             (l l))
-            ((>= i l) nil)
-          (apply function (mapcar #'(lambda (z) (elt z i))
-                                  more-sequences)))
-        (let ((x (make-sequence result-type l)))
-          (do ((i 0 (1+ i))
-               (l l))
-              ((>= i l) x)
-            (setf (elt x i)
-                  (apply function (mapcar #'(lambda (z) (elt z i))
-                                          more-sequences))))))))
+  (let* ((sequences (cons sequence more-sequences))
+         (limit (apply #'min (mapcar #'length sequences))))
+    (declare (type index limit))
+    (cond ((null result-type)
+           (dotimes (i limit nil)
+             (apply function (mapcar #'(lambda (z) (elt z i)) sequences))))
+          ((eq result-type 'LIST)
+           (let (result)
+             (dotimes (i limit (nreverse result))
+               (push (apply function (mapcar #'(lambda (z) (elt z i)) sequences))
+                     result))))
+          (t
+           (let ((result (case result-type
+                           (STRING
+                            (make-string limit))
+                           (VECTOR
+                            (make-array limit))
+                           (t
+                            (make-sequence result-type limit)))))
+             (dotimes (i limit result)
+               (setf (elt result i)
+                     (apply function (mapcar #'(lambda (z) (elt z i)) sequences)))))))))
