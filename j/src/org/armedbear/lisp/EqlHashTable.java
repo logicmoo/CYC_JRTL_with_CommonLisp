c99783fbedae1d5110ca6bc536e049c9331005ed
Peter Graves <gnooth@gmail.com>
2006-01-07 22:56:08 +0000

    Reformatted source.

--- j/src/org/armedbear/lisp/EqlHashTable.java
+++ j/src/org/armedbear/lisp/EqlHashTable.java
@@ -1,8 +1,8 @@
 /*
  * EqlHashTable.java
  *
- * Copyright (C) 2004-2005 Peter Graves
- * $Id: EqlHashTable.java,v 1.5 2005-11-07 16:48:44 piso Exp $
+ * Copyright (C) 2004-2006 Peter Graves
+ * $Id: EqlHashTable.java,v 1.6 2006-01-07 22:56:08 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,95 +23,105 @@ package org.armedbear.lisp;
 
 public final class EqlHashTable extends HashTable
 {
-    private int mask;
+  private int mask;
 
-    public EqlHashTable(int size, LispObject rehashSize,
-                        LispObject rehashThreshold)
-    {
-        super(calculateInitialCapacity(size), rehashSize, rehashThreshold);
-        mask = buckets.length - 1;
-    }
+  public EqlHashTable(int size, LispObject rehashSize,
+                      LispObject rehashThreshold)
+  {
+    super(calculateInitialCapacity(size), rehashSize, rehashThreshold);
+    mask = buckets.length - 1;
+  }
 
-    public Symbol getTest()
-    {
-        return Symbol.EQL;
-    }
+  public Symbol getTest()
+  {
+    return Symbol.EQL;
+  }
 
-    public LispObject get(LispObject key)
-    {
-        HashEntry e = buckets[key.sxhash() & mask];
-        while (e != null) {
-            if (key.eql(e.key))
-                return e.value;
-            e = e.next;
-        }
-        return null;
-    }
+  public LispObject get(LispObject key)
+  {
+    HashEntry e = buckets[key.sxhash() & mask];
+    while (e != null)
+      {
+        if (key.eql(e.key))
+          return e.value;
+        e = e.next;
+      }
+    return null;
+  }
 
-    public void put(LispObject key, LispObject value)
-    {
-        int index = key.sxhash() & mask;
-        HashEntry e = buckets[index];
-        while (e != null) {
-            if (key.eql(e.key)) {
-                e.value = value;
-                return;
-            }
-            e = e.next;
-        }
-        // Not found. We need to add a new entry.
-        if (++count > threshold) {
-            rehash();
-            // Need a new hash value to suit the bigger table.
-            index = key.sxhash() & mask;
-        }
-        e = new HashEntry(key, value);
-        e.next = buckets[index];
-        buckets[index] = e;
-    }
+  public void put(LispObject key, LispObject value)
+  {
+    int index = key.sxhash() & mask;
+    HashEntry e = buckets[index];
+    while (e != null)
+      {
+        if (key.eql(e.key))
+          {
+            e.value = value;
+            return;
+          }
+        e = e.next;
+      }
+    // Not found. We need to add a new entry.
+    if (++count > threshold)
+      {
+        rehash();
+        // Need a new hash value to suit the bigger table.
+        index = key.sxhash() & mask;
+      }
+    e = new HashEntry(key, value);
+    e.next = buckets[index];
+    buckets[index] = e;
+  }
 
-    public LispObject remove(LispObject key)
-    {
-        final int index = key.sxhash() & mask;
-        HashEntry e = buckets[index];
-        HashEntry last = null;
-        while (e != null) {
-            if (key.eql(e.key)) {
-                if (last == null)
-                    buckets[index] = e.next;
-                else
-                    last.next = e.next;
-                --count;
-                return e.value;
-            }
-            last = e;
-            e = e.next;
-        }
-        return null;
-    }
+  public LispObject remove(LispObject key)
+  {
+    final int index = key.sxhash() & mask;
+    HashEntry e = buckets[index];
+    HashEntry last = null;
+    while (e != null)
+      {
+        if (key.eql(e.key))
+          {
+            if (last == null)
+              buckets[index] = e.next;
+            else
+              last.next = e.next;
+            --count;
+            return e.value;
+          }
+        last = e;
+        e = e.next;
+      }
+    return null;
+  }
 
-    protected void rehash()
-    {
-        HashEntry[] oldBuckets = buckets;
-        int newCapacity = buckets.length * 2;
-        threshold = (int) (newCapacity * loadFactor);
-        buckets = new HashEntry[newCapacity];
-        mask = buckets.length - 1;
-        for (int i = oldBuckets.length; i-- > 0;) {
-            HashEntry e = oldBuckets[i];
-            while (e != null) {
-                final int index = e.key.sxhash() & mask;
-                HashEntry dest = buckets[index];
-                if (dest != null) {
-                    while (dest.next != null)
-                        dest = dest.next;
-                    dest.next = e;
-                } else
-                    buckets[index] = e;
-                HashEntry next = e.next;
-                e.next = null;
-                e = next;
-            }
-        }
-    }
+  protected void rehash()
+  {
+    HashEntry[] oldBuckets = buckets;
+    int newCapacity = buckets.length * 2;
+    threshold = (int) (newCapacity * loadFactor);
+    buckets = new HashEntry[newCapacity];
+    mask = buckets.length - 1;
+    for (int i = oldBuckets.length; i-- > 0;)
+      {
+        HashEntry e = oldBuckets[i];
+        while (e != null)
+          {
+            final int index = e.key.sxhash() & mask;
+            HashEntry dest = buckets[index];
+            if (dest != null)
+              {
+                while (dest.next != null)
+                  dest = dest.next;
+                dest.next = e;
+              }
+            else
+              buckets[index] = e;
+            HashEntry next = e.next;
+            e.next = null;
+            e = next;
+          }
+      }
+  }
 }
