58f6772c72f470e225dcdc9daae0109213fa30df
Peter Graves <gnooth@gmail.com>
2006-02-18 17:37:44 +0000

    Reformatted source.

--- j/src/org/armedbear/j/CppMode.java
+++ j/src/org/armedbear/j/CppMode.java
@@ -2,7 +2,7 @@
  * CppMode.java
  *
  * Copyright (C) 1998-2006 Peter Graves
- * $Id: CppMode.java,v 1.4 2006-02-18 17:36:30 piso Exp $
+ * $Id: CppMode.java,v 1.5 2006-02-18 17:37:44 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,97 +23,100 @@ package org.armedbear.j;
 
 public final class CppMode extends CMode implements Constants, Mode
 {
-    private static final String[] cppConditionals = {
-        "if",
-        "else",
-        "do",
-        "while",
-        "for",
-        "switch",
-        "try",
-        "catch"
+  private static final String[] cppConditionals =
+    {
+      "if",
+      "else",
+      "do",
+      "while",
+      "for",
+      "switch",
+      "try",
+      "catch"
     };
 
-    // Since this class is final, we may as well construct the singleton class
-    // instance right away.
-    private static CppMode mode = new CppMode();
+  // Since this class is final, we may as well construct the singleton class
+  // instance right away.
+  private static CppMode mode = new CppMode();
 
-    private CppMode()
-    {
-        super(CPP_MODE, CPP_MODE_NAME);
-        keywords = new Keywords(this);
-        conditionals = cppConditionals;
-    }
+  private CppMode()
+  {
+    super(CPP_MODE, CPP_MODE_NAME);
+    keywords = new Keywords(this);
+    conditionals = cppConditionals;
+  }
 
-    public static final Mode getMode()
-    {
-        return mode;
-    }
+  public static final Mode getMode()
+  {
+    return mode;
+  }
 
-    public final String getCommentStart()
-    {
-        return "// ";
-    }
+  public final String getCommentStart()
+  {
+    return "// ";
+  }
 
-    public final String getCommentEnd()
-    {
-        return null;
-    }
+  public final String getCommentEnd()
+  {
+    return null;
+  }
 
-    public final Formatter getFormatter(Buffer buffer)
-    {
-        return new CFormatter(buffer, LANGUAGE_CPP);
-    }
+  public final Formatter getFormatter(Buffer buffer)
+  {
+    return new CFormatter(buffer, LANGUAGE_CPP);
+  }
 
-    protected void setKeyMapDefaults(KeyMap km)
-    {
-        super.setKeyMapDefaults(km);
-        km.unmapKey(':'); // No electric ':'.
-    }
+  protected void setKeyMapDefaults(KeyMap km)
+  {
+    super.setKeyMapDefaults(km);
+    km.unmapKey(':'); // No electric ':'.
+  }
 
-    public Tagger getTagger(SystemBuffer buffer)
-    {
-        return new CppTagger(buffer);
-    }
+  public Tagger getTagger(SystemBuffer buffer)
+  {
+    return new CppTagger(buffer);
+  }
 
-    public boolean hasQualifiedNames()
-    {
-        return true;
-    }
+  public boolean hasQualifiedNames()
+  {
+    return true;
+  }
 
-    public boolean isQualifiedName(String s)
-    {
-        return s.indexOf("::") >= 0;
-    }
+  public boolean isQualifiedName(String s)
+  {
+    return s.indexOf("::") >= 0;
+  }
 
-    public int getCorrectIndentation(Line line, Buffer buffer)
-    {
-        String trim = line.trim();
-        final char trimFirstChar = trim.length() > 0 ? trim.charAt(0) : 0;
-        if (trimFirstChar == '}')
-            return indentClosingBrace(line, buffer);
-        if (trim.equals("public:") || trim.equals("private:") || trim.equals("protected:"))
-            return 0;
-        Line model = findModel(line);
-        if (model == null)
-            return 0;
-        if (trim.startsWith(": ") || trim.startsWith(":\t")) {
-            // start of member initialization list
-            return model.getIndentation() + 2;
-        }
-        if (trim.startsWith("{")) {
-            String modelTrim = model.trim();
-            if (modelTrim.startsWith(": ") || modelTrim.startsWith(":\t"))
-                return 0;
-        }
-        return super.getCorrectIndentation(line, buffer);
-    }
+  public int getCorrectIndentation(Line line, Buffer buffer)
+  {
+    String trim = line.trim();
+    final char trimFirstChar = trim.length() > 0 ? trim.charAt(0) : 0;
+    if (trimFirstChar == '}')
+      return indentClosingBrace(line, buffer);
+    if (trim.equals("public:") || trim.equals("private:") || trim.equals("protected:"))
+      return 0;
+    Line model = findModel(line);
+    if (model == null)
+      return 0;
+    if (trim.startsWith(": ") || trim.startsWith(":\t"))
+      {
+        // start of member initialization list
+        return model.getIndentation() + 2;
+      }
+    if (trim.startsWith("{"))
+      {
+        String modelTrim = model.trim();
+        if (modelTrim.startsWith(": ") || modelTrim.startsWith(":\t"))
+          return 0;
+      }
+    return super.getCorrectIndentation(line, buffer);
+  }
 
-    protected int indentClosingBrace(Line line, Buffer buffer)
-    {
-        Position pos = matchClosingBrace(new Position(line, 0));
-        if (!pos.getLine().trim().startsWith("{"))
-            pos = findPreviousConditional(pos);
-        return buffer.getIndentation(pos.getLine());
-    }
+  protected int indentClosingBrace(Line line, Buffer buffer)
+  {
+    Position pos = matchClosingBrace(new Position(line, 0));
+    if (!pos.getLine().trim().startsWith("{"))
+      pos = findPreviousConditional(pos);
+    return buffer.getIndentation(pos.getLine());
+  }
 }
