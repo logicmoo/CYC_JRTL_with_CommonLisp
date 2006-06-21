0333a518e1204f490250e688782e25be7793e4e7
Peter Graves <gnooth@gmail.com>
2006-06-21 11:42:29 +0000

    CREATE-NEW-FILE now takes a namestring rather than a pathname.

--- j/src/org/armedbear/lisp/create_new_file.java
+++ j/src/org/armedbear/lisp/create_new_file.java
@@ -1,8 +1,8 @@
 /*
  * create_new_file.java
  *
- * Copyright (C) 2004-2005 Peter Graves
- * $Id: create_new_file.java,v 1.5 2005-10-23 16:22:15 piso Exp $
+ * Copyright (C) 2004-2006 Peter Graves
+ * $Id: create_new_file.java,v 1.6 2006-06-21 11:42:29 piso Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -29,16 +29,12 @@ public final class create_new_file extends Primitive
 {
     private create_new_file()
     {
-        super("create-new-file", PACKAGE_SYS, false, "pathname");
+        super("create-new-file", PACKAGE_SYS, false, "namestring");
     }
 
     public LispObject execute(LispObject arg) throws ConditionThrowable
     {
-        Pathname pathname = coerceToPathname(arg);
-        String namestring = pathname.getNamestring();
-        if (namestring == null)
-            signal(new FileError("Pathname has no namestring: " + pathname.writeToString(),
-                                 pathname));
+        final String namestring = arg.getStringValue();
         try {
             return new File(namestring).createNewFile() ? T : NIL;
         }
