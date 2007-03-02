6310055d1139c6e7c9fabc12d4a9d784cc707f54
Peter Graves <gnooth@gmail.com>
2007-03-02 17:11:41 +0000

    reformatted source

--- j/examples/hello.java
+++ j/examples/hello.java
@@ -2,14 +2,16 @@ import org.armedbear.lisp.*;
 
 public class hello
 {
-    public static void main(String[] args)
-    {
-        try {
-            Interpreter interpreter = Interpreter.createInstance();
-            interpreter.eval("(format t \"Hello, world!~%\")");
-        }
-        catch (Throwable t) {
-            t.printStackTrace();
-        }
-    }
+  public static void main(String[] args)
+  {
+    try
+      {
+        Interpreter interpreter = Interpreter.createInstance();
+        interpreter.eval("(format t \"Hello, world!~%\")");
+      }
+    catch (Throwable t)
+      {
+        t.printStackTrace();
+      }
+  }
 }
