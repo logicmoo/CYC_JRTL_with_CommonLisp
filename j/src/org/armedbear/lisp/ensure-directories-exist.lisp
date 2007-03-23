983cd2b58575a05f6ff9600e2b2fc317dacdc62b
Peter Graves <gnooth@gmail.com>
2007-03-23 09:32:45 +0000

    minor cleanup

--- j/src/org/armedbear/lisp/ensure-directories-exist.lisp
+++ j/src/org/armedbear/lisp/ensure-directories-exist.lisp
@@ -1,7 +1,7 @@
 ;;; ensure-directories-exist.lisp
 ;;;
-;;; Copyright (C) 2004 Peter Graves
-;;; $Id: ensure-directories-exist.lisp,v 1.1 2004-01-07 19:51:14 piso Exp $
+;;; Copyright (C) 2004-2007 Peter Graves
+;;; $Id: ensure-directories-exist.lisp,v 1.2 2007-03-23 09:32:45 piso Exp $
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License
@@ -15,7 +15,7 @@
 ;;;
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program; if not, write to the Free Software
-;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 ;;; Adapted from SBCL.
 
@@ -47,5 +47,5 @@
                           :pathname pathspec
                           :format-control "Can't create directory ~A."
                           :format-arguments (list namestring)))
-                 (setf created-p t)))))
+                 (setq created-p t)))))
       (values pathname created-p))))
