eb2974b4c0f937288088989017cdd764c123291a
Douglas R. Miles <logicmoo@gmail.com>
2019-05-03 06:28:48 +0000

    scope the loader

--- platform/e2c/shrdlu/src/loader.lisp
+++ platform/e2c/shrdlu/src/loader.lisp
@@ -1,13 +1,21 @@
+(make-package "SHRDLU" :use '("CL"))
+
+(in-package :SHRDLU)
 ;
 ; This file loads in all the needed files for SHRDLU.
 ;
-(defpackage shrdlu
-  (:use :common-lisp))
-(in-package :shrdlu)
+
+;; (setq *DEFAULT-PATHNAME-DEFAULTS* #P"/mnt/gggg/opt/CYC_JRTL_with_CommonLisp/platform/e2c/shrdlu/consoleshrdlu/")  (LOAD "loader.lisp")
+
+;; (setq *DEFAULT-PATHNAME-DEFAULTS* #P"G:/opt/CYC_JRTL_with_CommonLisp/platform/e2c/shrdlu/consoleshrdlu/")
+;; (setq *DEFAULT-PATHNAME-DEFAULTS* #P"/mnt/gggg/CYC_JRTL_with_CommonLisp/platform/e2c/shrdlu/consoleshrdlu/")
 
 ; This appears to be the width of the terminal
 ; No, it's not.  Not sure what it is....
 (DEFCONSTANT CHRCT 76)
+
+(let ((*DEFAULT-PATHNAME-DEFAULTS* (make-pathname :directory (pathname-directory *load-pathname* ))))
+
 
 (load "fixes.lisp")
 (load "progmr.lisp")
@@ -28,9 +36,16 @@
 (load "newans.lisp")
 (load "mover.lisp")
 
+)
+
 ; Set either (DEBUGMODE) or (USERMODE) here.
 (USERMODE)
-(DEBUGMODE)
+
+#+ABCL
+(when ext:*COMMAND-LINE-ARGUMENT-LIST* (DEBUGMODE))
+
+#+ABCL
+(print ext:*COMMAND-LINE-ARGUMENT-LIST*)
 
 ; Start the program
 (INITIALSTUFF 'UMR-1.0 NIL)
